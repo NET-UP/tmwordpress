c621f096cb96195465a6efe785c0fdb1c21ef5a7
Schneider Tobias <schneidert@net-up.net>
2019-11-27 16:31:23 +0100

    added badge in eventlist

--- partials/_event_list_item.php
+++ partials/_event_list_item.php
@@ -26,7 +26,7 @@
 					$tm_output .= '</div>';
 					$tm_output .= '<div class="col-sm-4 col-md-5">';
 					  $tm_output .= '<a aria-label="' . __("Zur Ticketauswahlseite für ") . $event->ev_name  . '" href="/event/?id=' . $event->id . '" class="btn btn-primary btn-sm px-3 float-sm-right d-block" title="' . __("Zur Ticketauswahlseite ") . '">';
-						$tm_output .= '<i class="fas fa-ticket-alt"></i> &nbsp;' . __("Tickets");
+						$tm_output .= __("Weiter") . ' &nbsp;<i class="fas fa-angle-right"></i>';
 					  $tm_output .= '</a>';
 					$tm_output .= '</div>';
 				  $tm_output .= '</div>';
