405b42c6fc3d49fb5bdd063d827980d5eb0d550e
Schneider Tobias <schneidert@net-up.net>
2019-11-27 16:38:55 +0100

    added class to tags

--- partials/_event_page_tickets.php
+++ partials/_event_page_tickets.php
@@ -2,66 +2,13 @@
     function tm_event_page_tickets ( $event, $globals ) {
 #<% unless @event.categories.select{|ec| ec.ec_price >= 0}.empty? %>
 #<% end %>
-        if($event->state->prices_shown){
-            $tm_output .= '';
-        } $tm_output .= '
-                
-                    <div class="form card mb-3" data-validation="validateTicketsnumselectForm">
-                        <div class="table-header">
-                            <div class="row">
-                                <div class="<% if @event.state["sale_active"] %>col-5<% else %>col-8 col-sm-9<% end %>">
-                                    <%= custom_locale("webshop.select_unseated.table2.row1") %>
-                                </div>
-
-                                <div class="col-4 col-sm-3 <% if !@event.state["sale_active"] %>text-right<% end %>">
-                                    <%= custom_locale("webshop.select_unseated.table2.row2") %>
-                                </div>
-
-                                <% if @event.state["sale_active"] %>
-                                    <div class="col-3 col-sm-2">
-                                        <%= custom_locale("webshop.select_unseated.table2.row3") %>
-                                    </div>
-
-                                    <div class="col-2 d-none d-sm-block text-right">
-                                        <span><%= custom_locale("webshop.select_unseated.table2.row4") %></span>
-                                    </div>
-                                <% end %>
-                            </div>
-                        </div>
-
-                        <div class="table-body">
-                            <%= render custom_view( "ticket_table_body" ) %>
-                        </div>
-                        
-                        <% if @event.state["sale_active"] %>
-                            <div class="table-footer">
-                                <div class="row">
-                                    <div class="col-12 text-right">
-                                        <%= custom_locale("webshop.userdata.order_history.total") %>: &nbsp;
-                                        <span id="tickets_price_sum" class="primary-text"><%= custom_currency(0.00) %></span>
-                                    </div>
-                                </div>
-                            </div>
-                        <% end %>
-
-                    </div>
-                
-            
-
-            <div>
-                <%= link_to :back, :class => "btn btn-secondary px-3" do %>
+        if($event->state["prices_shown"] == true){
+            $tm_output .= '
+                <a href="/events">
                     <i class="fas fa-chevron-left"></i> &nbsp;<%= custom_locale("webshop.check_out.billinginfo.back") %>
-                <% end %>
-
-                <% if @event.state[:prices_shown] && @event.state["sale_active"] %>
-                    <% unless @event.categories.select{|ec| ec.ec_price > 0}.empty? %>
-                        <%= button_tag(:class => "btn btn-primary float-right px-3", :type=>"submit", :onclick => 'Validate("#ticket_select_form");', :id => "add_to_cart") do %>
-                            <%= custom_locale("webshop.body.to_basket") %> &nbsp;<i class="far fa-arrow-alt-circle-right"></i>
-                        <% end %>
-                    <% end %>
-                <% end %>
-            </div>
-        ';
+                </a>'
+        ;
+        }
         return $tm_output;
 	}
 ?>
