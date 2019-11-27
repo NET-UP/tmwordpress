e368c5e11e883568fb91b4288bdf5361903acf7c
Schneider Tobias <schneidert@net-up.net>
2019-11-27 14:43:50 +0100

    added event

--- pages/event.php
+++ pages/event.php
@@ -10,7 +10,9 @@
 					<div class="col-12">
 						<h5>
 						<span>
-							<h3 class="d-inline-block" role="fe_nu_ev_name">{$event->ev_name}</h3>
+							<h3 class="d-inline-block" role="fe_nu_ev_name">' .
+								$event->ev_name .
+							'</h3>
 						</span>
 						</h5>
 					</div>
@@ -18,11 +20,11 @@
 				
 				<div class="row">
 					<div class="col-12 col-lg-5 col-xl-6">';
-						include "event_page_information.php";
+						#include "event_page_information.php";
 		$tm_output .= '
 					</div>
 					<div class="col-12 col-lg-7 col-xl-6">';
-						include "event_page_tickets.php";
+						#include "event_page_tickets.php";
 		$tm_output .= '
 					</div>
 				</div>
