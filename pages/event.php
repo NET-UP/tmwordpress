aa0bc1d2313b5df8cf209b36d20563e31065a464
Schneider Tobias <schneidert@net-up.net>
2019-11-27 15:26:59 +0100

    added partials for event details

--- pages/event.php
+++ pages/event.php
@@ -21,11 +21,11 @@
 				
 				<div class="row">
 					<div class="col-12 col-lg-5 col-xl-6">';
-						#include "event_page_information.php";
+		$tm_output .= tm_event_page_information($event, $globals);
 		$tm_output .= '
 					</div>
 					<div class="col-12 col-lg-7 col-xl-6">';
-						#include "event_page_tickets.php";
+		#$tm_output .= tm_event_page_tickets($event, $globals);
 		$tm_output .= '
 					</div>
 				</div>
---
+++ partials/_event_page_information.php
@@ -0,0 +1,34 @@
+<? php
+    function tm_event_page_information ( $event, $globals ) {
+
+        $tm_output .= '
+            <card class="card mb-4">
+                <div class="card-img-top" style="background-image:url('. $event->event_img_url .')">
+                    <div class="badge badge-danger float-right mt-1 mr-2">'. $event->rules->badge .'</div>
+                </div>
+                <div class="card-body position-relative">
+
+                    <div class="card-meta">
+                        <div class="card-meta-tag" role="fe_nu_ev_date"><i class="far fa-calendar-alt tm-icon" aria-hidden="true"></i> &nbsp;'. date( "d.m.Y", strtotime($event->ev_date) ) .'</div> 
+                        <div class="card-meta-tag" role="fe_nu_ev_time"><i class="far fa-clock tm-icon" aria-hidden="true"></i> &nbsp;'. date( "H:i", strtotime($event->ev_date) ) .'</div>
+                        <div class="card-meta-tag" role="fe_nu_ev_location"><i class="fas fa-map-marker-alt tm-icon"></i> &nbsp; <a aria-label="' . __("Veranstaltungsort: ") . $event->ev_location_name . '" href="' . $globals->map_query_url . urlencode($event->event_location->street . " " . $event->event_location->house_number . " " . $event->event_location->zip . " " . $event->event_location->city . " " . $event->event_location->country ) . '" target="_blank" title="' . __("Veranstaltungsort: ") . $event->ev_location_name . '">' . $event->ev_location_name . '</a> </div>
+                        
+                    </div>
+
+                    <div class="card-text mt-3" role="fe_nu_ev_des">
+                        '. $event->ev_description .'
+                    </div>
+
+                    <div class="card-meta">
+                        'foreach($event->tags as $tags) {
+                            echo "<div card-meta-tag keyword>". $tags ."</div>"
+                        }'
+                    </div>
+
+                </div>
+            </card>
+        ';
+
+        return $tm_output;
+	}
+?>
