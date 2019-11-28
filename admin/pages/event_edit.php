ba07e076a24d5888df912ff07984db50755ede58
Schneider Tobias <schneidert@net-up.net>
2019-11-28 14:55:31 +0100

    edit event page

--- admin/pages/event_edit.php
+++ admin/pages/event_edit.php
@@ -1,1 +1,14 @@
-n word
+<?php
+    $tm_json = apiRequest($api->get_single_event);
+    $event = (object)$tm_json;
+    print_r($event);
+?>
+
+<div class="wrap">
+    <h2>TicketMachine > 
+
+    </h2>
+</div>
+<?php
+    
+?>
