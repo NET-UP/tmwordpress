32debe854b1e776f0e06d2ca0514e3d09c47c16b
Schneider Tobias <schneidert@net-up.net>
2019-11-28 10:31:09 +0100

    eventspage test

--- admin/menu.php
+++ admin/menu.php
@@ -36,12 +36,12 @@
 	add_action('admin_menu', 'tm_admin_menu');
 	
 	function tt_add_menu_items(){
+		include( plugin_dir_path( __FILE__ ) . 'pages/events.php');
 		add_menu_page('Example Plugin List Table', 'List Table Example', 'activate_plugins', 'tt_list_test', 'tt_render_list_page');
 	} 
 	add_action('admin_menu', 'tt_add_menu_items');
-	
+
 	function tm_events_page(){
-		include( plugin_dir_path( __FILE__ ) . 'pages/events.php');
 	}
 	function tm_settings_page(){
 		include( plugin_dir_path( __FILE__ ) . 'pages/settings.php');
