8347301d73c9be2b10f962351ef0e06d094f7498
Schneider Tobias <schneidert@net-up.net>
2019-11-28 16:21:56 +0100

    edit event page

--- admin/pages/event_edit.php
+++ admin/pages/event_edit.php
@@ -27,30 +27,30 @@
 
     <div class="col-xs-12">
         <label for="event_edit_locationname"><?php echo __('Veranstaltungsort', 'ticketmachine') ?></label>
-        <input id="event_location_name" type="text" class="form-control">
+        <input id="event_location_name" type="text" class="form-control" value="<?php echo $event->ev_location_name; ?>">
         <div class="row">
             <div class="col-xs-8">
                 <label for="event_edit_strasse"><?php echo __('Straße', 'ticketmachine') ?></label>
-                <input id="event_edit_strasse" type="text" class="form-control" value="">
+                <input id="event_edit_strasse" type="text" class="form-control" value="<?php echo $event->street; ?>">
             </div>
             <div class="col-xs-4">
                 <label for="house_number"><?php echo __('Haus-Nr.', 'ticketmachine') ?></label>
-                <input id="event_edit_hausnr" type="text" class="form-control">
+                <input id="event_edit_hausnr" type="text" class="form-control" value="<?php echo $event->house_number; ?>">
             </div>
         </div>
         <div class="row">
             <div class="col-xs-4">
                 <label for="event_edit_plz"><?php echo __('PLZ', 'ticketmachine') ?></label>
-                <input id="event_edit_plz" type="text" class="form-control">
+                <input id="event_edit_plz" type="text" class="form-control" value="<?php echo $event->zip; ?>">
             </div>
             <div class="col-xs-8">
                 <label for="event_edit_ort"><?php echo __('Ort', 'ticketmachine') ?></label>
-                <input id="event_edit_ort" type="text" class="form-control">
+                <input id="event_edit_ort" type="text" class="form-control" value="<?php echo $event->city; ?>">
             </div>
         </div>
         <div class="row">
             <label for="event_edit_land"><?php echo __('Land', 'ticketmachine') ?></label>
-            <input id="event_edit_land" type="text" class="form-control">
+            <input id="event_edit_land" type="text" class="form-control" value="<?php echo $event->country; ?>">
         </div>
     </div>
 
@@ -61,7 +61,7 @@
             </div>
             <div class="col-xs-4">
                 <div class="input-group">
-                    <input type="text" class="form-control event_date hasDatepicker">
+                    <input type="text" class="form-control event_date hasDatepicker" value="<?php echo $event->entrytime; ?>">
                     <label for="event_time_entry_date" class="input-group-addon w50">
                         <i class="fa fa-calendar"></i>
                     </label>
@@ -82,7 +82,7 @@
             </div>
             <div class="col-xs-4">
                 <div class="input-group">
-                    <input type="text" class="form-control event_date hasDatepicker">
+                    <input type="text" class="form-control event_date hasDatepicker" value="<?php echo $event->ev_date; ?>">
                     <label for="event_time_start_date" class="input-group-addon w50">
                         <i class="fa fa-calendar"></i>
                     </label>
@@ -103,7 +103,7 @@
             </div>
             <div class="col-xs-4">
                 <div class="input-group">
-                    <input type="text" class="form-control event_date hasDatepicker">
+                    <input type="text" class="form-control event_date hasDatepicker" value="<?php echo $event->endtime; ?>">
                     <label for="event_edit_end_date" class="input-group-addon w50">
                         <i class="fa fa-calendar"></i>
                     </label>
