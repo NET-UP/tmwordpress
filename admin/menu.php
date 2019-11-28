5c05aefe9b7402ebfc7f2d79a5faf886e4557ba2
Billie Bell <billie.bell@net-up.de>
2019-11-28 11:24:31 +0100

    Event list API call test

--- admin/menu.php
+++ admin/menu.php
@@ -7,24 +7,14 @@
 			'TicketMachine',
 			"TicketMachine",
 			'manage_options',
-			'tm_settings',
+			'tm_events',
 			'tm_plugin_menu',
 			null,
 			96
 		);
-		add_submenu_page(
-			'tm_settings',
-			__('Einstellungen', 'ticketmachine'),
-			__('Einstellungen', 'ticketmachine'),
-			'manage_options',
-			'tm_settings',
-			'tm_settings_page',
-			null,
-			98
-		);
 		
 		add_submenu_page(
-			'tm_settings',
+			'tm_events',
 			__('Veranstaltungen', 'ticketmachine'),
 			__('Veranstaltungen', 'ticketmachine'),
 			'manage_options',
@@ -32,6 +22,16 @@
 			'tt_render_list_page',
 			null,
 			97
+		);
+		add_submenu_page(
+			'tm_events',
+			__('Einstellungen', 'ticketmachine'),
+			__('Einstellungen', 'ticketmachine'),
+			'manage_options',
+			'tm_settings',
+			'tm_settings_page',
+			null,
+			98
 		);
 	}
 	
