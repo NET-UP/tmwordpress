00396b6d1f6bbd0f937f34483d1c4f399b3745de
Schneider Tobias <schneidert@net-up.net>
2019-11-28 10:34:43 +0100

    eventspage test

--- admin/menu.php
+++ admin/menu.php
@@ -1,4 +1,6 @@
 <?php
+	include( plugin_dir_path( __FILE__ ) . 'pages/events.php');
+
 	function tm_admin_menu () {
 		
 		add_menu_page(
@@ -17,7 +19,7 @@
 			__('Veranstaltungen', 'ticketmachine'),
 			'manage_options',
 			'tm_events',
-			'tm_events_page',
+			'tt_render_list_page',
 			null,
 			98
 		);
@@ -34,15 +36,7 @@
 	}
 	
 	add_action('admin_menu', 'tm_admin_menu');
-	
-	function tt_add_menu_items(){
-		include( plugin_dir_path( __FILE__ ) . 'pages/events.php');
-		add_menu_page('Example Plugin List Table', 'List Table Example', 'activate_plugins', 'tt_list_test', 'tt_render_list_page');
-	} 
-	add_action('admin_menu', 'tt_add_menu_items');
 
-	function tm_events_page(){
-	}
 	function tm_settings_page(){
 		include( plugin_dir_path( __FILE__ ) . 'pages/settings.php');
 	}
