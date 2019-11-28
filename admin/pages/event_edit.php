d3a2c155dc7d942d9de20a54e63dcc3fed75a875
Schneider Tobias <schneidert@net-up.net>
2019-11-28 15:08:51 +0100

    edit event page

--- admin/pages/event_edit.php
+++ admin/pages/event_edit.php
@@ -6,9 +6,14 @@
 ?>
 
 <div class="wrap">
-    <h2>TicketMachine > 
+    <h2>TicketMachine > Veranstaltung bearbeiten</h2>  <!-- needs create/update -->
+    <input type="text" name="ev_name" size="30" value="" id="ev_name" spellcheck="true" autocomplete="off">
+    <?php 
+        $content = '';
+        $editor_id = 'mycustomeditor';
 
-    </h2>
+    wp_editor( $content, $editor_id );
+    ?>
 </div>
 <?php
     
