e61e32b21046bef88b3cad88f3ac1d3bb1af723e
Schneider Tobias <schneidert@net-up.net>
2019-11-28 11:30:35 +0100

    eventspage test

--- admin/pages/events.php
+++ admin/pages/events.php
@@ -57,7 +57,6 @@ License: GPL2
 if(!class_exists('WP_List_Table')){
     require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
-
 include(str_replace("/admin/pages", "", plugin_dir_path( __FILE__ )) . 'globals.php');
 
 
@@ -88,23 +87,76 @@ class TT_Example_List_Table extends WP_List_Table {
      * 
      * @var array 
      **************************************************************************/
-    
-    var $example_data = array();
-    
+	
+	var $tm_json = apiRequest($api->get_event_list); //get event list
+	print_r($tm_json);
+
+
+    var $example_data = array(
+            array(
+                'ID'        => 1,
+                'title'     => '300',
+                'rating'    => 'R',
+                'director'  => 'Zach Snyder'
+            ),
+            array(
+                'ID'        => 2,
+                'title'     => 'Eyes Wide Shut',
+                'rating'    => 'R',
+                'director'  => 'Stanley Kubrick'
+            ),
+            array(
+                'ID'        => 3,
+                'title'     => 'Moulin Rouge!',
+                'rating'    => 'PG-13',
+                'director'  => 'Baz Luhrman'
+            ),
+            array(
+                'ID'        => 4,
+                'title'     => 'Snow White',
+                'rating'    => 'G',
+                'director'  => 'Walt Disney'
+            ),
+            array(
+                'ID'        => 5,
+                'title'     => 'Super 8',
+                'rating'    => 'PG-13',
+                'director'  => 'JJ Abrams'
+            ),
+            array(
+                'ID'        => 6,
+                'title'     => 'The Fountain',
+                'rating'    => 'PG-13',
+                'director'  => 'Darren Aronofsky'
+            ),
+            array(
+                'ID'        => 7,
+                'title'     => 'Watchmen',
+                'rating'    => 'R',
+                'director'  => 'Zach Snyder'
+            ),
+            array(
+                'ID'        => 8,
+                'title'     => '2001',
+                'rating'    => 'G',
+                'director'  => 'Stanley Kubrick'
+            ),
+        );
+
+
     /** ************************************************************************
      * REQUIRED. Set up a constructor that references the parent constructor. We 
      * use the parent reference to set some default configs.
      ***************************************************************************/
     function __construct(){
         global $status, $page;
-
+    
         $tm_json = apiRequest($api->get_event_list);
-        $this->example_data = $tm_json['result'];
-        
+        print_r($tm_json);
         //Set parent defaults
         parent::__construct( array(
-            'singular'  => 'event',     //singular name of the listed records
-            'plural'    => 'events',    //plural name of the listed records
+            'singular'  => 'movie',     //singular name of the listed records
+            'plural'    => 'movies',    //plural name of the listed records
             'ajax'      => false        //does this table support ajax?
         ) );
         
@@ -134,11 +186,8 @@ class TT_Example_List_Table extends WP_List_Table {
      **************************************************************************/
     function column_default($item, $column_name){
         switch($column_name){
-            case 'tags':
-                return $item[$column_name];
-            case 'ev_date'
-                return $item[$column_name];
-            case 'endtime'
+            case 'rating':
+            case 'director':
                 return $item[$column_name];
             default:
                 return print_r($item,true); //Show the whole array for troubleshooting purposes
@@ -165,15 +214,15 @@ class TT_Example_List_Table extends WP_List_Table {
     function column_title($item){
         
         //Build row actions
-        //$actions = array(
-        //    'edit'      => sprintf('<a href="?page=%s&action=%s&movie=%s">Edit</a>',$_REQUEST['page'],'edit',$item['ID']),
-        //    'delete'    => sprintf('<a href="?page=%s&action=%s&movie=%s">Delete</a>',$_REQUEST['page'],'delete',$item['ID']),
-        //);
+        $actions = array(
+            'edit'      => sprintf('<a href="?page=%s&action=%s&movie=%s">Edit</a>',$_REQUEST['page'],'edit',$item['ID']),
+            'delete'    => sprintf('<a href="?page=%s&action=%s&movie=%s">Delete</a>',$_REQUEST['page'],'delete',$item['ID']),
+        );
         
         //Return the title contents
         return sprintf('%1$s <span style="color:silver">(id:%2$s)</span>%3$s',
-            /*$1%s*/ $item['ev_name'],
-            /*$2%s*/ $item['id'],
+            /*$1%s*/ $item['title'],
+            /*$2%s*/ $item['ID'],
             /*$3%s*/ $this->row_actions($actions)
         );
     }
@@ -192,7 +241,7 @@ class TT_Example_List_Table extends WP_List_Table {
         return sprintf(
             '<input type="checkbox" name="%1$s[]" value="%2$s" />',
             /*$1%s*/ $this->_args['singular'],  //Let's simply repurpose the table's singular label ("movie")
-            /*$2%s*/ $item['id']                //The value of the checkbox should be the record's id
+            /*$2%s*/ $item['ID']                //The value of the checkbox should be the record's id
         );
     }
 
@@ -213,10 +262,9 @@ class TT_Example_List_Table extends WP_List_Table {
     function get_columns(){
         $columns = array(
             'cb'        => '<input type="checkbox" />', //Render a checkbox instead of text
-            'ev_name'     => 'Title',
-            'tags'    => 'Schlagwörter',
-            'ev_date'  => 'Anfangsdatum',
-            'endtime' => 'Enddatum'
+            'title'     => 'Title',
+            'rating'    => 'Rating',
+            'director'  => 'Director'
         );
         return $columns;
     }
@@ -238,10 +286,9 @@ class TT_Example_List_Table extends WP_List_Table {
      **************************************************************************/
     function get_sortable_columns() {
         $sortable_columns = array(
-            'ev_name'     => array('ev_name',false),     //true means it's already sorted
-            'tags'    => array('tags',false),
-            'ev_date'  => array('ev_date',false),
-            'endtime' => array('endtime',false)
+            'title'     => array('title',false),     //true means it's already sorted
+            'rating'    => array('rating',false),
+            'director'  => array('director',false)
         );
         return $sortable_columns;
     }
@@ -261,12 +308,12 @@ class TT_Example_List_Table extends WP_List_Table {
      * 
      * @return array An associative array containing all the bulk actions: 'slugs'=>'Visible Titles'
      **************************************************************************/
-    //function get_bulk_actions() {
-    //   $actions = array(
-    //        'delete'    => 'Delete'
-    //    );
-    //   return $actions;
-    //}
+    function get_bulk_actions() {
+        $actions = array(
+            'delete'    => 'Delete'
+        );
+        return $actions;
+    }
 
 
     /** ************************************************************************
