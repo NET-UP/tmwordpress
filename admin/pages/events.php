50fcc1155f7c264af7f54f6ec723336e9a49e3f8
Schneider Tobias <schneidert@net-up.net>
2019-11-28 10:45:46 +0100

    eventspage test

--- admin/pages/events.php
+++ admin/pages/events.php
@@ -87,6 +87,8 @@ class TT_Example_List_Table extends WP_List_Table {
      * 
      * @var array 
      **************************************************************************/
+	$tm_json = apiRequest($api->get_event_list); //get event list
+	print_r($tm_json);
     var $example_data = array(
             array(
                 'ID'        => 1,
