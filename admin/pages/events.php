a625551707c819351ff3a14d9fd2fb2cc6f8c636
Schneider Tobias <schneidert@net-up.net>
2019-11-28 12:54:44 +0100

    added edit and delete

--- admin/pages/events.php
+++ admin/pages/events.php
@@ -182,7 +182,7 @@ class TT_Example_List_Table extends WP_List_Table {
         return sprintf(
             '<input type="checkbox" name="%1$s[]" value="%2$s" />',
             /*$1%s*/ $this->_args['singular'],  //Let's simply repurpose the table's singular label ("movie")
-            /*$2%s*/ $item['ID']                //The value of the checkbox should be the record's id
+            /*$2%s*/ $item['id']                //The value of the checkbox should be the record's id
         );
     }
 
