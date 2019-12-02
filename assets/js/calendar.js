077142501a8a00c5f909bf97dc826c918099b71a
Billie Bell <billie.bell@net-up.de>
2019-12-02 12:21:44 +0100

    bug test

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -3,8 +3,10 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
+$("#tm_spinner").show();
 $.getJSON('/wp-content/plugins/ticketmachine/event.php' + location.search).success(function(data) {
 
+	$("#tm_spinner").hide();
 	var events_array = data;
 
 	var calendarEl = document.getElementById('calendar');
