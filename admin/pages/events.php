3e5058f4a50d5480d3289ada10f3ba30d54aeb3f
Schneider Tobias <schneidert@net-up.net>
2019-12-02 13:30:21 +0100

    added class for header

--- admin/pages/events.php
+++ admin/pages/events.php
@@ -447,9 +447,7 @@ function tt_render_list_page(){
         
         ?>
         <div class="wrap">
-            
-            <div id="icon-users" class="icon32"><br/></div>
-            <h1>TicketMachine > <?php echo __('Veranstaltungen'); ?></h1>
+            <h1 class="wp-heading-inline">TicketMachine > <?php echo __('Veranstaltungen'); ?></h1>
             <a href="?page=tm_events&action=edit" class="page-title-action">Hinzufügen</a>
             <hr class="wp-header-end">
             <!-- Forms are NOT created automatically, so you need to wrap the table in one to use features like bulk actions -->
