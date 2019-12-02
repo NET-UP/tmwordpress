5fa3c89199effa990509b2f4cea3f40a1c2d2a8c
Billie Bell <billie.bell@net-up.de>
2019-12-02 16:17:00 +0100

    bug test

--- ticketmachine.php
+++ ticketmachine.php
@@ -73,7 +73,9 @@
     if(is_admin()){
         include_once( plugin_dir_path( __FILE__ ) . 'admin/admin.php');
 		wp_enqueue_style( 'admin_CSS', plugins_url('admin/assets/css/style.css', __FILE__ ) );
-		wp_enqueue_style( 'admin_grid_CSS', plugins_url('admin/assets/css/grid.min.css', __FILE__ ) );
+        wp_enqueue_style( 'admin_grid_CSS', plugins_url('admin/assets/css/grid.min.css', __FILE__ ) );
+        wp_enqueue_style( 'jquery-ui-datepicker-style' , '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css');
+        wp_enqueue_script( 'jquery-ui-datepicker' );
     }
 	
     register_activation_hook(__FILE__, 'tm_activate');
