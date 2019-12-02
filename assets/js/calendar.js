2f8feef0cc06a3c1b68e90e46422e7ddb418bf06
Billie Bell <billie.bell@net-up.de>
2019-12-02 12:06:22 +0100

    bug test

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -8,62 +8,58 @@ $.getJSON('/wp-content/plugins/ticketmachine/event.php' + location.search).succe
 	var events_array = data;
 	console.log(data);
 
+	console.log("xd");
+	var calendarEl = document.getElementById('calendar');
 
-	document.addEventListener('DOMContentLoaded', function() {
-		console.log("xd");
-		var calendarEl = document.getElementById('calendar');
-
-		var calendar = new FullCalendar.Calendar(calendarEl, {
-			plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap' ],
-			themeSystem: 'bootstrap',
-			titleFormat: {
-				month: 'short',
-				year: 'numeric'
-			},
-			views: {
-				listMonth: { noEventsMessage: "Für diesen Monat sind keine Veranstaltungen verfügbar" },
-				dayGridMonth: { }
-			},
-			defaultView: (function () { if (jQuery(window).width() <= 768) { return defaultView = 'listMonth'; } else { return defaultView = 'dayGridMonth'; } })(),
-			header: false,
-			weekNumbers: false,
-			locale: "de",
-			height: "auto",
-			firstDay: 1,
-			eventLimit: false, // allow "more" link when too many events
-			events: events_array,
-			loading: function(bool) {
-				document.getElementById('loading').style.display =
-				bool ? 'block' : 'none';
-			}
-		});
-		
-		calendar.render();
-		
-		var view = calendar.view;
-		
-		jQuery("#calendar-title").html(view.title);
-		
-		// Next/Prev buttons
-		jQuery("#calendar-next").on('click', function() {
-			var view = calendar.view;
-			calendar.next();
-			jQuery("#calendar-title").html(view.title);
-		});
-		jQuery("#calendar-prev").on('click', function() {
-			var view = calendar.view;
-			calendar.prev();
-			jQuery("#calendar-title").html(view.title);
-		});
-		jQuery(window).on('resize', function() {
-			if(jQuery(window).width() <= 768){
-				calendar.changeView('listMonth');
-			}else{
-				calendar.changeView('dayGridMonth');
-				
-			}
-		});
+	var calendar = new FullCalendar.Calendar(calendarEl, {
+		plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list', 'bootstrap' ],
+		themeSystem: 'bootstrap',
+		titleFormat: {
+			month: 'short',
+			year: 'numeric'
+		},
+		views: {
+			listMonth: { noEventsMessage: "Für diesen Monat sind keine Veranstaltungen verfügbar" },
+			dayGridMonth: { }
+		},
+		defaultView: (function () { if (jQuery(window).width() <= 768) { return defaultView = 'listMonth'; } else { return defaultView = 'dayGridMonth'; } })(),
+		header: false,
+		weekNumbers: false,
+		locale: "de",
+		height: "auto",
+		firstDay: 1,
+		eventLimit: false, // allow "more" link when too many events
+		events: events_array,
+		loading: function(bool) {
+			document.getElementById('loading').style.display =
+			bool ? 'block' : 'none';
+		}
+	});
 	
+	calendar.render();
+	
+	var view = calendar.view;
+	
+	jQuery("#calendar-title").html(view.title);
+	
+	// Next/Prev buttons
+	jQuery("#calendar-next").on('click', function() {
+		var view = calendar.view;
+		calendar.next();
+		jQuery("#calendar-title").html(view.title);
+	});
+	jQuery("#calendar-prev").on('click', function() {
+		var view = calendar.view;
+		calendar.prev();
+		jQuery("#calendar-title").html(view.title);
+	});
+	jQuery(window).on('resize', function() {
+		if(jQuery(window).width() <= 768){
+			calendar.changeView('listMonth');
+		}else{
+			calendar.changeView('dayGridMonth');
+			
+		}
 	});
 
 });
