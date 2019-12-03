f6688559806f14f4c7d9bee0466c6a5ff8b21eb7
Billie Bell <billie.bell@net-up.de>
2019-12-03 16:02:56 +0100

    datetimepicker

--- admin/admin.php
+++ admin/admin.php
@@ -5,7 +5,7 @@
     wp_enqueue_script( 'moment_JS', plugins_url('assets/js/ext/moment-with-locales.js', __FILE__ ) );
     wp_enqueue_script( 'bootstrap3_JS', plugins_url('assets/js/ext/bootstrap.min.js', __FILE__ ) );
     wp_enqueue_script( 'datetimepicker_JS', plugins_url('assets/js/ext/bootstrap-datetimepicker.min.js', __FILE__ ) );
-    wp_enqueue_script( 'datetimepicker_JS', plugins_url('assets/js/ext/bootstrap-tag.min.js', __FILE__ ) );
+    wp_enqueue_script( 'taginput_JS', plugins_url('assets/js/ext/bootstrap-tag.min.js', __FILE__ ) );
 
     include( plugin_dir_path( __FILE__ ) . 'menu.php');
 
