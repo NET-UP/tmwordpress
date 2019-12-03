4da62914820399e236f5e1ed2c4e8b66b3ff49e6
Billie Bell <billie.bell@net-up.de>
2019-12-03 17:16:49 +0100

    Save event

--- admin/pages/event_save.php
+++ admin/pages/event_save.php
@@ -2,6 +2,9 @@
     if(!isset($_POST['shown'])) {
         $_POST['shown'] = 0;
     }
+    if(isset($_POST['tags'])) {
+        $_POST['tags'] = explode(",", $_POST['tags']);
+    }
     print_r($_POST); 
 
 ?>
