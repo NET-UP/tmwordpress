5f50ce194107012326eefe68b964dc92cc30f377
Billie Bell <billie.bell@net-up.de>
2019-12-03 11:05:49 +0100

    datetimepicker

--- admin/admin.php
+++ admin/admin.php
@@ -1,6 +1,6 @@
 <?php
-    wp_enqueue_style( 'admin_CSS', plugins_url('admin/assets/css/style.css', __FILE__ ) );
-    wp_enqueue_style( 'admin_grid_CSS', plugins_url('admin/assets/css/grid.min.css', __FILE__ ) );
+    wp_enqueue_style( 'admin_CSS', plugins_url('assets/css/style.css', __FILE__ ) );
+    wp_enqueue_style( 'admin_grid_CSS', plugins_url('assets/css/grid.min.css', __FILE__ ) );
 
     include( plugin_dir_path( __FILE__ ) . 'menu.php');
 
