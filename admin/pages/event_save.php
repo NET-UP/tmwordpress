c38482b021124bc27115a49fecc8d834d20d1ed3
Billie Bell <billie.bell@net-up.de>
2019-12-03 18:04:09 +0100

    Save event

--- admin/pages/event_save.php
+++ admin/pages/event_save.php
@@ -8,13 +8,13 @@
         $_POST['tags'] = explode(",", $_POST['tags']);
     }
     if(isset($_POST['entrytime'])) {
-        $_POST['entrytime'] = date(DATE_ISO8601, strtotime($_POST['entrytime']['date'] . $_POST['entrytime']['time']));
+        $_POST['entrytime'] = new DateTime(strtotime($_POST['entrytime']['date'] . $_POST['entrytime']['time']));
     }
     if(isset($_POST['ev_date'])) {
-        $_POST['ev_date'] = date(DATE_ISO8601, strtotime($_POST['ev_date']['date'] . $_POST['ev_date']['time']));
+        $_POST['ev_date'] = new DateTime(strtotime($_POST['ev_date']['date'] . $_POST['ev_date']['time']));
     }
     if(isset($_POST['endtime'])) {
-        $_POST['endtime'] = date(DATE_ISO8601, strtotime($_POST['endtime']['date'] . $_POST['endtime']['time']));
+        $_POST['endtime'] = new DateTime(strtotime($_POST['endtime']['date'] . $_POST['endtime']['time']));
     }
     
     #$tm_json = apiRequest($api->get_single_event, TRUE);
