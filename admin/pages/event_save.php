9545f097431733c6611932eda5541fd887f92207
Billie Bell <billie.bell@net-up.de>
2019-12-03 18:06:44 +0100

    Save event

--- admin/pages/event_save.php
+++ admin/pages/event_save.php
@@ -8,16 +8,16 @@
         $_POST['tags'] = explode(",", $_POST['tags']);
     }
     if(isset($_POST['entrytime'])) {
-        $_POST['entrytime'] = new DateTime(strtotime($_POST['entrytime']['date'] . $_POST['entrytime']['time']));
-        $_POST['entrytime'] = $_POST['entrytime']->format(DateTime::ISO8601);
+        $entrytime= new DateTime(strtotime($_POST['entrytime']['date'] . $_POST['entrytime']['time']));
+        $_POST['entrytime'] = $entrytime->format(DateTime::ISO8601);
     }
     if(isset($_POST['ev_date'])) {
-        $_POST['ev_date'] = new DateTime(strtotime($_POST['ev_date']['date'] . $_POST['ev_date']['time']));
-        $_POST['ev_date'] = $_POST['ev_date']->format(DateTime::ISO8601);
+        $ev_date = new DateTime(strtotime($_POST['ev_date']['date'] . $_POST['ev_date']['time']));
+        $_POST['ev_date'] = $ev_date->format(DateTime::ISO8601);
     }
     if(isset($_POST['endtime'])) {
-        $_POST['endtime'] = new DateTime(strtotime($_POST['endtime']['date'] . $_POST['endtime']['time']));
-        $_POST['endtime'] = $_POST['endtime']->format(DateTime::ISO8601);
+        $endtime = new DateTime(strtotime($_POST['endtime']['date'] . $_POST['endtime']['time']));
+        $_POST['endtime'] = $endtime->format(DateTime::ISO8601);
     }
     
     #$tm_json = apiRequest($api->get_single_event, TRUE);
