ae00fdd21f8520fcfd8a2bf44380c6f5503bb251
Billie Bell <billie.bell@net-up.de>
2019-12-03 17:21:46 +0100

    Save event

--- admin/pages/event_save.php
+++ admin/pages/event_save.php
@@ -5,6 +5,10 @@
     if(isset($_POST['tags'])) {
         $_POST['tags'] = explode(",", $_POST['tags']);
     }
+    if(isset($_POST['entrytime'])) {
+        $entrytime = new DateTime($_POST['entrytime']['date'] . $_POST['entrytime']['time']);
+        $_POST['entrytime'] = $entrytime->format(DateTime::ATOM);
+    }
     print_r($_POST); 
 
 ?>
