d08ce14f2596cad6d538a5c63f42a124c9b20757
Billie Bell <billie.bell@net-up.de>
2019-12-03 17:35:37 +0100

    Save event

--- admin/pages/event_save.php
+++ admin/pages/event_save.php
@@ -6,16 +6,13 @@
         $_POST['tags'] = explode(",", $_POST['tags']);
     }
     if(isset($_POST['entrytime'])) {
-        $entrytime = new DateTime($_POST['entrytime']['date'] . $_POST['entrytime']['time']);
-        $_POST['entrytime'] = $entrytime->format(DateTime::ATOM);
+        $_POST['entrytime'] = date(DATE_ISO8601, $_POST['entrytime']['date'] . $_POST['entrytime']['time']);
     }
     if(isset($_POST['ev_date'])) {
-        $ev_date = new DateTime($_POST['ev_date']['date'] . $_POST['ev_date']['time']);
-        $_POST['ev_date'] = $ev_date->format(DateTime::ATOM);
+        $_POST['ev_date'] = date(DATE_ISO8601, $_POST['ev_date']['date'] . $_POST['ev_date']['time']);
     }
     if(isset($_POST['endtime'])) {
-        $endtime = new DateTime($_POST['endtime']['date'] . $_POST['endtime']['time']);
-        $_POST['endtime'] = $endtime->format(DateTime::ATOM);
+        $_POST['endtime'] = date(DATE_ISO8601, $_POST['endtime']['date'] . $_POST['endtime']['time']);
     }
 ?>
 
