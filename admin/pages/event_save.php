64b46ed151ebd5693fa515992581489652616dd2
Billie Bell <billie.bell@net-up.de>
2019-12-03 18:05:25 +0100

    Save event

--- admin/pages/event_save.php
+++ admin/pages/event_save.php
@@ -9,12 +9,15 @@
     }
     if(isset($_POST['entrytime'])) {
         $_POST['entrytime'] = new DateTime(strtotime($_POST['entrytime']['date'] . $_POST['entrytime']['time']));
+        $_POST['entrytime'] = $_POST['entrytime']->format(DateTime::ISO8601);
     }
     if(isset($_POST['ev_date'])) {
         $_POST['ev_date'] = new DateTime(strtotime($_POST['ev_date']['date'] . $_POST['ev_date']['time']));
+        $_POST['ev_date'] = $_POST['ev_date']->format(DateTime::ISO8601);
     }
     if(isset($_POST['endtime'])) {
         $_POST['endtime'] = new DateTime(strtotime($_POST['endtime']['date'] . $_POST['endtime']['time']));
+        $_POST['endtime'] = $_POST['endtime']->format(DateTime::ISO8601);
     }
     
     #$tm_json = apiRequest($api->get_single_event, TRUE);
