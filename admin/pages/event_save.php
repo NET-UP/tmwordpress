38bb4df99acba6520e16ef233827000a339a22b4
Billie Bell <billie.bell@net-up.de>
2019-12-04 10:01:05 +0100

    datetimepicker

--- admin/pages/event_save.php
+++ admin/pages/event_save.php
@@ -27,3 +27,10 @@
     $post_json = json_encode($_POST);
     
     $tm_json = apiRequest($api->get_single_event_no_categories, $post_json, "POST");?>
+
+<pre>
+    <?php print_r($post_json); ?>
+</pre>
+<pre>
+    <?php print_r($tm_json); ?>
+</pre>
