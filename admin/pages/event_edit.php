4f0547272a20f3b4574b36e576094af381d4663b
Billie Bell <billie.bell@net-up.de>
2019-12-04 10:19:35 +0100

    datetimepicker

--- admin/pages/event_edit.php
+++ admin/pages/event_edit.php
@@ -16,6 +16,7 @@
     ?>
     <form name="event" action="?page=tm_events&action=save&id=<?php echo $_GET['id']; ?>" method="post" id="event">
         <input type="hidden" name="organizer_id" value="<?php echo $event->organizer_id; ?>">
+        <input type="hidden" name="og_abbreviation" value="<?php echo $event->og_abbreviation; ?>">
         
         <?php if($event->id > 0){ ?>
             <input type="hidden" name="id" value="<?php echo $event->id; ?>">
