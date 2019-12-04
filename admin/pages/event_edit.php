789966a22ae9b51513c840081126355f225ab807
Billie Bell <billie.bell@net-up.de>
2019-12-04 17:24:25 +0100

    alt-text

--- admin/pages/event_edit.php
+++ admin/pages/event_edit.php
@@ -93,7 +93,7 @@
                             </h2>
                             <div class="inside">
                                 <div class='image-preview-wrapper'>
-                                    <img id='image-preview' src='<?php echo $event->event_img_url; ?>' width='100' height='100' style='max-height: 100px; width: 100px;'>
+                                    <img id='image-preview' src='<?php echo $event->event_img_url; ?>' width='100' height='100' style='max-height: 500px; width: 100%;'>
                                 </div>
                                 <input id="upload_image_button" type="button" class="button" value="<?php _e( 'Bild festlegen' ); ?>" />
                                 <input type='hidden' name='event_img_url' id='image_attachment_id' value='<?php echo $event->event_img_url; ?>'>
