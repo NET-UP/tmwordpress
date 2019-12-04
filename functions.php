dd5c5ca8a630bd4f0f2ed8d930511ce796e7aef4
Billie Bell <billie.bell@net-up.de>
2019-12-04 09:50:29 +0100

    datetimepicker

--- functions.php
+++ functions.php
@@ -1,66 +1,29 @@
 <?php
-	#function apiRequest($url, $post=FALSE, $headers=array()) {
-	#  $ch = curl_init($url);
-	#  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	#  $headers = [];
-	# 
-	#  if($post)
-	#	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-#
-	#  if(strlen($post) > 0 || is_object($post)) {
-	#	curl_setopt($ch, CURLOPT_POST, 1);
-	#	$headers[] = 'Content-Type: application/json';
-	#  }else{
-	#	$headers[] = 'Accept: application/json';
-	#  }
-#
-	#  $headers[] = 'User-Agent: https://www.ticketmachine.de/';
-	# 
-	#  if(isset($_SESSION['access_token']))
-	#	$headers[] = 'Authorization: Bearer '.$_SESSION['access_token'];
-	# 
-	#  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	# 
-	#  $response = curl_exec($ch);
-	#  return json_decode($response, true);
-	#  
-	#}
+	function apiRequest($url, $post=FALSE, $method, $headers=array()) {
+	  $ch = curl_init($url);
+	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	  $headers = [];
+	 
+	  if($post)
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
 
-	function apiRequest($url, $data=FALSE, $method){
-		$curl = curl_init($url);
-		$headers = [];
-	
-		switch ($method){
-			case "POST":
-				curl_setopt($curl, CURLOPT_POST, 1);
-				if ($data)
-					curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-				break;
-			case "PUT":
-				curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-				if ($data)
-					curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
-				break;
-			default:
-				if ($data)
-					$url = sprintf("%s?%s", $url, http_build_query($data));
-		}
+	  if($method == "POST") {
+		curl_setopt($ch, CURLOPT_POST, 1);
 		$headers[] = 'Content-Type: application/json';
-		$headers[] = 'User-Agent: https://www.ticketmachine.de/';
+	  }else{
+		$headers[] = 'Accept: application/json';
+	  }
 
-		if(isset($_SESSION['access_token']))
-			$headers[] = 'Authorization: Bearer '.$_SESSION['access_token'];
-	
-		// OPTIONS:
-		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-	
-		// EXECUTE:
-		$result = curl_exec($curl);
-		if(!$result){die("Connection Failure");}
-		return json_decode($result, true);
+	  $headers[] = 'User-Agent: https://www.ticketmachine.de/';
+	 
+	  if(isset($_SESSION['access_token']))
+		$headers[] = 'Authorization: Bearer '.$_SESSION['access_token'];
+	 
+	  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	 
+	  $response = curl_exec($ch);
+	  return json_decode($response, true);
+	  
 	}
 	
 	switch ($globals->lang) {
