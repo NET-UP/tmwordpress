121c0fbc2f66b2549785733ecaa12bd674a8d5f8
Billie Bell <billie.bell@net-up.de>
2019-12-04 11:47:32 +0100

    Save event

--- admin/pages/actions/event_save.php
+++ admin/pages/actions/event_save.php
@@ -32,6 +32,8 @@
     $response = (object)$tm_json['model_error'][0];
 ?>
 
+<pre><?php print_r($_POST); ?></pre>
+
 <?php if(strlen($response->error_code) > 0){ ?>
     <div class="notice notice-error is-dismissable">
         <p><?php echo __($response->error_message); ?></p>
