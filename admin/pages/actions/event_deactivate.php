8d6619b4f9bd9983214614ccd1526ee555ece3bd
Billie Bell <billie.bell@net-up.de>
2019-12-05 09:48:31 +0100

    Delete/Copy event

--- admin/pages/actions/event_deactivate.php
+++ admin/pages/actions/event_deactivate.php
@@ -9,34 +9,6 @@
         $_POST['organizer_id'] = $globals->organizer_id;
         $_POST['state']['shown'] = 0;
         
-        if(!isset($_POST['approved'])) {
-            $_POST['approved'] = 0;
-        }
-        if(isset($_POST['tags'])) {
-            $_POST['tags'] = explode(",", $_POST['tags']);
-        }
-        if(isset($_POST['entrytime'])) {
-            $_POST['entrytime'] = date(DATE_ISO8601, strtotime($_POST['entrytime']['date'] . $_POST['entrytime']['time']));
-        }
-        if(isset($_POST['ev_date'])) {
-            $_POST['ev_date'] = date(DATE_ISO8601, strtotime($_POST['ev_date']['date'] . $_POST['ev_date']['time']));
-        }
-        if(isset($_POST['endtime'])) {
-            $_POST['endtime'] = date(DATE_ISO8601, strtotime($_POST['endtime']['date'] . $_POST['endtime']['time']));
-        }
-    
-        if($_POST['id'] > 0) {
-            $_POST['id'] = (int)$_POST['id'];
-        }
-        if(isset($_POST['vat_id'])){
-            $_POST['vat_id'] = 1;
-        }
-        $_POST['organizer_id'] = (int)$_POST['organizer_id'];
-        $_POST['approved'] = (int)$_POST['approved'];
-        $_POST['state']['shown'] = (int)$_POST['state']['shown'];
-        $_POST['rules']['sale_active'] = (int)$_POST['rules']['sale_active'];
-        $_POST['vat_id'] = (int)$_POST['vat_id'];
-        
         $post_json = json_encode($_POST);
 
         $tm_json = apiRequest($api->get_single_event_no_categories, $post_json, "POST");
