f1f2c27115fb0965c8b46f4b066a47dc687b7dc9
Billie Bell <billie.bell@net-up.de>
2019-12-05 10:31:20 +0100

    Delete/Copy event

--- admin/pages/events.php
+++ admin/pages/events.php
@@ -422,7 +422,7 @@ function tt_render_list_page(){
         if ( $_GET['action'] == "save" && isset($_GET['id']) && $_POST ) {
             include "actions/event_save.php";
         } elseif ( $_GET['action'] == "publish" && isset($_GET['id']) || $_GET['action'] == "deactivate" && isset($_GET['id']) ) {
-            include "actions/event_shown_toggle.php";
+            include "actions/event_toggle.php";
         } elseif ( $_GET['action'] == "delete" && isset($_GET['id']) ) {
             include "actions/event_remove.php";
         } elseif ( $_GET['action'] == "copy" && isset($_GET['id']) ){
