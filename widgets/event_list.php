8f1ad985960bcfa9da6901b9f65d2a3d7b39ff11
Billie Bell <billie.bell@net-up.de>
2019-12-10 16:04:34 +0100

    Edit event tag bugfix

--- widgets/event_list.php
+++ widgets/event_list.php
@@ -2,7 +2,7 @@
 
     function tm_widget_event_list ( $atts, $globals, $api ) {
 		$tm_json = apiRequest($api->get_event_list);
-		$events = (object)$tm_json['result'];
+		$events = $tm_json['result'];
 
         $tm_output = json_decode($events);
 
