6019be9eaecb892480e85a171186f91e90cb0421
Schneider Tobias <schneidert@net-up.net>
2019-12-10 14:56:18 +0100

    google calender test

--- partials/_event_page_tickets.php
+++ partials/_event_page_tickets.php
@@ -7,13 +7,13 @@
             
         }
 
-        $google_calendar_url =   'https://calendar.google.com/calendar/r/eventedit?
-                                &text='. urlencode($event->ev_name) .'
-                                &dates='. date("Ymd", strtotime($event->ev_date)) .'T'. date("His", strtotime($event->ev_date)) . 'Z' . '/'. date("Ymd", strtotime($event->endtime)).'T'. date("His", strtotime($event->endtime)) . 'Z
-                                &details='. urlencode(wp_strip_all_tags($event->ev_description)) .'
-                                &location='. urlencode($event->ev_location_name) .'
-                                sf=true&
-                                output=xml';
+$google_calendar_url ='https://calendar.google.com/calendar/r/eventedit?
+&text='. urlencode($event->ev_name) .'
+&dates='. date("Ymd", strtotime($event->ev_date)) .'T'. date("His", strtotime($event->ev_date)) . 'Z' . '/'. date("Ymd", strtotime($event->endtime)).'T'. date("His", strtotime($event->endtime)) . 'Z
+&details='. urlencode(wp_strip_all_tags($event->ev_description)) .'
+&location='. urlencode($event->ev_location_name) .'
+sf=true&
+output=xml';
 
         $tm_output .= '
                 <a class="btn btn-secondary px-3" href="/events">
