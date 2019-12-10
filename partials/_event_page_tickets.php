105c6669c248106f6e25f527a98a63feba89a2a4
Schneider Tobias <schneidert@net-up.net>
2019-12-10 13:33:05 +0100

    ical test

--- partials/_event_page_tickets.php
+++ partials/_event_page_tickets.php
@@ -3,19 +3,6 @@
     function tm_event_page_tickets ( $event, $globals ) {
 #<% unless @event.categories.select{|ec| ec.ec_price >= 0}.empty? %>
 #<% end %>
-        $ical = "BEGIN:VCALENDAR
-                VERSION:2.0
-                PRODID:-//hacksw/handcal//NONSGML v1.0//EN
-                BEGIN:VEVENT
-                UID:" . md5(uniqid(mt_rand(), true)) . "@yourhost.test
-                DTSTAMP:" . gmdate('Ymd').'T'. gmdate('His') . "Z
-                DTSTART:19970714T170000Z
-                DTEND:19970715T035959Z
-                SUMMARY:Bastille Day Party
-                END:VEVENT
-                END:VCALENDAR";
-        header('Content-type: text/calendar; charset=utf-8');
-        header('Content-Disposition: inline; filename=calendar.ics');
         if($event->state["prices_shown"] == true){
             
         }
@@ -23,7 +10,7 @@
                 <a class="btn btn-secondary px-3" href="/events">
                     <i class="fas fa-chevron-left"></i> &nbsp; Zurück
                 </a>
-                <div>'. $ical .'</div>';
+                <a href="' . plugin_dir_path( __FILE__ ) . '/includes/ical.php?id=' . $event->id . '"></a>';
         return $tm_output;
 	}
 ?>
