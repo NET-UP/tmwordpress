6f0fdedac84b14e11dc9dd93b070a0beadee730c
Billie Bell <billie.bell@net-up.de>
2019-12-10 15:05:00 +0100

    Edit event tag bugfix

--- partials/_event_page_tickets.php
+++ partials/_event_page_tickets.php
@@ -10,7 +10,7 @@
 $google_calendar_url ='https://calendar.google.com/calendar/r/eventedit?
 &text='. urlencode($event->ev_name) .'
 &dates='. date("Ymd", strtotime($event->ev_date)) .'T'. date("His", strtotime($event->ev_date)) . 'Z' . '/'. date("Ymd", strtotime($event->endtime)).'T'. date("His", strtotime($event->endtime)) . 'Z
-&details='. urlencode(wp_strip_all_tags($event->ev_description)) .'
+&details='. htmlentities(urlencode(wp_strip_all_tags($event->ev_description))) .'
 &location='. urlencode($event->ev_location_name) .'
 &sf=true
 &output=xml';
