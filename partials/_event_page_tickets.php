6f5f577a63ede5b757eb275ad3de8bf269c4c19c
Schneider Tobias <schneidert@net-up.net>
2019-12-10 14:45:48 +0100

    google calender test

--- partials/_event_page_tickets.php
+++ partials/_event_page_tickets.php
@@ -6,11 +6,24 @@
         if($event->state["prices_shown"] == true){
             
         }
+
+        $googe_calendar_url =   'http://www.google.com/calendar/render?
+                                action=TEMPLATE
+                                &text='. url_encode($event->ev_name) .'
+                                &dates='. date("Ymd", strtotime($event->ev_date)) .'T'. date("His", strtotime($event->ev_date)) . 'Z' . '/'. date("Ymd", strtotime($event->endtime)).'T'. date("His", strtotime($event->endtime)) . 'Z
+                                &details='. url_encode($event->ev_description) .'
+                                &location='. url_encode($event->ev_location_name) .'
+                                &trp=false
+                                &sprop=
+                                &sprop=name:';
+
         $tm_output .= '
                 <a class="btn btn-secondary px-3" href="/events">
                     <i class="fas fa-chevron-left"></i> &nbsp; Zurück
                 </a>
-                <a href="/wp-content/plugins/ticketmachine/includes/ical.php?id=' . $event->id . '"><i class="fas fa-calendar-alt" title="Als ical speichern"></i></a>';
+                <a href="/wp-content/plugins/ticketmachine/includes/ical.php?id=' . $event->id . '"><i class="fas fa-calendar-alt" title="Als ical speichern"></i></a>
+                <a href="' . $google_calendar_url . '">GOOGLE</a>';
+
         return $tm_output;
 	}
 ?>
