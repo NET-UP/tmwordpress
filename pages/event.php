2b2f01602bf79b9de31164784a45ce3f68fc142a
Schneider Tobias <schneidert@net-up.net>
2019-12-11 14:01:00 +0100

    add google map

--- pages/event.php
+++ pages/event.php
@@ -20,12 +20,12 @@
 				<div class="row">
 					<div class="col-12 col-lg-5 col-xl-6">';
 		$tm_output .= tm_event_page_information($event, $globals);
-		$tm_output .= tm_event_page_google($event, $globals);
 		$tm_output .= '
 					</div>
 					<div class="col-12 col-lg-7 col-xl-6">';
 		$tm_output .= tm_event_page_details($event, $globals);			
 		$tm_output .= tm_event_page_tickets($event, $globals);
+		$tm_output .= tm_event_page_google($event, $globals);
 		$tm_output .= '
 					</div>
 				</div>
--- partials/_event_page_google.php
+++ partials/_event_page_google.php
@@ -1,16 +1,16 @@
 <?php
     function tm_event_page_google($event, $globals) {
         $tm_output .= "<h1>My First Google Map</h1>
-        <iframe width='100%' height='100%' id='mapcanvas' src='https://maps.google.com/maps?q=London,%20United%20Kingdom&Roadmap&z=10&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'>
-            <div class='zxos8_gm'>
-                <a href='https://www.reconditioned-engines.co.uk'>https://www.reconditioned-engines.co.uk</a>
-            </div>
-            <div style='overflow:hidden;'>
-                <div id='gmap_canvas' style='height:100%;width:700px;'>
-                </div>
-                <div><small>Powered by <a href='https://www.embedgooglemap.co.uk'>Embed Google Map</a></small>
-            </div>
-        </iframe>";
+                        <iframe width='100%' height='300' id='mapcanvas' src='https://maps.google.com/maps?q=London,%20United%20Kingdom&Roadmap&z=10&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'>
+                            <div class='zxos8_gm'>
+                                <a href='https://www.reconditioned-engines.co.uk'>https://www.reconditioned-engines.co.uk</a>
+                            </div>
+                            <div style='overflow:hidden;'>
+                                <div id='gmap_canvas' style='height:100%;width:700px;'>
+                                </div>
+                                <div><small>Powered by <a href='https://www.embedgooglemap.co.uk'>Embed Google Map</a></small>
+                            </div>
+                        </iframe>";
         return $tm_output;
     }
 ?>
