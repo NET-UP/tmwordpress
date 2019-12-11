0ddc2e034f98f4abdf5b86ef494b2c1f3f83809f
Billie Bell <billie.bell@net-up.de>
2019-12-11 15:19:12 +0100

    Edit event tag bugfix

--- assets/js/ticketmachine.js
+++ assets/js/ticketmachine.js
@@ -1,4 +1,5 @@
 jQuery(document).on('click', '.allow-google-maps', function(){
     setCookie('allow_google_maps', 1)
-    location.reload();
+    var url = jQuery(this).data("embed");
+    $('.allow-google-maps-container').html("<iframe width='100%' height='300' id='mapcanvas' src='" + url + "' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'></iframe>");
 });
