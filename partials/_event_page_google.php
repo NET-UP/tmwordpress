38b73e94aab6e5ba8e2bb79fe255dbcf306cd705
Schneider Tobias <schneidert@net-up.net>
2019-12-11 13:47:36 +0100

    add google map

--- partials/_event_page_google.php
+++ partials/_event_page_google.php
@@ -1,17 +1,17 @@
-<h1>My First Google Map</h1>
-
-<div id="googleMap" style="width:100%;height:400px;"></div>
-
 <script>
-function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
+    function myMap() {
+        var mapProp= {
+            center:new google.maps.LatLng(51.508742,-0.120850),
+            zoom:5,
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+        }
 </script>
 
 <?php
-
+    function tm_event_page_google($event, $globals) {
+        $tm_output .= '<h1>My First Google Map</h1>
+                       <div id="googleMap" style="width:100%;height:400px;"></div>';
+        return $tm_output;
+    }
 ?>
