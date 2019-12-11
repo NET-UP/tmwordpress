4b8811e89819adea3380b1160132d78f8cf5fc8d
Billie Bell <billie.bell@net-up.de>
2019-12-11 16:03:18 +0100

    Edit event tag bugfix

--- partials/_event_page_information.php
+++ partials/_event_page_information.php
@@ -24,9 +24,7 @@
                     </div>
 
                     <div class="card-text mt-3" role="fe_nu_ev_des">
-                        <div class="max-height">
                             '. $event->ev_description .'
-                        </div>
                     </div>
 
                     <div class="card-meta">';
