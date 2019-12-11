ef9298afe518aeca57ddc7d9a3d1611d6da9b1c0
Billie Bell <billie.bell@net-up.de>
2019-12-11 12:02:54 +0100

    Edit event tag bugfix

--- widgets/event_list.php
+++ widgets/event_list.php
@@ -12,14 +12,14 @@
 
                 $tm_output .= '<li class="media">';
 
-                if($atts['show_image'] == 1){
+                if($atts['show_image'] > 0){
                     $tm_output .= '<div class="mr-3 media-img" style="background-image:url('. $event->event_img_url .')"></div>';
                 }
                                     
                     $tm_output .= '<div class="media-body">';
                     $tm_output .= '<h5 class="mt-0 mb-1">' . $event->ev_name . '</h5>';
 
-                    if($atts['show_description'] == 1){
+                    if($atts['show_description'] > 0){
                         if(!$atts['description_length']){
                             $atts['description_length'] = 15;
                         }
