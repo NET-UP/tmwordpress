53322d07825eccdf200863776120381bb3423bcd
Schneider Tobias <schneidert@net-up.net>
2019-12-11 12:23:21 +0100

    added event details

--- partials/_event_page_details.php
+++ partials/_event_page_details.php
@@ -5,13 +5,17 @@
                             <div class="row card-body position-relative">
                                 <div class="col-sm-6">
                                     <h3 class="d-inline-block">Details</h3>
-                                    <div> Beginn &nbsp;
-                                        <i class="far fa-calendar-alt tm-icon" aria-hidden="true"></i> &nbsp;'. date( "d. F", strtotime($event->ev_date) ) .'
+                                    <label>__("Beginn", "ticketmachine"):</label>
+                                    <div>
+                                        <i class="far fa-calendar-alt tm-icon" aria-hidden="true"></i> &nbsp;'. strftime( "%e. %B", strtotime($event->ev_date) ) .' 
+                                        <i class="far fa-clock tm-icon" aria-hidden="true"></i> &nbsp;'. strftime( "%H:%M", strtotime($event->ev_date) ) .'
+                                    </div>
+                                    <label>__("Ende", "ticketmachine"):</label>
+                                    <div>
+                                        <i class="far fa-calendar-alt tm-icon" aria-hidden="true"></i> &nbsp;'. strftime( "%e. %B", strtotime($event->endtime) ) .' 
+                                        <i class="far fa-clock tm-icon" aria-hidden="true"></i> &nbsp;'. strftime( "%H:%M", strtotime($event->endtime) ) .'
                                     </div> 
-                                    <div>Ende &nbsp;
-                                        <i class="far fa-calendar-alt tm-icon" aria-hidden="true"></i> &nbsp;'. date( "d. F", strtotime($event->endtime) ) .'
-                                    </div> 
-                                    <div>Eintritt: kostenlos</div>';       
+                                    <div>__("Eintritt", "ticketmachine"): __("kostenlos", "ticketmachine")</div>';       
             $tm_output .=       '</div>
                                 <div class="col-sm-6">
                                     <h3 class="d-inline-block">Veranstaltungsort</h3>
