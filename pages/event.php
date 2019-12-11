b178244ad4270d0759a3ca095a4aa0bc42b46536
Schneider Tobias <schneidert@net-up.net>
2019-12-11 14:14:11 +0100

    add google map

--- pages/event.php
+++ pages/event.php
@@ -20,7 +20,7 @@
 				<div class="row">
 					<div class="col-12 col-lg-5 col-xl-6">';
 		$tm_output .= tm_event_page_information($event, $globals);
-		$tm_output .= tm_event_page_action($event, $globals);
+		$tm_output .= tm_event_page_actions($event, $globals);
 		$tm_output .= '
 					</div>
 					<div class="col-12 col-lg-7 col-xl-6">';
