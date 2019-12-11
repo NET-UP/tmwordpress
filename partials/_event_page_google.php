5953b36789549a7d420c5122010c5dd4fe338e60
Billie Bell <billie.bell@net-up.de>
2019-12-11 15:16:44 +0100

    Edit event tag bugfix

--- partials/_event_page_google.php
+++ partials/_event_page_google.php
@@ -5,7 +5,7 @@
             $tm_output .= "<iframe width='100%' height='300' id='mapcanvas' src='https://maps.google.com/maps?q=". urlencode($event->event_location->street . ' ' . $event->event_location['house_number'] . ' ' . $event->event_location['zip'] . ' ' . $event->event_location['city'] . ' ' . $event->event_location['country'] ) ."&z=10&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'>
                             </iframe>";
         }else{
-            $tm_output .= "<div class='allow-google-maps-container'><div class='vertical-center text-center'><a class='allow-google-maps' href='#'>Google Maps erlauben</a></div></div>";
+            $tm_output .= "<div class='allow-google-maps-container'><div class='vertical-center text-center'><a class='allow-google-maps' href='#' data-embed='https://maps.google.com/maps?q=". urlencode($event->event_location->street . ' ' . $event->event_location['house_number'] . ' ' . $event->event_location['zip'] . ' ' . $event->event_location['city'] . ' ' . $event->event_location['country'] ) ."&z=10&ie=UTF8&iwloc=&output=embed'>Google Maps erlauben</a></div></div>";
         }
 
         return $tm_output;
