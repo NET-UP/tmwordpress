91695815ea071d988aa15d48252b2e16753003c0
Schneider Tobias <schneidert@net-up.net>
2019-12-11 14:50:03 +0100

    add google map overlay

--- partials/_event_page_google.php
+++ partials/_event_page_google.php
@@ -1,7 +1,13 @@
 <?php
     function tm_event_page_google($event, $globals) {
-        $tm_output .= "<iframe width='100%' height='300' id='mapcanvas' src='https://maps.google.com/maps?q=". urlencode($event->event_location->street . ' ' . $event->event_location['house_number'] . ' ' . $event->event_location['zip'] . ' ' . $event->event_location['city'] . ' ' . $event->event_location['country'] ) ."&z=10&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'>
+        $tm_output .= "<iframe width='100%' height='300' id='mapcanvas' src='' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'>
+                        </iframe>"
+        if () {
+            $tm_output .= "<iframe width='100%' height='300' id='mapcanvas' src='https://maps.google.com/maps?q=". urlencode($event->event_location->street . ' ' . $event->event_location['house_number'] . ' ' . $event->event_location['zip'] . ' ' . $event->event_location['city'] . ' ' . $event->event_location['country'] ) ."&z=10&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'>
                         </iframe>";
+ 
+        }
+        
         return $tm_output;
     }
 ?>
