40a15c803c2c3fa0d042dbf9d4836b06677e11db
Billie Bell <billie.bell@net-up.de>
2019-12-11 10:30:24 +0100

    Edit event tag bugfix

--- widgets/event_list.php
+++ widgets/event_list.php
@@ -5,14 +5,14 @@
         $params = $atts;
         $events = tmapi_events($params)->result;
         $tm_output .= '<div class="tm_widget_event_list">';
-            $tm_output .= '<ul class="list-group list-group-flush">';
+            $tm_output .= '<ul class="list-unstyled">';
 
             foreach($events as $event){
                 $event = (object)$event;
 
-                $tm_output .= '<li class="list-group-item">
+                $tm_output .= '<li class="media">
                                     <a href="#">
-                                        <div class="mr-3 media-img" style="background-image:url('. $event->event_img_url .')"></div>
+                                        <div class="mr-3 media-img"style="background-image:url('. $event->event_img_url .')"></div>
                                         <div class="media-body">
                                             <h5 class="mt-0 mb-1">' . $event->ev_name . '</h5>
                                         </div>
