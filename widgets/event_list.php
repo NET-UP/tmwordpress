5c5b058f40f12a809610f12802dac7f2667fb235
Billie Bell <billie.bell@net-up.de>
2019-12-11 09:10:11 +0100

    Edit event tag bugfix

--- widgets/event_list.php
+++ widgets/event_list.php
@@ -1,9 +1,10 @@
 <?php
 
     function tm_widget_event_list ( $atts, $globals, $api ) {
-		$events = tmapi_events($params);
 
-        foreach($events->result as $event){
+		$events = tmapi_events($params)->results;
+
+        foreach($events as $event){
             $tm_output .= $event->id;
         }
 
