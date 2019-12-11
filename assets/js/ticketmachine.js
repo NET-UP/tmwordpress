cee0185c92b6e883c1efbf4d52e2d3f5b7c2a14f
Schneider Tobias <schneidert@net-up.net>
2019-12-11 15:46:21 +0100

    add google map overlay

--- assets/js/ticketmachine.js
+++ assets/js/ticketmachine.js
@@ -1,7 +1,7 @@
 jQuery(document).on('click', '.allow-google-maps', function(){
-    setCookie('allow_google_maps', 1);
+    createCookie('allow_google_maps', 1);
     var url = jQuery(this).data("embed");
-    $('.allow-google-maps-container').html("<iframe width='100%' height='300' id='mapcanvas' src='" + url + "' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'></iframe>");
+    $('.allow-google-maps-container').html("<iframe width='100%' height='300' id='mapcanvas' src='" + url + "' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'></iframe><a class='disallow-google-maps float-right' href='#'>Google Maps nicht erlauben</a>");
 });
 
 jQuery(document).on('click', '.disallow-google-maps', function(){
