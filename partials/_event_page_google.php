29c0609ffa0cd1529cf8f1d8290a5712fb58bd47
Billie Bell <billie.bell@net-up.de>
2019-12-11 15:05:20 +0100

    Edit event tag bugfix

--- partials/_event_page_google.php
+++ partials/_event_page_google.php
@@ -1,7 +1,7 @@
 <?php
     function tm_event_page_google($event, $globals) {
         
-        if (isset($_COOKIE["lg"])){
+        if (isset($_COOKIE["allow-google-maps"])){
             $tm_output .= "<iframe width='100%' height='300' id='mapcanvas' src='https://maps.google.com/maps?q=". urlencode($event->event_location->street . ' ' . $event->event_location['house_number'] . ' ' . $event->event_location['zip'] . ' ' . $event->event_location['city'] . ' ' . $event->event_location['country'] ) ."&z=10&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'>
                             </iframe>";
         }else{
