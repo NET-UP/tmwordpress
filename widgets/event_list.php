8538f7dcd485a51343abaef775136a8df5092de4
Billie Bell <billie.bell@net-up.de>
2019-12-11 09:52:05 +0100

    Edit event tag bugfix

--- widgets/event_list.php
+++ widgets/event_list.php
@@ -12,7 +12,7 @@
 
                 $tm_output .= '<li class="media">
                                     <a href="#">
-                                        <div class="mr-3 media-img"></div>
+                                        <div class="mr-3 media-img" style="background-image:url('. $event->event_img_url .')"></div>
                                         <div class="media-body">
                                             <h5 class="mt-0 mb-1">' . $event->ev_name . '</h5>
                                         </div>
