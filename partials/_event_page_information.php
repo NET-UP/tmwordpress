2cf651083bef3f3d65b785c9790b6445529dc0ec
Billie Bell <billie.bell@net-up.de>
2019-12-11 16:46:36 +0100

    Edit event tag bugfix

--- partials/_event_page_information.php
+++ partials/_event_page_information.php
@@ -11,7 +11,7 @@
                 </h5>
             </div>
             <card class="card mb-3">
-                <div class="card-img-top" style="background-image:url('. $event->event_img_url .')">
+                <div class="card-img-top full" style="background-image:url('. $event->event_img_url .')">
                     <div class="badge badge-danger float-right mt-1 mr-2">'. $event->rules["badge"] .'</div>
                 </div>
                 <div class="card-body position-relative">
