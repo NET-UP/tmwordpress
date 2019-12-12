31b5fd3ee0de828c35c432f9227f0357c0729826
Billie Bell <billie.bell@net-up.de>
2019-12-12 10:32:45 +0100

    Edit event tag bugfix

--- partials/_event_page_information.php
+++ partials/_event_page_information.php
@@ -6,7 +6,7 @@
             <div class="title-height no-height-mobile">
                 <h5>
                     <span>
-                        <h3 class="d-inline-block" role="fe_nu_ev_name">' . $event->ev_name . '</h3>
+                        <h3 class="d-inline-block">' . $event->ev_name . '</h3>
                     </span>
                 </h5>
             </div>
@@ -17,13 +17,13 @@
                 <div class="card-body position-relative">
 
                     <div class="card-meta">
-                        <div class="card-meta-tag" role="fe_nu_ev_date"><i class="far fa-calendar-alt tm-icon" aria-hidden="true"></i> &nbsp;'. date( "d.m.Y", strtotime($event->ev_date) ) .'</div> 
-                        <div class="card-meta-tag" role="fe_nu_ev_time"><i class="far fa-clock tm-icon" aria-hidden="true"></i> &nbsp;'. date( "H:i", strtotime($event->ev_date) ) .'</div>
-                        <div class="card-meta-tag" role="fe_nu_ev_location"><i class="fas fa-map-marker-alt tm-icon"></i> &nbsp; <a aria-label="' . __("Veranstaltungsort: ") . $event->ev_location_name . '" href="' . $globals->map_query_url . urlencode($event->event_location->street . " " . $event->event_location["house_number"] . " " . $event->event_location["zip"] . " " . $event->event_location["city"] . " " . $event->event_location["country"] ) . '" target="_blank" title="' . __("Veranstaltungsort: ") . $event->ev_location_name . '">' . $event->ev_location_name . '</a> </div>
+                        <div class="card-meta-tag"><i class="far fa-calendar-alt tm-icon" aria-hidden="true"></i> &nbsp;'. date( "d.m.Y", strtotime($event->ev_date) ) .'</div> 
+                        <div class="card-meta-tag"><i class="far fa-clock tm-icon" aria-hidden="true"></i> &nbsp;'. date( "H:i", strtotime($event->ev_date) ) .'</div>
+                        <div class="card-meta-tag"><i class="fas fa-map-marker-alt tm-icon"></i> &nbsp; <a aria-label="' . __("Veranstaltungsort: ") . $event->ev_location_name . '" href="' . $globals->map_query_url . urlencode($event->event_location->street . " " . $event->event_location["house_number"] . " " . $event->event_location["zip"] . " " . $event->event_location["city"] . " " . $event->event_location["country"] ) . '" target="_blank" title="' . __("Veranstaltungsort: ") . $event->ev_location_name . '">' . $event->ev_location_name . '</a> </div>
                         
                     </div>
 
-                    <div class="card-text mt-3" role="fe_nu_ev_des">
+                    <div class="card-text mt-3">
                         '. $event->ev_description .'
                     </div>
 
@@ -35,8 +35,8 @@
 
                     <div class="card-meta">';
 
-                    foreach($event->tags as $tags) {
-                        $tm_output .= "<div class='card-meta-tag keyword'>". $tags ."</div>";
+                    foreach($event->tags as $tag) {
+                        $tm_output .= "<a href='/events?tag=" . urlencode($tag) . "' class='card-meta-tag keyword'>". $tag ."</a>";
                     }
         $tm_output .= '
                     </div>
