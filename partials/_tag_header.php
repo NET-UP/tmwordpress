f80f719434075a47998fec3b6413bdd588b1f05f
Schneider Tobias <schneidert@net-up.net>
2019-12-12 12:49:13 +0100

    bugfix for tags

--- partials/_tag_header.php
+++ partials/_tag_header.php
@@ -2,15 +2,17 @@
 
     function tm_tag_header ( $globals ) {
 
-        $tm_output = '<label class="mr-3 ml-1">' . __("Schlagwörter", "ticketmachine") . ':</label> <div class="card-meta-tag keyword">' . $globals->tag . ' <a class="ml-2" href="' . str_replace("tag=" . urldecode($globals->tag), "", $_SERVER[REQUEST_URI]) . '"><i class="fa fa-times"></i></a></div>';
+        $tm_output = '<label class="mr-3 ml-1">' . __("Schlagwörter", "ticketmachine") . ':</label> <div class="card-meta-tag keyword">' . $globals->tag . ' <a class="ml-2" href="' . str_replace("tag=" . urldecode($globals->tag), "", $_SERVER[QUERY_STRING]) . '"><i class="fa fa-times"></i></a></div>';
 
-        $a = $_SERVER[REQUEST_URI];
+        $a = $_SERVER[QUERY_STRING];
         $b = urlencode($globals->tag);
         #$b = rawurlencode("Fußball");
         if (strpos($a, $b)){
             print_r($b . " was found in " . $a);
+            print_r(mb_detect_encoding($a) . mb_detect_encoding($b));
         }else{
             print_r($b . " was NOT found in " . $a);
+            print_r(mb_detect_encoding($a) . mb_detect_encoding($b));
         }
         return $tm_output;
     }
