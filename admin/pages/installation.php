8c335f4834d3b574ad224f21cc6379b2eb6ccb31
Schneider Tobias <schneidert@net-up.net>
2019-12-12 17:09:28 +0100

    Installation

--- admin/pages/installation.php
+++ admin/pages/installation.php
@@ -13,11 +13,11 @@
                     <br>
                      benötigen wir Ihre E-Mail-Adresse.
                     </div>
-                    <input type="email" style="width: 100%;height: 35px;border-left-width: 4px;border-left-color: orange;" placeholder="E-Mail Adresse bitte eingeben">
+                    <input type="email" style="width: 100%;height: 35px;border-left-width: 4px;border-left-color: orange;margin: 15px 0px;" placeholder="E-Mail Adresse bitte eingeben">
                     <div style="font-size: 16px;">
-                        <label><input type="checkbox" style="border-color: orange;">Ich habe die <a href="#">Datenschutzerklärung</a> gelesen und akzeptiert.</label>
+                        <label><input type="checkbox" style="border-color: orange;">Ich habe die <a href="#" style="text-decoration-line: none;">Datenschutzerklärung</a> gelesen und akzeptiert.</label>
                     </div>
-                    <button>LOS GEHT'S!</button>
+                    <button style="width: 100%;background-color: orange;padding: 7px 0px;">LOS GEHT'S!<i class="fas fa-check" style="font-size: 20px;color: #FFF;float: right;margin-right: 0.5em;"></i></button>
                 </div>
             </div>
         </div>
