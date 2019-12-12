920be7f74232b39e349d77e4e07be63157797a0e
Schneider Tobias <schneidert@net-up.net>
2019-12-12 12:45:39 +0100

    bugfix for tags

--- partials/_tag_header.php
+++ partials/_tag_header.php
@@ -4,8 +4,8 @@
 
         $tm_output = '<label class="mr-3 ml-1">' . __("Schlagwörter", "ticketmachine") . ':</label> <div class="card-meta-tag keyword">' . $globals->tag . ' <a class="ml-2" href="' . str_replace("tag=" . urldecode($globals->tag), "", $_SERVER[REQUEST_URI]) . '"><i class="fa fa-times"></i></a></div>';
 
-        $a = rawurldecode($_SERVER[REQUEST_URI]);
-        $b = rawurldecode($globals->tag);
+        $a = $_SERVER[REQUEST_URI];
+        $b = $globals->tag;
         #$b = rawurlencode("Fußball");
         if (strpos($a, $b)){
             print_r($b . " was found in " . $a);
