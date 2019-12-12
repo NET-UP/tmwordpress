506865d8780a96be2345fd5765f73ec3962f5a2e
Billie Bell <billie.bell@net-up.de>
2019-12-12 13:24:28 +0100

    Edit event tag bugfix

--- partials/_tag_header.php
+++ partials/_tag_header.php
@@ -3,16 +3,16 @@
     function tm_tag_header ( $globals ) {
         
 
+        $a = urlencode(htmlentities($_SERVER[REQUEST_URI]));
+        $b = urlencode($globals->tag);
+
         $tm_output = '<label class="mr-3 ml-1">' . __("Schlagwörter", "ticketmachine") . ':</label> 
                       <div class="card-meta-tag keyword">' . $globals->tag . ' 
-                        <a class="ml-2" href="' . $_SERVER[REQUEST_URI] . '">
+                        <a class="ml-2" href="' . str_replace($b, "", $a) .'">
                             <i class="fa fa-times"></i>
                         </a>
                       </div>';
 
-        #$a = $_SERVER[REQUEST_URI];
-        $a = urlencode($globals->tag);
-        $b = urlencode(htmlentities("Fußball"));
         if (strpos($a, $b)){
             print_r($b . " was found in " . $a);
             #print_r(mb_detect_encoding($a) . mb_detect_encoding($b));
