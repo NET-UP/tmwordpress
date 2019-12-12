398a7674e947b00f9e5e436f97c7423a08d3937a
Schneider Tobias <schneidert@net-up.net>
2019-12-12 16:12:54 +0100

    Installation

--- admin/pages/installation.php
+++ admin/pages/installation.php
@@ -4,32 +4,16 @@
             <div class="row">
                 <div class="col-xs-12">
                     <img class="row" src="">
-                
-                
                     <div class="row">Vielen Dank, dass Sie mich installiert haben!</div>
-            
-                
                     <div class="row">Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
-                
-                
                     <i class="row"></i>
-                
-                
                     <div class="row">Alle Veranstaltungen werden über den TicketMachine Cloud Service <i></i> gespeichert.</div>
-                
-                
                     <i class="row"></i>
-                
-                
                     <div class="row">Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen), benötigen wir Ihre E-Mail-Adresse</div>
-                
-                
                     <input class="row" type="email">
-                
-                
-                    <input class="row" type="checkbox"><label>Ich habe die <a>Datenschutzerklärung</a>gelesen und akzeptiert.</label>
-                
-                
+                    <div class="row">
+                        <input type="checkbox"><label>Ich habe die <a>Datenschutzerklärung</a>gelesen und akzeptiert.</label>
+                    </div>
                     <button class="row">LOS GEHT'S!</button>
                 </div>
             </div>
