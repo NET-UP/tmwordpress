53f1819b63f18e0657dff57065c9169adb27cb49
Billie Bell <billie.bell@net-up.de>
2019-12-12 12:06:48 +0100

    alt-text

--- partials/_tag_header.php
+++ partials/_tag_header.php
@@ -2,7 +2,7 @@
 
     function tm_tag_header ( $globals ) {
 
-        $tm_output = '<label class="mr-3 ml-1">' . __("Schlagwörter", "ticketmachine") . ':</label> <div class="card-meta-tag keyword">' . $globals->tag . ' <a class="ml-2" href="' . str_replace("tag=" . htmlentities(urlencode($globals->tag)), "", $_SERVER[REQUEST_URI]) . '"><i class="fa fa-times"></i></a></div>';
+        $tm_output = '<label class="mr-3 ml-1">' . __("Schlagwörter", "ticketmachine") . ':</label> <div class="card-meta-tag keyword">' . $globals->tag . ' <a class="ml-2" href="' . urlencode(str_replace("tag=" . urldecode($globals->tag), "", $_SERVER[REQUEST_URI])) . '"><i class="fa fa-times"></i></a></div>';
 
         print_r(str_replace("tag=" . urldecode($globals->tag), "", $_SERVER[REQUEST_URI]));;
 
