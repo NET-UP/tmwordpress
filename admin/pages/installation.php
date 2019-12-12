7739a773fb5a18c02a80a1ccb519bd538e4a920c
Schneider Tobias <schneidert@net-up.net>
2019-12-12 16:11:21 +0100

    Installation

--- admin/pages/installation.php
+++ admin/pages/installation.php
@@ -2,34 +2,36 @@
     <div class="container">
         <div class="d-flex justify-content-center">
             <div class="row">
-                <img class="col-xs-12" src="">
-            </div>
-            <div class="row">
-                <div class="col-xs-12">Vielen Dank, dass Sie mich installiert haben!</div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12">Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
-            </div>
-            <div class="row">
-                <i class="col-xs-12"></i>
-            </div>
-            <div class="row">
-                <div class="col-xs-12">Alle Veranstaltungen werden über den TicketMachine Cloud Service <i></i> gespeichert.</div>
-            </div>
-            <div class="row">
-                <i class="col-xs-12"></i>
-            </div>
-            <div class="row">
-                <div class="col-xs-12">Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen), benötigen wir Ihre E-Mail-Adresse</div>
-            </div>
-            <div class="row">
-                <input class="col-xs-12" type="email">
-            </div>
-            <div class="row">
-                <input class="col-xs-12" type="checkbox"><label>Ich habe die <a>Datenschutzerklärung</a>gelesen und akzeptiert.</label>
-            </div>
-            <div class="row">
-                <button class="col-xs-12">LOS GEHT'S!</button>
+                <div class="col-xs-12">
+                    <img class="row" src="">
+                
+                
+                    <div class="row">Vielen Dank, dass Sie mich installiert haben!</div>
+            
+                
+                    <div class="row">Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
+                
+                
+                    <i class="row"></i>
+                
+                
+                    <div class="row">Alle Veranstaltungen werden über den TicketMachine Cloud Service <i></i> gespeichert.</div>
+                
+                
+                    <i class="row"></i>
+                
+                
+                    <div class="row">Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen), benötigen wir Ihre E-Mail-Adresse</div>
+                
+                
+                    <input class="row" type="email">
+                
+                
+                    <input class="row" type="checkbox"><label>Ich habe die <a>Datenschutzerklärung</a>gelesen und akzeptiert.</label>
+                
+                
+                    <button class="row">LOS GEHT'S!</button>
+                </div>
             </div>
         </div>
     </div>
