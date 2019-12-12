bd9536e45b86ad0c93b4a6ba6bd128ab15f27f35
Billie Bell <billie.bell@net-up.de>
2019-12-12 11:57:10 +0100

    Edit event tag bugfix

--- partials/_tag_header.php
+++ partials/_tag_header.php
@@ -2,7 +2,7 @@
 
     function tm_tag_header ( $globals ) {
 
-        $tm_output = '<label class="mr-3 ml-1">' . __("Schlagwörter", "ticketmachine") . ':</label> <div class="card-meta-tag keyword">' . $globals->tag . ' <a class="ml-2" href="' . str_replace("tag=" . $globals->tag, "", $_SERVER[REQUEST_URI]) . '"><i class="fa fa-times"></i></a></div>';
+        $tm_output = '<label class="mr-3 ml-1">' . __("Schlagwörter", "ticketmachine") . ':</label> <div class="card-meta-tag keyword">' . $globals->tag . ' <a class="ml-2" href="' . str_replace("tag=" . urlencode($globals->tag), "", $_SERVER[REQUEST_URI]) . '"><i class="fa fa-times"></i></a></div>';
 
         return $tm_output;
     }
