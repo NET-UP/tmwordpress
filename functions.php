71a7cc991dfbff3a0d2f6a825d4fd3b88328e247
Billie Bell <billie.bell@net-up.de>
2019-12-12 09:32:03 +0100

    Edit event tag bugfix

--- functions.php
+++ functions.php
@@ -67,7 +67,7 @@
 		}
 		
 		if($params->tag) {
-			$url .= "&tags.tag[eq]=" . $params->query;
+			$url .= "&tags[eq]=" . $params->query;
 		}
 
 		$events = apiRequest($url, $post, $method, $headers);
