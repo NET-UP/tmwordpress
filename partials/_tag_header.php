c6f6309b953ead31e9716e7c7f25722e3127356f
Billie Bell <billie.bell@net-up.de>
2019-12-12 12:11:50 +0100

    alt-text

--- partials/_tag_header.php
+++ partials/_tag_header.php
@@ -5,6 +5,8 @@
         $tm_output = '<label class="mr-3 ml-1">' . __("Schlagwörter", "ticketmachine") . ':</label> <div class="card-meta-tag keyword">' . $globals->tag . ' <a class="ml-2" href="' . str_replace("tag=" . urldecode($globals->tag), "", $_SERVER[REQUEST_URI]) . '"><i class="fa fa-times"></i></a></div>';
 
         print_r(str_replace("tag=" . $globals->tag, "", urldecode($_SERVER[REQUEST_URI])));
+
+        print_r($globals->tag);
         return $tm_output;
     }
 
