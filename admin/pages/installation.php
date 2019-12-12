669f187f73bcfa8cfe3c6956c212ad61285a96dc
Schneider Tobias <schneidert@net-up.net>
2019-12-12 16:45:21 +0100

    Installation

--- admin/pages/installation.php
+++ admin/pages/installation.php
@@ -3,18 +3,18 @@
         <div class="d-flex justify-content-center">
             <div class="row">
                 <div class="col-xs-12 text-center">
-                    <img class="" src="">
-                    <h1 class="">Vielen Dank, dass Sie mich installiert haben!</h1>
-                    <div class="">Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
-                    <i class="fas fa-cloud"></i>
-                    <div class="">Alle Veranstaltungen werden über den TicketMachine Cloud Service <i></i> gespeichert.</div>
-                    <i class="far fa-user-circle"></i>
-                    <div class="">Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen), benötigen wir Ihre E-Mail-Adresse</div>
-                    <input class="" type="email">
-                    <div class="">
+                    <img  src="">
+                    <h1>Vielen Dank, dass Sie mich installiert haben!</h1>
+                    <div style="font-size: 16px;">Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
+                    <i class="fas fa-cloud" style="font-size: 30px;"></i>
+                    <div style="font-size: 16px;">Alle Veranstaltungen werden über den TicketMachine Cloud Service <i></i> gespeichert.</div>
+                    <i class="far fa-user-circle" style="font-size: 30px;"></i>
+                    <div style="font-size: 16px;">Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen), benötigen wir Ihre E-Mail-Adresse</div>
+                    <input type="email">
+                    <div style="font-size: 16px;">
                         <input type="checkbox"><label>Ich habe die <a>Datenschutzerklärung</a>gelesen und akzeptiert.</label>
                     </div>
-                    <button class="">LOS GEHT'S!</button>
+                    <button>LOS GEHT'S!</button>
                 </div>
             </div>
         </div>
