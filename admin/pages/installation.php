f7e44ea05995178cc3a6fa1fe5a2f6ee8f600cbf
Schneider Tobias <schneidert@net-up.net>
2019-12-12 16:00:58 +0100

    Installation

--- admin/pages/installation.php
+++ admin/pages/installation.php
@@ -1,14 +1,16 @@
 <div class="wrap tm-admin-page">
-    <div class="d-flex justify-content-center">
-        <img src="">
-        <div class="text-align-center">Vielen Dank, dass Sie mich installiert haben!</div>
-        <div>Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
-        <i></i>
-        <div>Alle Veranstaltungen werden über den TicketMachine Cloud Service <i></i> gespeichert.</div>
-        <i></i>
-        <div>Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen), benötigen wir Ihre E-Mail-Adresse</div>
-        <input type="email">
-        <input type="checkbox"><label>Ich habe die <a>Datenschutzerklärung</a>gelesen und akzeptiert.</label>
-        <button>LOS GEHT'S!</button>
+    <div class="container">
+        <div class="d-flex justify-content-center">
+            <img class="row" src="">
+            <div class="row">Vielen Dank, dass Sie mich installiert haben!</div>
+            <div class="row">Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
+            <i class="row"></i>
+            <div class="row">Alle Veranstaltungen werden über den TicketMachine Cloud Service <i></i> gespeichert.</div>
+            <i class="row"></i>
+            <div class="row">Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen), benötigen wir Ihre E-Mail-Adresse</div>
+            <input class="row" type="email">
+            <input class="row" type="checkbox"><label>Ich habe die <a>Datenschutzerklärung</a>gelesen und akzeptiert.</label>
+            <button class="row">LOS GEHT'S!</button>
+        </div>
     </div>
 </div>
