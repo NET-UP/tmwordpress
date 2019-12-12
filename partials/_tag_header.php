ba8bb8a932205a43ecd682084f764cd067f4d7c3
Billie Bell <billie.bell@net-up.de>
2019-12-12 13:17:21 +0100

    alt-text

--- partials/_tag_header.php
+++ partials/_tag_header.php
@@ -11,8 +11,8 @@
                       </div>';
 
         #$a = $_SERVER[REQUEST_URI];
-        $a = htmlentities($globals->tag, ENT_QUOTES, "UTF-8");
-        $b = htmlentities("Fußball", ENT_QUOTES, "UTF-8");
+        $a = utf8_encode(htmlentities($globals->tag, ENT_QUOTES, "UTF-8"));
+        $b = utf8_encode(htmlentities("Fußball", ENT_QUOTES, "UTF-8"));
         if (strpos($a, $b)){
             print_r($b . " was found in " . $a);
             #print_r(mb_detect_encoding($a) . mb_detect_encoding($b));
