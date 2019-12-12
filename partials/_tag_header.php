a4e8e80b5fcd43b90f7e7ab16e440382314d7065
Billie Bell <billie.bell@net-up.de>
2019-12-12 12:22:23 +0100

    alt-text

--- partials/_tag_header.php
+++ partials/_tag_header.php
@@ -4,8 +4,7 @@
 
         $tm_output = '<label class="mr-3 ml-1">' . __("Schlagwörter", "ticketmachine") . ':</label> <div class="card-meta-tag keyword">' . $globals->tag . ' <a class="ml-2" href="' . str_replace("tag=" . urldecode($globals->tag), "", $_SERVER[REQUEST_URI]) . '"><i class="fa fa-times"></i></a></div>';
 
-        print_r(urldecode($_SERVER[REQUEST_URI]) ." - tag=" . $globals->tag);#
-        if (strpos(urldecode($_SERVER[REQUEST_URI]), "tag=" . $globals->tag)){
+        if (strpos(urldecode($_SERVER[REQUEST_URI]), $globals->tag)){
             print_r("billie big gay");
         }
         return $tm_output;
