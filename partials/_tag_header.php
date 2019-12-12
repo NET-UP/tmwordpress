2b88c9f4c5b05f251ba1f67acdf4af95df142fed
Schneider Tobias <schneidert@net-up.net>
2019-12-12 13:04:41 +0100

    bugfix for tags

--- partials/_tag_header.php
+++ partials/_tag_header.php
@@ -4,7 +4,7 @@
 
         $tm_output = '<label class="mr-3 ml-1">' . __("Schlagwörter", "ticketmachine") . ':</label> 
                       <div class="card-meta-tag keyword">' . $globals->tag . ' 
-                        <a class="ml-2" href="' . str_replace("tag=" . urldecode($globals->tag), "", $_SERVER[REQUEST_URI]) . '">
+                        <a class="ml-2" href="' . str_replace("tag=" . urlencode($globals->tag), "", $_SERVER[REQUEST_URI]) . '">
                             <i class="fa fa-times"></i>
                         </a>
                       </div>';
