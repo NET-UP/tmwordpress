6544aeb0d19e4075db76255dee557f855b764260
Billie Bell <billie.bell@net-up.de>
2019-12-12 10:11:01 +0100

    Edit event tag bugfix

--- partials/_search_header.php
+++ partials/_search_header.php
@@ -22,12 +22,16 @@
                                         <div class='input-group-append'>
                                             <button type='submit' alt='" . __("Suche absenden") . "' class='btn btn-secondary form-control'><i class='fas fa-search'></i></button>
                                         </div>
-                                        <div class='col-12 d-sm-none mb-3'></div>
-                                        <div class='btn-group ml-0 ml-sm-4'>
-                                            <a href='#' aria-label='" . __("Zum vorigen Monat") . "' class='btn btn-secondary' id='calendar-prev'><i class='fas fa-angle-left'></i></a>
-                                            <a href='#' class='btn btn-secondary' id='calendar-title'></a>
-                                            <a href='#' aria-label='" . __("Zum nächsten Monat") . "' class='btn btn-secondary' id='calendar-next'><i class='fas fa-angle-right'></i></a>
-                                        </div>";
+                                        <div class='col-12 d-sm-none mb-3'></div>";
+
+
+                                        if($_GET['display'] == "calendar"){
+                            $tm_output .= "<div class='btn-group ml-0 ml-sm-4'>
+                                                <a href='#' aria-label='" . __("Zum vorigen Monat") . "' class='btn btn-secondary' id='calendar-prev'><i class='fas fa-angle-left'></i></a>
+                                                <a href='#' class='btn btn-secondary' id='calendar-title'></a>
+                                                <a href='#' aria-label='" . __("Zum nächsten Monat") . "' class='btn btn-secondary' id='calendar-next'><i class='fas fa-angle-right'></i></a>
+                                            </div>";
+                                        }
 
                                         if($globals->show_list && $globals->show_calendar){
                                             $tm_output .= "<div class='btn-group ml-4'>";
