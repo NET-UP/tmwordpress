f35b701b5b09153cbe278b98983f93fa54f54c4b
Schneider Tobias <schneidert@net-up.net>
2019-12-12 15:52:16 +0100

    Installation

--- admin/pages/installation.php
+++ admin/pages/installation.php
@@ -1,3 +1,16 @@
 <div class="wrap tm-admin-page">
-    <h1>TEST</h1>
+    <div style="width: 500px;">
+        <div class="container">
+            <img src="">
+            <div class="text-align-center">Vielen Dank, dass Sie mich installiert haben!</div>
+            <div>Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
+            <i></i>
+            <div>Alle Veranstaltungen werden über den TicketMachine Cloud Service <i></i> gespeichert.</div>
+            <i></i>
+            <div>Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen), benötigen wir Ihre E-Mail-Adresse</div>
+            <input type="email">
+            <input type="checkbox"><label>Ich habe die <a>Datenschutzerklärung</a>gelesen und akzeptiert.</label>
+            <button>LOS GEHT'S!</button>
+        </div>
+    </div>
 </div>
