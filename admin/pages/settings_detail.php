aab526389ad1b81d57f99cc1a065c968aaec23b3
Billie Bell <billie.bell@net-up.de>
2019-12-16 14:07:33 +0100

    alt-text

--- admin/pages/settings_detail.php
+++ admin/pages/settings_detail.php
@@ -1,3 +1,40 @@
+<?php
+	if (isset($_POST['submit'])) {
+
+		$save_array = 
+			array(
+				"show_social_media" => $_POST['show_social_media'],
+				"show_social_media_ical" => $_POST['show_social_media_ical'],
+				"show_social_media_google_cal" => $_POST['show_social_media_google_cal'],
+				"show_social_media_facebook" => $_POST['show_social_media_facebook'],
+				"show_social_media_twitter" => $_POST['show_social_media_twitter'],
+				"show_social_media_email" => $_POST['show_social_media_email'],
+				"show_social_media_messenger" => $_POST['show_social_media_messenger'],
+				"show_social_media_whatsapp" => $_POST['show_social_media_whatsapp'],
+				"show_google_map" => $_POST['show_google_map']
+			);
+		if (!empty($tm_config)) {
+			$wpdb->update(
+				$wpdb->prefix . "ticketmachine_config",
+				$save_array,
+				array('id' => $tm_config->id)
+			);
+			?>
+			<div class="notice notice-success is-dismissable">
+				<p><?php echo __('Gespeichert!'); ?></p>
+			</div>
+			<?php
+		}else{
+			?>
+			<div class="notice notice-error is-dismissable">
+				<p><?php echo __('Etwas ist schiefgelaufen.'); ?></p>
+			</div>
+			<?php
+		}
+		$tm_config = (object)$_POST;
+	}
+?>
+
 <table class="form-table">
 	<tbody>
 		<tr>
