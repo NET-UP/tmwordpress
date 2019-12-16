6f464f3a67959c98eef32d7e880f826e42560ed2
Billie Bell <billie.bell@net-up.de>
2019-12-16 09:56:14 +0100

    Edit event tag bugfix

--- admin/pages/installation.php
+++ admin/pages/installation.php
@@ -2,30 +2,27 @@
     <div class="container">
         <div class="d-flex justify-content-center">
             <div class="row">
-                <div class="col-xs-12 text-center">
+                <div class="col-xs-12 text-center" style="font-size: 16px;">
                     <img style="width: 90px;" src=" <?php echo dirname(plugin_dir_url( __FILE__ ), 1) . '/assets/img/logo.png'; ?>" alt="TicketMachine Logo">
                     <h1>Vielen Dank, dass Sie mich installiert haben!</h1>
-                    <div style="font-size: 16px;">Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
+                    <div>Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
                     <i class="fas fa-cloud" style="font-size: 30px;margin-bottom: 10px;margin-top: 20px;"></i>
-                    <div style="font-size: 16px;">Alle Veranstaltungen werden über den TicketMachine Cloud Service
-                        <i class="fas fa-info-circle" style="font-size: 15px;" title="Was soll hier stehen?"></i>
+                    <div>Alle Veranstaltungen werden über den TicketMachine Cloud Service
+                        <i class="fas fa-info-circle" title="Was soll hier stehen?"></i>
                         <br>
                         gespeichert.
                     </div>
                     <i class="far fa-user-circle" style="font-size: 30px;margin-bottom: 10px;margin-top: 20px;"></i>
-                    <div style="font-size: 16px;">Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen),
+                    <div>Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen),
                     <br>
                      benötigen wir Ihre E-Mail-Adresse.
                     </div>
 
-                    <div class="input-group mb-3">
-                        <input type="email" style="width: 100%;height: 35px;border-left-width: 4px;border-left-color: orange;margin: 15px 0px;" placeholder="E-Mail Adresse bitte eingeben">
-                        <div class="input-group-append">
-                            <span class="input-group-text" id="basic-addon2">@</span>
-                        </div>
+                    <div class="email-icon-right mt-3 mb-3">
+                        <input type="email" style="width: 100%;height: 35px;border-left-width: 4px;border-left-color: orange;" placeholder="E-Mail Adresse bitte eingeben">
                     </div>
                         
-                    <div style="font-size: 16px;">
+                    <div>
                         <label><input type="checkbox" style="border-color: orange;">Ich habe die <a href="#" style="text-decoration-line: none;">Datenschutzerklärung</a> gelesen und akzeptiert.</label>
                     </div>
                     <button style="font-size: 16px;font-weight: bold;color: white;width: 100%;background-color: orange;padding: 7px 0px;">LOS GEHT'S!<i class="fas fa-check" style="font-size: 20px;color: #FFF;float: right;margin-right: 0.5em;"></i></button>
