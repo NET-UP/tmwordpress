0e7a44d058442b78a6c19bf4272ba7197840eaab
Schneider Tobias <schneidert@net-up.net>
2019-12-16 13:43:15 +0100

    added detail tab

--- admin/pages/settings_detail.php
+++ admin/pages/settings_detail.php
@@ -2,7 +2,41 @@
 	<tbody>
 		<tr>
 			<th><label><?php echo __('Social Media teilen aktiviert?', 'ticketmachine'); ?></label></th>
-            <td><input name="show_calendar" type="checkbox" value=1 class="regular-text" <?php //if($tm_config->show_calendar){ ?>checked <?php // } ?>/></td>
-		</tr>
-	</tbody>
+            <td><input name="show_media" type="checkbox" value=1 class="regular-text" <?php //if($tm_config->show_calendar){ ?>checked <?php // } ?>/></td>
+        </tr>
+        <tr>
+            <th><label><?php echo __('iCal', 'ticketmachine'); ?></label></th>
+            <td><input name="show_ical" type="checkbox" value=1 class="regular-text"></td>
+        </tr>
+        <tr>
+            <th><label><?php echo __('Google Kalender', 'ticketmachine'); ?></label></th>
+            <td><input name="show_google_calendar" type="checkbox" value=1 class="regular-text"></td>
+        </tr>
+        <tr>
+            <th><label><?php echo __('Facebook', 'ticketmachine'); ?></label></th>
+            <td><input name="show_facebook" type="checkbox" value=1 class="regular-text"></td>
+        </tr>
+        <tr>
+            <th><label><?php echo __('Twitter', 'ticketmachine'); ?></label></th>
+            <td><input name="show_twitter" type="checkbox" value=1 class="regular-text"></td>
+        </tr>
+        <tr>
+            <th><label><?php echo __('E-Mail', 'ticketmachine'); ?></label></th>
+            <td><input name="show_email" type="checkbox" value=1 class="regular-text"></td>
+        </tr>
+        <tr>
+            <th><label><?php echo __('Messenger', 'ticketmachine'); ?></label></th>
+            <td><input name="show_messenger" type="checkbox" value=1 class="regular-text"></td>
+        </tr>
+        <tr>
+            <th><label><?php echo __('WhatsApp', 'ticketmachine'); ?></label></th>
+            <td><input name="show_whatsapp" type="checkbox" value=1 class="regular-text"></td>
+        </tr>
+    </tbody>
+    <tbody>
+        <tr>
+            <th><label><?php echo __('Google Maps aktiviert?', 'ticketmachine'); ?></label></th>
+            <td><input name="show_google_map" type="checkbox" value=1 class="regular-text"></td>
+        </tr>
+    </tbody>
 </table>
