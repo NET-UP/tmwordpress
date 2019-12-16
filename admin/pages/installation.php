d836f9c1638b13fd04eeef6ea31630b528cecc2b
Schneider Tobias <schneidert@net-up.net>
2019-12-16 09:19:34 +0100

    added logo info icon

--- admin/pages/installation.php
+++ admin/pages/installation.php
@@ -3,11 +3,11 @@
         <div class="d-flex justify-content-center">
             <div class="row">
                 <div class="col-xs-12 text-center">
-                    <img src=" <?php echo dirname(plugin_dir_url( __FILE__ ), 1) . '/assets/img/logo.png'; ?>" alt="TicketMachine Logo">
+                    <img style="width: 90px;" src=" <?php echo dirname(plugin_dir_url( __FILE__ ), 1) . '/assets/img/logo.png'; ?>" alt="TicketMachine Logo">
                     <h1>Vielen Dank, dass Sie mich installiert haben!</h1>
                     <div style="font-size: 16px;">Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
                     <i class="fas fa-cloud" style="font-size: 30px;margin-bottom: 10px;margin-top: 30px;"></i>
-                    <div style="font-size: 16px;">Alle Veranstaltungen werden über den TicketMachine Cloud Service <i></i> gespeichert.</div>
+                    <div style="font-size: 16px;">Alle Veranstaltungen werden über den TicketMachine Cloud Service gespeichert.<i class="fas fa-info-circle" style="font-size: 30px;margin-bottom: 10px;margin-top: 30px;"></i></div>
                     <i class="far fa-user-circle" style="font-size: 30px;margin-bottom: 10px;margin-top: 30px;"></i>
                     <div style="font-size: 16px;">Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen),
                     <br>
