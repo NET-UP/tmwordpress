0e9be8c94a17e7a8417861421a24f7fd63d76ee4
Billie Bell <billie.bell@net-up.de>
2019-12-16 14:05:03 +0100

    alt-text

--- admin/pages/settings_detail.php
+++ admin/pages/settings_detail.php
@@ -2,35 +2,35 @@
 	<tbody>
 		<tr>
 			<th><label><?php echo __('Social Media teilen aktiviert?', 'ticketmachine'); ?></label></th>
-            <td><input name="show_media" type="checkbox" value=1 class="regular-text" <?php //if($tm_config->show_calendar){ ?>checked <?php // } ?>/></td>
+            <td><input name="show_social_media" type="checkbox" value=1 class="regular-text" <?php if($tm_config->show_social_media){ ?>checked <?php  } ?>/></td>
         </tr>
         <tr>
             <th class="pl-4"><label><?php echo __('iCal', 'ticketmachine'); ?></label></th>
-            <td><input name="show_ical" type="checkbox" value=1 class="regular-text"></td>
+            <td><input name="show_social_media_ical" type="checkbox" value=1 class="regular-text" <?php if($tm_config->show_social_media_ical){ ?>checked <?php  } ?>/></td>
         </tr>
         <tr>
             <th class="pl-4"><label><?php echo __('Google Kalender', 'ticketmachine'); ?></label></th>
-            <td><input name="show_google_calendar" type="checkbox" value=1 class="regular-text"></td>
+            <td><input name="show_social_media_google_cal" type="checkbox" value=1 class="regular-text" <?php if($tm_config->show_social_media_google_cal){ ?>checked <?php  } ?>/></td>
         </tr>
         <tr>
             <th class="pl-4"><label><?php echo __('Facebook', 'ticketmachine'); ?></label></th>
-            <td><input name="show_facebook" type="checkbox" value=1 class="regular-text"></td>
+            <td><input name="show_social_media_facebook" type="checkbox" value=1 class="regular-text" <?php if($tm_config->show_social_media_facebook){ ?>checked <?php  } ?>/></td>
         </tr>
         <tr>
             <th class="pl-4"><label><?php echo __('Twitter', 'ticketmachine'); ?></label></th>
-            <td><input name="show_twitter" type="checkbox" value=1 class="regular-text"></td>
+            <td><input name="show_social_media_twitter" type="checkbox" value=1 class="regular-text" <?php if($tm_config->show_social_media_twitter){ ?>checked <?php  } ?>/></td>
         </tr>
         <tr>
             <th class="pl-4"><label><?php echo __('E-Mail', 'ticketmachine'); ?></label></th>
-            <td><input name="show_email" type="checkbox" value=1 class="regular-text"></td>
+            <td><input name="show_social_media_email" type="checkbox" value=1 class="regular-text" <?php if($tm_config->show_social_media_email){ ?>checked <?php  } ?>/></td>
         </tr>
         <tr>
             <th class="pl-4"><label><?php echo __('Messenger', 'ticketmachine'); ?></label></th>
-            <td><input name="show_messenger" type="checkbox" value=1 class="regular-text"></td>
+            <td><input name="show_social_media_messenger" type="checkbox" value=1 class="regular-text" <?php if($tm_config->show_social_media_messenger){ ?>checked <?php  } ?>/></td>
         </tr>
         <tr>
             <th class="pl-4"><label><?php echo __('WhatsApp', 'ticketmachine'); ?></label></th>
-            <td><input name="show_whatsapp" type="checkbox" value=1 class="regular-text"></td>
+            <td><input name="show_social_media_whatsapp" type="checkbox" value=1 class="regular-text" <?php if($tm_config->show_social_media_whatsapp){ ?>checked <?php  } ?>/></td>
         </tr>
     </tbody>
 </table>
@@ -38,7 +38,7 @@
     <tbody>
         <tr>
             <th><label><?php echo __('Google Maps aktiviert?', 'ticketmachine'); ?></label></th>
-            <td><input name="show_google_map" type="checkbox" value=1 class="regular-text"></td>
+            <td><input name="show_google_map" type="checkbox" value=1 class="regular-text" <?php if($tm_config->show_google_map){ ?>checked <?php  } ?>/></td>
         </tr>
     </tbody>
 </table>
