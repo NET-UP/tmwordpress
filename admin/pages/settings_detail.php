b439737864762e9f01becc5ad7873e56f3bcfe31
Schneider Tobias <schneidert@net-up.net>
2019-12-16 13:35:49 +0100

    added detail tab

--- admin/pages/settings_detail.php
+++ admin/pages/settings_detail.php
@@ -1,1 +1,8 @@
-<h1>TEST</h1>
+<table class="form-table">
+	<tbody>
+		<tr>
+			<th><label><?php echo __('Social Media teilen aktiviert?', 'ticketmachine'); ?></label></th>
+            <td><input name="show_calendar" type="checkbox" value=1 class="regular-text" <?php //if($tm_config->show_calendar){ ?>checked <?php // } ?>/></td>
+		</tr>
+	</tbody>
+</table>
