06c61ed729598ac0bd824edd88dec5c7f9f17d38
Schneider Tobias <schneidert@net-up.net>
2019-12-16 13:25:39 +0100

    added detail tab

--- admin/pages/settings_detail.php
+++ admin/pages/settings_detail.php
@@ -1,8 +1,41 @@
+<?php
+	if (isset($_POST['submit'])) {
+
+        if (isset($_POST['show_calendar'])){
+            $_POST['show_calendar'] = 1;
+        }
+
+		$save_array = 
+			array(
+				"show_calendar" => $_POST['show_calendar'],
+			);
+		if (!empty($tm_config)) {
+			$wpdb->update(
+				$wpdb->prefix . "ticketmachine_config",
+				$save_array,
+				array('id' => $tm_config->id)
+			);
+			?>
+			<div class="notice notice-success is-dismissable">
+				<p><?php echo __('Gespeichert!'); ?></p>
+			</div>
+			<?php
+		}else{
+			?>
+			<div class="notice notice-error is-dismissable">
+				<p><?php echo __('Etwas ist schiefgelaufen.'); ?></p>
+			</div>
+			<?php
+		}
+		$tm_config = (object)$_POST;
+	}
+?>
+
 <table class="form-table">
 	<tbody>
 		<tr>
-			<th><label><?php echo __('Liste aktivieren?', 'ticketmachine'); ?></label></th>
-            <td><input name="show_list" type="checkbox" value=1 class="regular-text" <?php if($tm_config->show_list){ ?>checked <?php } ?>/></td>
+			<th><label><?php echo __('Kalender aktivieren?', 'ticketmachine'); ?></label></th>
+            <td><input name="show_calendar" type="checkbox" value=1 class="regular-text" <?php if($tm_config->show_calendar){ ?>checked <?php } ?>/></td>
 		</tr>
 
 	</tbody>
