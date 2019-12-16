ee5a5638f09764dfd25944fe274ff8eefea9b01f
Billie Bell <billie.bell@net-up.de>
2019-12-16 13:50:10 +0100

    alt-text

--- admin/pages/settings_detail.php
+++ admin/pages/settings_detail.php
@@ -5,31 +5,31 @@
             <td><input name="show_media" type="checkbox" value=1 class="regular-text" <?php //if($tm_config->show_calendar){ ?>checked <?php // } ?>/></td>
         </tr>
         <tr>
-            <th class="ml-4"><label><?php echo __('iCal', 'ticketmachine'); ?></label></th>
+            <th class="pl-4"><label><?php echo __('iCal', 'ticketmachine'); ?></label></th>
             <td><input name="show_ical" type="checkbox" value=1 class="regular-text"></td>
         </tr>
         <tr>
-            <th class="ml-4"><label><?php echo __('Google Kalender', 'ticketmachine'); ?></label></th>
+            <th class="pl-4"><label><?php echo __('Google Kalender', 'ticketmachine'); ?></label></th>
             <td><input name="show_google_calendar" type="checkbox" value=1 class="regular-text"></td>
         </tr>
         <tr>
-            <th class="ml-4"><label><?php echo __('Facebook', 'ticketmachine'); ?></label></th>
+            <th class="pl-4"><label><?php echo __('Facebook', 'ticketmachine'); ?></label></th>
             <td><input name="show_facebook" type="checkbox" value=1 class="regular-text"></td>
         </tr>
         <tr>
-            <th class="ml-4"><label><?php echo __('Twitter', 'ticketmachine'); ?></label></th>
+            <th class="pl-4"><label><?php echo __('Twitter', 'ticketmachine'); ?></label></th>
             <td><input name="show_twitter" type="checkbox" value=1 class="regular-text"></td>
         </tr>
         <tr>
-            <th class="ml-4"><label><?php echo __('E-Mail', 'ticketmachine'); ?></label></th>
+            <th class="pl-4"><label><?php echo __('E-Mail', 'ticketmachine'); ?></label></th>
             <td><input name="show_email" type="checkbox" value=1 class="regular-text"></td>
         </tr>
         <tr>
-            <th class="ml-4"><label><?php echo __('Messenger', 'ticketmachine'); ?></label></th>
+            <th class="pl-4"><label><?php echo __('Messenger', 'ticketmachine'); ?></label></th>
             <td><input name="show_messenger" type="checkbox" value=1 class="regular-text"></td>
         </tr>
         <tr>
-            <th class="ml-4"><label><?php echo __('WhatsApp', 'ticketmachine'); ?></label></th>
+            <th class="pl-4"><label><?php echo __('WhatsApp', 'ticketmachine'); ?></label></th>
             <td><input name="show_whatsapp" type="checkbox" value=1 class="regular-text"></td>
         </tr>
     </tbody>
