d5a737cc7d914b6241cc972a251ed9071f44cf59
Billie Bell <billie.bell@net-up.de>
2019-12-16 10:19:54 +0100

    alt-text

--- assets/js/ticketmachine.js
+++ assets/js/ticketmachine.js
@@ -33,4 +33,20 @@ jQuery(document).ready(function(){
         }
     });
 
+    jQuery(".share-popup").click(function(){
+    var window_size = "width=585,height=511";
+    var url = this.href;
+    var domain = url.split("/")[2];
+    switch(domain) {
+        case "www.facebook.com":
+            window_size = "width=585,height=368";
+            break;
+        case "www.twitter.com":
+            window_size = "width=585,height=261";
+            break;
+    }
+    window.open(url, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,' + window_size);
+    return false;
+});
+
 });
