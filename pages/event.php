fe974d67ce8779776297c43f34bd31bcced963bc
Billie Bell <billie.bell@net-up.de>
2019-12-19 12:35:42 +0100

    Fix for FK

--- pages/event.php
+++ pages/event.php
@@ -3,28 +3,32 @@
 	function tm_display_event ( $atts ) {
 		global $event, $globals, $api;
 
-		$params = [ "id" => $_GET['id'] ];
+		if(!empty($_GET['id'])){
+			$params = array_push_assoc($params, "id", $_GET['id']);
+		}
 		$event = tmapi_event($params);
 
-		$tm_output .= '
-			<div class=	"col-12">
-				<div class="row">
-					<div class="col-12 col-lg-5 col-xl-6 tm_left">';
-		$tm_output .= tm_event_page_information($event, $globals);
-		$tm_output .= '
-					</div>
-					<div class="col-12 col-lg-7 col-xl-6 tm_right">';
-		$tm_output .= tm_event_page_actions($event, $globals);
-		$tm_output .= tm_event_page_tickets($event, $globals);
-		$tm_output .= tm_event_page_details($event, $globals);
-		if ($globals->show_google_map) {
-			$tm_output .= tm_event_page_google_map($event, $globals);
-		}
-		$tm_output .= '
+		if(isset($event->id)){
+			$tm_output .= '
+				<div class=	"col-12">
+					<div class="row">
+						<div class="col-12 col-lg-5 col-xl-6 tm_left">';
+			$tm_output .= tm_event_page_information($event, $globals);
+			$tm_output .= '
+						</div>
+						<div class="col-12 col-lg-7 col-xl-6 tm_right">';
+			$tm_output .= tm_event_page_actions($event, $globals);
+			$tm_output .= tm_event_page_tickets($event, $globals);
+			$tm_output .= tm_event_page_details($event, $globals);
+			if ($globals->show_google_map) {
+				$tm_output .= tm_event_page_google_map($event, $globals);
+			}
+			$tm_output .= '
+						</div>
 					</div>
 				</div>
-			</div>
-		';
+			';
+		}
 		
 		return $tm_output;
 		
