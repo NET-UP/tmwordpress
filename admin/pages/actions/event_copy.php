a8344d327ad7a10a70fa5a3999f5160023af6985
Billie Bell <billie.bell@net-up.de>
2019-12-19 16:00:44 +0100

    Fix for FK

--- admin/pages/actions/event_copy.php
+++ admin/pages/actions/event_copy.php
@@ -7,13 +7,13 @@
         
         $post_json = json_encode($_POST);
         $tm_json = tmapi_event_copy($post_json);
-        $response = (object)$tm_json->model_error[0];
+        $response = (object)$tm_json;
     }
 ?>
 
-<?php if(strlen($response->error_code) > 0){ ?>
+<?php if(isset($response->model_error[0]['error_code']) && strlen($response->model_error[0]['error_code']) > 0){ ?>
     <div class="notice notice-error is-dismissable">
-        <p><?php echo __($response->error_message); ?></p>
+        <p><?php echo __($response->model_error[0]['error_message']); ?></p>
     </div>
 <?php }elseif(empty($tm_json)){ ?>
     <div class="notice notice-error is-dismissable">
