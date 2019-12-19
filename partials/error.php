c0a8c51dc3d5d23a915bd9dbc1ff2abce455f6a1
Billie Bell <billie.bell@net-up.de>
2019-12-19 14:03:05 +0100

    Fix for FK

--- partials/error.php
+++ partials/error.php
@@ -1,7 +1,12 @@
 <?php
 
-    function tm_message( $message, $type ) {
-        $tm_output = $message;
+    function tm_alert( $message, $type ) {
+
+        if($type == "error") {
+            $type = "danger";
+        }
+
+        $tm_output = "<div class='alert alert-" . $type . "'>" . $message . "</div>";
 
         return $tm_output;
     }
