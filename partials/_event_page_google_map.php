5cc5c31d48aa626d853c48d4d5278a6267692d0c
Billie Bell <billie.bell@net-up.de>
2019-12-19 14:24:03 +0100

    Fix for FK

--- partials/_event_page_google_map.php
+++ partials/_event_page_google_map.php
@@ -2,7 +2,6 @@
     function tm_event_page_google_map($event, $globals) {
         
         if (isset($_COOKIE["allow_google_maps"])){
-
             $tm_output = "<iframe width='100%' height='300' id='mapcanvas' src='https://maps.google.com/maps?q=". urlencode($event->event_location['street'] . ' ' . $event->event_location['house_number'] . ' ' . $event->event_location['zip'] . ' ' . $event->event_location['city'] . ' ' . $event->event_location['country'] ) ."&z=10&ie=UTF8&iwloc=&output=embed' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'>
                             </iframe>
                             <a class='disallow-google-maps float-right' href='#'>Google Maps nicht erlauben</a>";
