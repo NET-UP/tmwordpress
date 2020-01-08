32d1b7d3f27b587128493ee739e829023e02785c
Billie Bell <billie.bell@net-up.de>
2020-01-08 15:02:25 +0100

    Edit event tag bugfix

--- admin/admin.php
+++ admin/admin.php
@@ -2,7 +2,7 @@
     wp_enqueue_style( 'admin_CSS', plugins_url('assets/css/style.css', __FILE__ ) );
     wp_enqueue_style( 'admin_grid_CSS', plugins_url('assets/css/grid.min.css', __FILE__ ) );
 	wp_enqueue_style( 'fontawesome-5_CSS', plugins_url('../assets/css/ext/fontawesome.min.css', __FILE__ ) );
-	wp_enqueue_style( 'datetimepicker_CSS', plugins_url('../assets/css/ext/bootstrap-datetimepicker.css', __FILE__ ) );
+	wp_enqueue_style( 'datetimepicker_CSS', plugins_url('assets/css/ext/bootstrap-datetimepicker.css', __FILE__ ) );
     
     wp_enqueue_script( 'moment_JS', plugins_url('assets/js/ext/moment-with-locales.js', __FILE__ ) );
     wp_enqueue_script( 'bootstrap3_JS', plugins_url('assets/js/ext/bootstrap.min.js', __FILE__ ) );
