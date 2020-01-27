00f72df1aefde308aa5407566e50be0e1588e6e5
Billie Bell <billie.bell@net-up.de>
2020-01-27 16:55:00 +0100

    Edit event tag bugfix

--- globals.php
+++ globals.php
@@ -75,7 +75,6 @@
 		'response_type' => 'code',
 		'client_id' => $api->client_id,
 		'redirect_uri' => "https://www.ticketmachine.de/oauth/start.php?start_uri=" . (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",
-		'start_uri' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",
 		'state' => $_SESSION['state'],
 		'scope' => 'public organizer organizer/event'
 	);
