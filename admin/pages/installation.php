ed5a69d4188cba680c62f4caae262311de7ace4b
Billie Bell <billie.bell@net-up.de>
2020-01-28 11:46:01 +0100

    Edit event tag bugfix

--- admin/pages/installation.php
+++ admin/pages/installation.php
@@ -36,41 +36,43 @@
 <div class="wrap tm-admin-page">
     <div class="container">
         <div class="box box-md">
-            <div class="row card p-4">
-                <div class="col-12 text-center" style="font-size: 16px;">
-                    <img style="width: 90px;" src=" <?php echo dirname(plugin_dir_url( __FILE__ ), 1) . '/assets/img/logo.png'; ?>" alt="TicketMachine Logo">
-                    <h1>Vielen Dank, dass Sie mich installiert haben!</h1>
-                    <div>Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
-                    <i class="fas fa-cloud tm-icon-big mt-4 mb-2"></i>
-                    <div>Alle Veranstaltungen werden über den TicketMachine Cloud Service
-                        <i class="fas fa-info-circle" title="Was soll hier stehen?"></i>
+            <div class="card p-4">
+                <div class="row">
+                    <div class="col-12 text-center" style="font-size: 16px;">
+                        <img style="width: 90px;" src=" <?php echo dirname(plugin_dir_url( __FILE__ ), 1) . '/assets/img/logo.png'; ?>" alt="TicketMachine Logo">
+                        <h1>Vielen Dank, dass Sie mich installiert haben!</h1>
+                        <div>Sie sind noch einen Schritt entfernt, um TicketMachine nutzen zu können.</div>
+                        <i class="fas fa-cloud tm-icon-big mt-4 mb-2"></i>
+                        <div>Alle Veranstaltungen werden über den TicketMachine Cloud Service
+                            <i class="fas fa-info-circle" title="Was soll hier stehen?"></i>
+                            <br>
+                            gespeichert.
+                        </div>
+                        <i class="far fa-user-circle tm-icon-big mt-4 mb-2"></i>
+                        <div>Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen),
                         <br>
-                        gespeichert.
-                    </div>
-                    <i class="far fa-user-circle tm-icon-big mt-4 mb-2"></i>
-                    <div>Um Ihr TicketMachine Konto zu erstellen (oder eine Verbindung herzustellen),
-                    <br>
-                     benötigen wir Ihre E-Mail-Adresse.
-                    </div>
+                        benötigen wir Ihre E-Mail-Adresse.
+                        </div>
 
-                    <?php
-	
-                        $api->auth->testdata = array(
-                            'response_type' => 'code',
-                            'client_id' => "4c0bcf69d871fb55362382f436e768b277592f3243e7da9bfef4dff997392fe0",
-                            'redirect_uri' => $api->auth->redirect_uri . "?start_uri=" . $api->auth->start_uri,
-                            'state' => $_SESSION['state'],
-                            'scope' => 'public organizer organizer/event'
-                        );
-    
-                      //$authorize_url = "http://apiv2." . $api->environment . "ticketmachine.de/oauth/authorize";
-                      $authorize_url = "http://localhost:3002/oauth/authorize";
-                      $authorize_url .= "?";
-                      //$authorize_url .= http_build_query($api->auth->data);
-                      $authorize_url .= http_build_query($api->auth->testdata);
-                      echo '<p><a class="button button-primary" href="'.$authorize_url.'">Mit TicketMachine verbinden</a></p>';
-                    ?>
+                        <?php
+        
+                            $api->auth->testdata = array(
+                                'response_type' => 'code',
+                                'client_id' => "4c0bcf69d871fb55362382f436e768b277592f3243e7da9bfef4dff997392fe0",
+                                'redirect_uri' => $api->auth->redirect_uri . "?start_uri=" . $api->auth->start_uri,
+                                'state' => $_SESSION['state'],
+                                'scope' => 'public organizer organizer/event'
+                            );
+        
+                        //$authorize_url = "http://apiv2." . $api->environment . "ticketmachine.de/oauth/authorize";
+                        $authorize_url = "http://localhost:3002/oauth/authorize";
+                        $authorize_url .= "?";
+                        //$authorize_url .= http_build_query($api->auth->data);
+                        $authorize_url .= http_build_query($api->auth->testdata);
+                        echo '<p><a class="button button-primary" href="'.$authorize_url.'">Mit TicketMachine verbinden</a></p>';
+                        ?>
 
+                    </div>
                 </div>
             </div>
         </div>
