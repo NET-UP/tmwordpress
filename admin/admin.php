3787e54efcf3337339c568ef5777650189248889
Billie Bell <billie.bell@net-up.de>
2020-01-28 11:56:54 +0100

    Edit event tag bugfix

--- admin/admin.php
+++ admin/admin.php
@@ -4,10 +4,10 @@
 	wp_enqueue_style( 'fontawesome-5_CSS', plugins_url('../assets/css/ext/fontawesome.min.css', __FILE__ ) );
 	wp_enqueue_style( 'datetimepicker_CSS', plugins_url('assets/css/ext/bootstrap-datetimepicker.css', __FILE__ ) );
     
-    admin_enqueue_script( 'moment_JS', plugins_url('assets/js/ext/moment-with-locales.js', __FILE__ ) );
-    admin_enqueue_script( 'bootstrap3_JS', plugins_url('assets/js/ext/bootstrap.min.js', __FILE__ ) );
-    admin_enqueue_script( 'datetimepicker_JS', plugins_url('assets/js/ext/bootstrap-datetimepicker.min.js', __FILE__ ) );
-    admin_enqueue_script( 'taginput_JS', plugins_url('assets/js/ext/bootstrap-tag.min.js', __FILE__ ) );
+    admin_enqueue_scripts( 'moment_JS', plugins_url('assets/js/ext/moment-with-locales.js', __FILE__ ) );
+    admin_enqueue_scripts( 'bootstrap3_JS', plugins_url('assets/js/ext/bootstrap.min.js', __FILE__ ) );
+    admin_enqueue_scripts( 'datetimepicker_JS', plugins_url('assets/js/ext/bootstrap-datetimepicker.min.js', __FILE__ ) );
+    admin_enqueue_scripts( 'taginput_JS', plugins_url('assets/js/ext/bootstrap-tag.min.js', __FILE__ ) );
 
     include( plugin_dir_path( __FILE__ ) . 'menu.php');
 
