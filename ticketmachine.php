59b186ef559ca166f7d52da2f6730d0e98220bbe
Billie Bell <mrbilliebell@outlook.com>
2020-01-29 00:12:48 +0100

    test

--- ticketmachine.php
+++ ticketmachine.php
@@ -195,8 +195,6 @@
                     show_social_media_whatsapp bit(1) DEFAULT 1 NOT NULL,
                     show_google_map bit(1) DEFAULT 1 NOT NULL,
                     event_grouping varchar(64) DEFAULT 'Year' NOT NULL,
-                    events_slug varchar(128) DEFAULT 'events' NOT NULL,
-                    event_slug varchar(128) DEFAULT 'event' NOT NULL,
                     events_slug_id varchar(128) DEFAULT '" . $events_slug->ID . "' NOT NULL,
                     event_slug_id varchar(128) DEFAULT '" . $event_slug->ID . "' NOT NULL,
                 PRIMARY KEY  (id)
