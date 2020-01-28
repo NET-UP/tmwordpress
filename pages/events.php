dbef76d7604454861689b61dd2d593f2d3e44327
Billie Bell <billie.bell@net-up.de>
2020-01-28 10:49:09 +0100

    Edit event tag bugfix

--- pages/events.php
+++ pages/events.php
@@ -81,42 +81,44 @@
 			$atts['show_description'] = 1;
 			$atts['show_date'] = 1;
 			
-			$tm_output .= '<ul class="list-unstyled col-12">';
+			$tm_output .= '<div class="col-12">';
+				$tm_output .= '<ul class="list-unstyled">';
 
-            foreach($events as $event){
-                $event = (object)$event;
+				foreach($events as $event){
+					$event = (object)$event;
 
-                $tm_output .= '<li class="media">';
+					$tm_output .= '<li class="media">';
 
-                if($atts['show_image'] > 0){
-                    $tm_output .= '<div class="mr-3 media-img" style="background-image:url('. $event->event_img_url .')"></div>';
-                }
-                                    
-                    $tm_output .= '<div class="media-body">';
-                    $tm_output .= '<h5 class="mt-0 mb-1"><a href="/event?id=' . $event->id . '">' . $event->ev_name . '</a></h5>';
-
-                    if($atts['show_date'] > 0){
-						$tm_output .= '
-						<div class="card-meta-tag"><i class="far fa-calendar-alt tm-icon" aria-hidden="true"></i> &nbsp;'. date( "d.m.Y", strtotime($event->ev_date) ) .'</div> 
-                        <div class="card-meta-tag"><i class="far fa-clock tm-icon" aria-hidden="true"></i> &nbsp;'. date( "H:i", strtotime($event->ev_date) ) .'</div>';
+					if($atts['show_image'] > 0){
+						$tm_output .= '<div class="mr-3 media-img" style="background-image:url('. $event->event_img_url .')"></div>';
 					}
-					
-                    if($atts['show_description'] > 0){
-                        if(!$atts['description_length']){
-                            $atts['description_length'] = 15;
-                        }
-                        $tm_output .= '<div>' . wp_trim_words(wp_strip_all_tags($event->ev_description), $atts['description_length'], "...") . '</div>';
-                    }
+										
+						$tm_output .= '<div class="media-body">';
+						$tm_output .= '<h5 class="mt-0 mb-1"><a href="/event?id=' . $event->id . '">' . $event->ev_name . '</a></h5>';
 
-                $tm_output .= '</div>
-                            </li>';
-            }
+						if($atts['show_date'] > 0){
+							$tm_output .= '
+							<div class="card-meta-tag"><i class="far fa-calendar-alt tm-icon" aria-hidden="true"></i> &nbsp;'. date( "d.m.Y", strtotime($event->ev_date) ) .'</div> 
+							<div class="card-meta-tag"><i class="far fa-clock tm-icon" aria-hidden="true"></i> &nbsp;'. date( "H:i", strtotime($event->ev_date) ) .'</div>';
+						}
+						
+						if($atts['show_description'] > 0){
+							if(!$atts['description_length']){
+								$atts['description_length'] = 15;
+							}
+							$tm_output .= '<div>' . wp_trim_words(wp_strip_all_tags($event->ev_description), $atts['description_length'], "...") . '</div>';
+						}
 
-            if($atts['show_more'] > 0){
-                $tm_output .= '<li class="media"><a href="/events">' . __("Show all events", "ticketmachine") . '</a></li>';
-            }
+					$tm_output .= '</div>
+								</li>';
+				}
 
-			$tm_output .= '</ul>';
+				if($atts['show_more'] > 0){
+					$tm_output .= '<li class="media"><a href="/events">' . __("Show all events", "ticketmachine") . '</a></li>';
+				}
+
+				$tm_output .= '</ul>';
+			$tm_output .= '</div>';
 
 		}elseif($current_page == "boxes"){
 			
