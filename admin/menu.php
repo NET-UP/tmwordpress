29baf6fcae8d85afe6a92ca4d94e701d2c73ab0f
Billie Bell <billie.bell@net-up.de>
2020-01-29 12:01:32 +0100

    test

--- admin/menu.php
+++ admin/menu.php
@@ -9,15 +9,25 @@
 			'TicketMachine',
 			"TicketMachine",
 			'manage_options',
-			'tm_events',
+			'tm_event_manager',
 			'tm_plugin_menu',
 			null,
 			96
 		);
 
-		if($globals->activated) {	
+		if($globals->activated) {
 			add_submenu_page(
-				'tm_events',
+				'tm_event_manager',
+				__('Events', 'ticketmachine'),
+				__('Events', 'ticketmachine'),
+				'manage_options',
+				'tm_event_manager',
+				'tt_overview_page',
+				null,
+				97
+			);	
+			add_submenu_page(
+				'tm_event_manager',
 				__('Events', 'ticketmachine'),
 				__('Events', 'ticketmachine'),
 				'manage_options',
@@ -28,7 +38,7 @@
 			);
 
 			add_submenu_page(
-				'tm_events',
+				'tm_event_manager',
 				__('Settings', 'ticketmachine'),
 				__('Settings', 'ticketmachine'),
 				'manage_options',
@@ -50,11 +60,11 @@
 			#);
 		}else{
 			add_submenu_page(
-				'tm_events',
+				'tm_event_manager',
 				__('Install', 'ticketmachine'),
 				__('Install', 'ticketmachine'),
 				'manage_options',
-				'tm_events',
+				'tm_event_manager',
 				'tm_installation_page',
 				null,
 				100
@@ -64,14 +74,17 @@
 
 	add_filter( 'submenu_file', function($submenu_file){
 		$screen = get_current_screen();
-		if($screen->id === 'tm_events'){
-			$submenu_file = 'tm_events';
+		if($screen->id === 'tm_event_manager'){
+			$submenu_file = 'tm_event_manager';
 		}
 		return $submenu_file;
 	});
 	
 	add_action('admin_menu', 'tm_admin_menu');
 
+	function tm_overview_page(){
+		include( plugin_dir_path( __FILE__) . 'pages/overview.php');
+	}
 	function tm_settings_page(){
 		include( plugin_dir_path( __FILE__ ) . 'pages/settings.php');
 	}
---
+++ admin/pages/overview.php
@@ -0,0 +1,43 @@
+<?php
+	defined("ABSPATH") or die("Permission denied");
+	include( str_replace("/admin/pages", "", plugin_dir_path(__FILE__)) . 'admin/includes/scriptstyles.php');
+	
+    global $globals, $api, $wpdb;
+    $tm_config = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}ticketmachine_config LIMIT 0,1");
+    $tm_config = $tm_config[0];
+
+    if($_GET['code']) {
+        echo "Authorization complete!<br>Code: " . $_GET['code'];
+
+        // Exchange the auth code for an access token
+	    $token = apiRequest($api->token, array(
+            'grant_type' => 'authorization_code',
+            'client_id' => $api->client_id,
+            'client_secret' => $api->client_secret,
+            'code' => $_GET['code'],
+            'redirect_uri' => $api->auth->redirect_uri . "?start_uri=" . $api->auth->start_uri
+        ));
+        $_SESSION['access_token'] = $token['access_token'];
+
+        //print_r($token);
+		$save_array = 
+            array(
+                "activated" => 1,
+            );
+
+        $wpdb->update(
+            $wpdb->prefix . "ticketmachine_config",
+            $save_array,
+            array('id' => $tm_config->id)
+        );
+
+    }else{
+?>
+
+<div class="wrap tm-admin-page">
+	<h1>TicketMachine > <?php echo __('Overview', 'ticketmachine') ?></h1>
+</div>
+
+<?php
+    }
+?>
