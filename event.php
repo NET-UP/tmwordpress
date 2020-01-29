6e625a1b966e7e53f846608b8972c87cea370d60
Billie Bell <mrbilliebell@outlook.com>
2020-01-29 01:37:43 +0100

    test

--- event.php
+++ event.php
@@ -9,45 +9,52 @@
 	$calendar = array();
 	$i = 0;
 
-	foreach($events->result as $event) {
-		$event = (object) $event;
+	
+			
+	if(empty($events)) {	
 		
-		$params = [ "id" => $event->id ];
-		
-		if($event->state['sale_active'] > 0){
+	}else{
+
+		foreach($events->result as $event) {
+			$event = (object) $event;
+			
+			$params = [ "id" => $event->id ];
+			
+			if($event->state['sale_active'] > 0){
+				$event->status_color = "#d4edda";
+				$event->status_text_color = "#155724";
+			}else{
+				$event->status_color = "#f8d7da";
+				$event->status_text_color = "#721c24";
+			}
+
+			//Override for free plugin
 			$event->status_color = "#d4edda";
 			$event->status_text_color = "#155724";
-		}else{
-			$event->status_color = "#f8d7da";
-			$event->status_text_color = "#721c24";
-		}
+			
+			$start = strtotime($event->ev_date) * 1000;
+			$end = strtotime($event->endtime) * 1000;	
 
-		//Override for free plugin
-		$event->status_color = "#d4edda";
-		$event->status_text_color = "#155724";
-		
-		$start = strtotime($event->ev_date) * 1000;
-		$end = strtotime($event->endtime) * 1000;	
-
-		if ($end < (strtotime("midnight", time())*1000)){
-			$event->status_color = "#eeeeee";
-			$event->status_text_color = "#999999";
-		}elseif($i == 0) {
-			$i = 1;
-			$default_date = $start;
+			if ($end < (strtotime("midnight", time())*1000)){
+				$event->status_color = "#eeeeee";
+				$event->status_text_color = "#999999";
+			}elseif($i == 0) {
+				$i = 1;
+				$default_date = $start;
+			}
+			
+			$calendar[] = array(
+				'id' =>$event->id,
+				'title' => $event->ev_name,
+				'url' => "/" . $globals->event_slug . "/?id=" . $event->id,
+				'start' => $start,
+				'end' => $end,
+				'class' => "event-success",
+				'color' => $event->status_color,
+				'textColor' => $event->status_text_color,
+				'defaultDate' => $default_date
+			);
 		}
-		
-		$calendar[] = array(
-			'id' =>$event->id,
-			'title' => $event->ev_name,
-			'url' => "/" . $globals->event_slug . "/?id=" . $event->id,
-			'start' => $start,
-			'end' => $end,
-			'class' => "event-success",
-			'color' => $event->status_color,
-			'textColor' => $event->status_text_color,
-			'defaultDate' => $default_date
-		);
 	}
 
 	$calendarData = $calendar;
