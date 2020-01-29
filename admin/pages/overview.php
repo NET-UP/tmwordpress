5d163d57622092a7287c3234ed18e06eff03edf3
Billie Bell <billie.bell@net-up.de>
2020-01-29 13:13:46 +0100

    Edit event tag bugfix

--- admin/pages/overview.php
+++ admin/pages/overview.php
@@ -35,7 +35,7 @@
     <?php if($_GET['code']) { ?>
 
         <div class="box mb-3">
-            <div class="close"><i class="fas fa-times"></i></div>
+            <a href="#" class="close"><i class="fas fa-times"></i></a>
             <h1 class="text-center"><?php echo __('Welcome to', 'ticketmachine'); ?> <span>TicketMachine</span> <span class="text-primary">1.0!</span></h1>
         </div>
 
