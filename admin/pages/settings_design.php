c2796bc423de90f23d895e3fd026b319cc3e8f13
Billie Bell <billie.bell@net-up.de>
2020-01-29 13:15:50 +0100

    Edit event tag bugfix

--- admin/pages/settings_design.php
+++ admin/pages/settings_design.php
@@ -136,5 +136,4 @@
 <?php 
 	// first check that $hook_suffix is appropriate for your admin page
 	wp_enqueue_style( 'wp-color-picker' );
-	wp_enqueue_script( 'my-script-handle', plugins_url('settings_design.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
 ?>
