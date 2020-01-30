8785004ceb7e0e502a73f5ca97510ba401dc4b1b
Billie Bell <billie.bell@net-up.de>
2020-01-30 12:34:34 +0100

    Edit event tag bugfix

--- globals.php
+++ globals.php
@@ -5,9 +5,9 @@
 	global $wpdb, $globals, $api;
 	$tm_config = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}ticketmachine_config LIMIT 0,1");
 	$tm_config = $tm_config[0];
-	$_SESSION['access_token'] = $tm_config['access_token'];
 
 	$globals = (object)$tm_config;
+	$_SESSION['access_token'] = $globals->access_token;
 	$api = new stdClass();
 	$api->auth = new stdClass();
 
