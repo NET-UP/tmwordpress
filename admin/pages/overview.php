e9cdd6d63a862d76118a54345706460bb7a1f5e0
Billie Bell <billie.bell@net-up.de>
2020-01-30 17:17:38 +0100

    Edit event tag bugfix

--- admin/pages/overview.php
+++ admin/pages/overview.php
@@ -6,7 +6,7 @@
     $tm_config = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}ticketmachine_config LIMIT 0,1");
     $tm_config = $tm_config[0];
 
-    if($_GET['code']) {
+    if($_GET['code'] && !isset($globals->api_refresh_token) && !isset($globals->activated)) {
         //Exchange the auth code for an access token
 	    $token = tmapi_get_access_token($_GET['code'], "new");
 
