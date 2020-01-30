ac7cd464c863db61cf366992579ac002d23061a0
Billie Bell <billie.bell@net-up.de>
2020-01-30 17:39:54 +0100

    Edit event tag bugfix

--- admin/pages/events.php
+++ admin/pages/events.php
@@ -463,9 +463,9 @@ function tm_render_list_page(){
         ?>
         <div class="wrap tm-admin-page">
             <h1 class="dont-display"></h1>
-
-            <h1>TicketMachine <i class="fas fa-angle-right mx-1"></i> <?php echo __('Events', 'ticketmachine'); ?></h1>
-            <a href="?page=tm_events&action=edit" class="button button-secondary ml-2"><?php echo __('Add','ticketmachine'); ?></a>
+            
+            <h1 class="wp-heading-inline mr-3">TicketMachine <i class="fas fa-angle-right mx-1"></i> <?php echo __('Events', 'ticketmachine'); ?></h1>
+            <a href="?page=tm_events&action=edit" class="page-title-action"><?php echo __('Add','ticketmachine'); ?></a>
             
             <!-- Forms are NOT created automatically, so you need to wrap the table in one to use features like bulk actions -->
             <form method="get">
