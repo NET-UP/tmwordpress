9bbee2deb93a1779a6420a770615d7bf6d3af1f1
Billie Bell <billie.bell@net-up.de>
2020-01-30 11:25:59 +0100

    Edit event tag bugfix

--- globals.php
+++ globals.php
@@ -84,8 +84,7 @@
 	$api->auth->data = array(
 		'response_type' => 'code',
 		'client_id' => $api->client_id,
-		'redirect_uri' => $api->auth->redirect_uri . "?start_uri=" . $api->auth->start_uri,
-		'state' => $_SESSION['state'],
+		'redirect_uri' => $api->auth->redirect_uri,
 		'scope' => 'public organizer organizer/event',
 		'locale' => $parsed_locale
 	);
