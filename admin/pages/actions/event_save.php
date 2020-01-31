36a412801180d4757d6841d529f67643333784f3
Billie Bell <billie.bell@net-up.de>
2020-01-31 18:26:03 +0100

    test

--- admin/pages/actions/event_save.php
+++ admin/pages/actions/event_save.php
@@ -37,7 +37,6 @@
     $_POST['vat_id'] = (int)$_POST['vat_id'];
 
     $post = $_POST;
-    $post['description'] = str_replace('\\', '', $post['description']);
 
     $post_json = json_encode($post, JSON_UNESCAPED_SLASHES);
     
