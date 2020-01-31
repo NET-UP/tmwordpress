363a73ef1d59a25f7f50151a63322993534acdd1
Billie Bell <billie.bell@net-up.de>
2020-01-31 16:06:43 +0100

    Edit event tag bugfix

---
+++ admin/partials/sidebar.php
@@ -0,0 +1,18 @@
+<div class="col-xl-3">
+    <h1 class="wp-heading-inline mr-3 mb-3 d-none d-xl-block">
+        <?php echo __('', 'ticketmachine') ?>&nbsp;
+    </h1>
+    <div class="box box-primary mb-3">
+        <div class="box-title bg-primary text-white"><?php echo __("TicketMachine Pro", "ticketmachine"); ?></div>
+        <div class="box-body">
+            <ul class="fa-ul mt-0 ml-4">
+                <li><span class="fa-li"><i class="fas fa-shopping-cart"></i></span> <?php echo __('Sell event tickets', 'ticketmachine') ?></li>
+                <li><span class="fa-li"><i class="fas fa-sync-alt"></i></span> <?php echo __('Recurring events', 'ticketmachine') ?></li>
+                <li><span class="fa-li"><i class="fas fa-chair"></i></span> <?php echo __('Interactive seated events', 'ticketmachine') ?></li>
+                <li><span class="fa-li"><i class="fas fa-percent"></i></span> <?php echo __('Create redeemable coupons', 'ticketmachine') ?></li>
+                <li><span class="fa-li"><i class="fas fa-search-dollar"></i></span> <?php echo __('Price categories for events', 'ticketmachine') ?></li>
+            </ul>
+            <a class="button button-primary btn-primary" href="#"><?php echo __("Apply for Pro", "ticketmachine"); ?></a>
+        </div>
+    </div>
+</div>
