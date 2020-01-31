55e2e718885f02dc9a1109907f1346c12ff00142
Billie Bell <billie.bell@net-up.de>
2020-01-31 16:10:16 +0100

    Edit event tag bugfix

--- admin/pages/events.php
+++ admin/pages/events.php
@@ -507,15 +507,14 @@ function tm_render_list_page(){
                         <?php $EventListTable->display(); ?>
                     </form>
                 </div>
+
+                <?php 
+                    include( str_replace("/admin/pages", "", plugin_dir_path(__FILE__)) . 'admin/partials/sidebar.php');
+                ?>
             </div>
-
-            <?php 
-                include( str_replace("/admin/pages", "", plugin_dir_path(__FILE__)) . 'admin/partials/sidebar.php');
-            ?>
         </div>
+
         <?php
-
     }
-
 }
 ?>
