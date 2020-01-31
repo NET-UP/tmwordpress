f0849cfe9d268a3a3bc8c82e4977552ebecae789
Billie Bell <billie.bell@net-up.de>
2020-01-31 15:59:58 +0100

    Edit event tag bugfix

--- admin/pages/overview.php
+++ admin/pages/overview.php
@@ -103,7 +103,7 @@
             <h1 class="wp-heading-inline mr-3 mb-3">
                 <?php echo __('', 'ticketmachine') ?>&nbsp;
             </h1>
-            <div class="box mb-3">
+            <div class="box box-primary mb-3">
                 <div class="box-title bg-primary text-white"><?php echo __("TicketMachine Pro", "ticketmachine"); ?></div>
                 <div class="box-body">
                     <ul class="fa-ul mt-0 ml-4">
