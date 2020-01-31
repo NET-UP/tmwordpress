250e32700eb908d638a159dfa938a39612814ca8
Billie Bell <billie.bell@net-up.de>
2020-01-31 18:09:12 +0100

    test

--- admin/pages/actions/event_save.php
+++ admin/pages/actions/event_save.php
@@ -41,7 +41,7 @@
     $tm_json = tmapi_event($post_json, "POST");
     $response = (object)$tm_json;
     
-    echo "<pre>" . htmlentities(print_r($post_json, true)) . "</pre>";
+    echo "<pre>" . htmlentities(print_r($_POST, true)) . "</pre>";
 ?>
 
 
