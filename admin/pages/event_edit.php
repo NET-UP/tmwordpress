4fe1ddc2601cf639161d357e676c476168f79882
Billie Bell <mrbilliebell@outlook.com>
2020-02-01 16:15:53 +0100

    test

--- admin/pages/event_edit.php
+++ admin/pages/event_edit.php
@@ -109,7 +109,7 @@
                                     <div id="major-publishing-actions">
                                         <div id="publishing-action">
                                             <span class="spinner"></span>
-                                            <input type="submit" class="button button-primary button-large" id="publish" value="<?php echo isset($event->id) ? __('Create', 'ticketmachine') : __('Update', 'ticketmachine') ?>">
+                                            <input type="submit" class="button button-primary button-large" id="publish" value="<?php echo isset($event->id) ? __('Save', 'ticketmachine') : __('Update', 'ticketmachine') ?>">
                                         </div>
                                         <div class="clear"></div>
                                     </div>
