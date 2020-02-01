366684d0963d30330cd347455c374c739b85589d
Billie Bell <mrbilliebell@outlook.com>
2020-02-01 01:39:57 +0100

    test

--- globals.php
+++ globals.php
@@ -41,7 +41,7 @@
 	$api->client_id = $tm_config->api_client_id;
 	$api->client_secret = $tm_config->api_client_secret;
 	$globals->environment = $tm_config->api_environment;
-	$globals->timezone = "+04:00";
+	$globals->timezone = "-02:00";
 	
 	$globals->search_query = "";
 	if(isset($_GET['q'])){
