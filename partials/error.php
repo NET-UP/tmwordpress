43e487674c6d2e0a267c3f93a128bf928f6a2d7d
Billie Bell <mrbilliebell@outlook.com>
2020-02-01 05:22:06 +0100

    test

--- partials/error.php
+++ partials/error.php
@@ -1,12 +1,18 @@
 <?php
 
-    function tm_alert( $message, $type ) {
+    function tm_alert( $message, $type, $actions = [] ) {
 
         if($type == "error") {
             $type = "danger";
         }
 
         $tm_output = "<div class='alert alert-" . $type . "'>" . $message . "</div>";
+
+        if(!empty($actions)){
+            foreach($actions as $text => $url){
+                $tm_output .= "<a href='" . $url . "' class='btn'>" . $text . "</a>";
+            }
+        }
 
         return $tm_output;
     }
