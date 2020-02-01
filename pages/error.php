55cf5589b336884a35e5e2c0c3c8735f15bb031a
Billie Bell <mrbilliebell@outlook.com>
2020-02-01 05:32:01 +0100

    test

--- pages/error.php
+++ pages/error.php
@@ -2,7 +2,7 @@
 
     function tm_error_page( $message, $actions = [], $title= "Oops!") {
 
-        $tm_puput = "<h1>" . __($title, "ticketmachine") . "</h1>";
+        $tm_output = "<h1>" . __($title, "ticketmachine") . "</h1>";
 
         $tm_output .= tm_alert($message, "error", $actions);
 
