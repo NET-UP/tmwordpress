7b01da9f3e70e9c09a28dd533f7e663577f63dd4
Billie Bell <mrbilliebell@outlook.com>
2020-02-02 20:08:38 +0100

    test

---
+++ partials/_sidebar.php
@@ -0,0 +1,12 @@
+<div class="col-12 col-lg-3 mb-4"><div class="row"><div class=" col-12">
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title">Kategorien</div>
+            
+            <ul class="fa-ul m-2">
+                <li><span class="fa-li"><i class="fas fa-calendar-alt"></i></span><strong>Alle</strong></li>
+            </ul>
+
+        </div>
+    </div>
+</div>
