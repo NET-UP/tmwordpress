f50b9df2a7e9866782b791c8d843b1b80e4f9040
Billie Bell <billie.bell@net-up.de>
2020-02-03 11:55:16 +0100

    test

--- includes/google_calendar.php
+++ includes/google_calendar.php
@@ -2,7 +2,7 @@
 
 $google_calendar_url ='https://calendar.google.com/calendar/r/eventedit?
 &text='. urlencode($event->ev_name) .'
-&dates='. date_i18n("Ymd", strtotime(get_date_from_gmt($event->ev_date))) .'T'. date_i18n("His", strtotime(get_date_from_gmt($event->ev_date))) . 'Z' . '/'. date_i18n("Ymd", strtotime(get_date_from_gmt($event->endtime))).'T'. date_i18n("His", strtotime(get_date_from_gmt($event->endtime))) . 'Z
+&dates='. date_i18n(DATE_ISO8601, strtotime(get_date_from_gmt($event->ev_date))) . '/'. date_i18n(DATE_ISO8601, strtotime(get_date_from_gmt($event->endtime))).'
 &details='. str_replace(' ', '+', wp_strip_all_tags($event->ev_description)) .'
 &location='. urlencode($event->ev_location_name) .'
 &sf=true
