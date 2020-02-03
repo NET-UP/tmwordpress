e72634ddea88c9f708b24aeb0a0bf617adf37866
Billie Bell <billie.bell@net-up.de>
2020-02-03 15:22:54 +0100

    test

--- admin/pages/overview.php
+++ admin/pages/overview.php
@@ -6,31 +6,32 @@
     $tm_config = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}ticketmachine_config LIMIT 0,1");
     $tm_config = $tm_config[0];
 
+    if(!empty($_GET['code']) && empty($globals->api_refresh_token)) {
+        //Exchange the auth code for an access token
+        $token = tmapi_get_access_token($_GET['code'], "new");
+
+        $current_organizer = (object)tmapi_organizers()[0];
+
+        $save_array = array(
+            "activated" => 1,
+            "api_access_token" => $token['access_token'],
+            "api_refresh_token" => $token['refresh_token'],
+            "api_refresh_last" => time(),
+            "api_refresh_interval" => $token['expires_in']/2,
+            "organizer_id" => $current_organizer->id,
+            "organizer" => $current_organizer->og_abbreviation
+        );
+
+
+        $wpdb->update(
+            $wpdb->prefix . "ticketmachine_config",
+            $save_array,
+            array('id' => $tm_config->id)
+        );
+        $globals->activated = 1;
+    }
+    
     if($globals->activated == 1) {
-
-        if(!empty($_GET['code']) && empty($globals->api_refresh_token)) {
-            //Exchange the auth code for an access token
-            $token = tmapi_get_access_token($_GET['code'], "new");
-
-            $current_organizer = (object)tmapi_organizers()[0];
-
-            $save_array = array(
-                "activated" => 1,
-                "api_access_token" => $token['access_token'],
-                "api_refresh_token" => $token['refresh_token'],
-                "api_refresh_last" => time(),
-                "api_refresh_interval" => $token['expires_in']/2,
-                "organizer_id" => $current_organizer->id,
-                "organizer" => $current_organizer->og_abbreviation
-            );
-
-
-            $wpdb->update(
-                $wpdb->prefix . "ticketmachine_config",
-                $save_array,
-                array('id' => $tm_config->id)
-            );
-        }
 ?>
 
 <div class="wrap tm-admin-page">
