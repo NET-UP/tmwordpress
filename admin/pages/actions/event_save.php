7fd9b4e9e79ed814c1f35073691ac7efc28e626f
Billie Bell <billie.bell@net-up.de>
2020-02-03 10:44:48 +0100

    test

--- admin/pages/actions/event_save.php
+++ admin/pages/actions/event_save.php
@@ -11,13 +11,13 @@
         $_POST['tags'] = explode(",", $_POST['tags']);
     }
     if(isset($_POST['entrytime'])) {
-        $_POST['entrytime'] = date(DATE_ISO8601, strtotime($_POST['entrytime']['date'] . $_POST['entrytime']['time']));
+        $_POST['entrytime'] = date_i18n(DATE_ISO8601, strtotime($_POST['entrytime']['date'] . $_POST['entrytime']['time']), true);
     }
     if(isset($_POST['ev_date'])) {
-        $_POST['ev_date'] = date(DATE_ISO8601, strtotime($_POST['ev_date']['date'] . $_POST['ev_date']['time']));
+        $_POST['ev_date'] = date_i18n(DATE_ISO8601, strtotime($_POST['ev_date']['date'] . $_POST['ev_date']['time']), true);
     }
     if(isset($_POST['endtime'])) {
-        $_POST['endtime'] = date(DATE_ISO8601, strtotime($_POST['endtime']['date'] . $_POST['endtime']['time']));
+        $_POST['endtime'] = date_i18n(DATE_ISO8601, strtotime($_POST['endtime']['date'] . $_POST['endtime']['time']), true);
     }
 
     if(isset($_POST['description'])) {
