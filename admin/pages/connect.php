081a238043080b8b47dddeb34eaf0e22251c90a3
Billie Bell <billie.bell@net-up.de>
2020-02-06 09:47:56 +0100

    test

--- admin/pages/connect.php
+++ admin/pages/connect.php
@@ -4,7 +4,7 @@
       include(WP_PLUGIN_DIR . "/ticketmachine/globals.php");
   }
     
-  $authorize_url = "http://apiv2." . $api->environment . "ticketmachine.de/oauth/authorize";
+  $authorize_url = "http://cloud." . $api->environment . "ticketmachine.de/oauth/authorize";
   $authorize_url .= "?";
   $authorize_url .= http_build_query($api->auth->data);
   echo '<p>Not authorized</p>';
