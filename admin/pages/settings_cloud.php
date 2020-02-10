9f9eefbf55f77fe016ebacef7f75de011275f0ee
Billie Bell <billie.bell@net-up.de>
2020-02-10 10:45:48 +0100

    test

--- admin/pages/settings_cloud.php
+++ admin/pages/settings_cloud.php
@@ -7,6 +7,6 @@
     $authorize_url .= http_build_query($api->auth->data);
 ?>
 
-<a class="button button-primary mt-4 px-3 py-md-1" style="font-size:14px" href="<?php echo $authorize_url; ?>">
-    <?php echo __("Sync events", "ticketmachine"); ?>
+<a class="button button-primary mt-4" style="font-size:14px" href="<?php echo $authorize_url; ?>">
+    <i class="fas fa-sync-alt"></i> &nbsp;<?php echo __("Sync events", "ticketmachine"); ?>
 </a>
