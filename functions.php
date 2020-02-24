20227eeea204b9c7990b59d876a13736e32b132a
Billie Bell <billie.bell@net-up.de>
2020-02-24 13:09:14 +0100

    test

--- functions.php
+++ functions.php
@@ -6,34 +6,27 @@
 	}
 
 	function ticketmachine_apiRequest($url, $post=FALSE, $method="GET", $headers=array()) {
-	  $ch = curl_init($url);
-	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	  $headers = [];
 
-	  if($method == "POST") {
-		if($post) {
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_POST, 1);
-			$headers[] = 'Content-Type: application/json';
-		}
-	  }else{
-		if($post) {
-			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-			$headers[] = 'Accept: application/json';
-		}
-	  }
+		$headers[] = 'User-Agent: https://www.ticketmachine.de/';
+		
+		if(isset($_SESSION['access_token']))
+			$headers[] = 'Authorization: Bearer '.$_SESSION['access_token'];
 
-	  $headers[] = 'User-Agent: https://www.ticketmachine.de/';
-	 
-	  if(isset($_SESSION['access_token']))
-		$headers[] = 'Authorization: Bearer '.$_SESSION['access_token'];
-	 
-	  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	  curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-	 
-	  $response = curl_exec($ch);
-	  return json_decode($response, true);
-	  
+		if($method == "POST") {
+			if($post) {
+				$headers[] = 'Content-Type: application/json';
+				$resource = wp_remote_post($url);
+			}
+		}else{
+			if($post) {  
+				$headers[] = 'Accept: application/json';
+				$resource = wp_remote_get($url);	
+			}
+		}
+		
+		$response = wp_remote_retrieve_body( $resource );
+		return json_decode($response, true);
+		
 	}
 
 	/* API Requests */
