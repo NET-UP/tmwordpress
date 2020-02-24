c9c96ba095ddf8235b3f9d40f83d757c8a62662f
Billie Bell <billie.bell@net-up.de>
2020-02-24 15:16:48 +0100

    test

--- admin/partials/sidebar.php
+++ admin/partials/sidebar.php
@@ -9,7 +9,7 @@
                 <li><span class="fa-li"><i class="fas fa-shopping-cart"></i></span> <?php esc_html_e('Sell event tickets', 'ticketmachine') ?></li>
                 <li><span class="fa-li"><i class="fas fa-sync-alt"></i></span> <?php esc_html_e('Recurring events', 'ticketmachine') ?></li>
                 <li><span class="fa-li"><i class="fas fa-percent"></i></span> <?php esc_html_e('Create redeemable coupons', 'ticketmachine') ?></li>
-                <li><span class="fa-li"><i class="fas fa-percent"></i></span> <?php esc_html_e('Update or cancel bookings', 'ticketmachine') ?></li>
+                <li><span class="fa-li"><i class="fas fa-list-ul"></i></i></span> <?php esc_html_e('Update or cancel bookings', 'ticketmachine') ?></li>
                 <li><span class="fa-li"><i class="fas fa-search-dollar"></i></span> <?php esc_html_e('Price categories for events', 'ticketmachine') ?></li>
             </ul>
             <a class="button button-primary btn-primary" target="_blank" href="https://www.ticketmachine.de/kontakt/"><?php esc_html_e("Apply for Pro", "ticketmachine"); ?></a>
