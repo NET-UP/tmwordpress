5aa87f4b42a8a93221cab75c425b43f9497c8ba3
Billie Bell <billie.bell@net-up.de>
2020-02-24 16:35:43 +0100

    test

--- admin/menu.php
+++ admin/menu.php
@@ -4,7 +4,7 @@
 	function ticketmachine_admin_menu () {
 		global $globals, $api;
 
-		if (current_user_can('edit_posts')){
+		if (current_user_can('administrator')){
 
 			include( plugin_dir_path( __FILE__ ) . 'pages/events.php');
 			include( plugin_dir_path( __FILE__ ) . 'pages/categories.php');
