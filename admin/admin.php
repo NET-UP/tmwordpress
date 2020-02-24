7fde1c94cc95c61d8f5b272738ccee312d72933c
Billie Bell <billie.bell@net-up.de>
2020-02-24 16:17:37 +0100

    test

--- admin/admin.php
+++ admin/admin.php
@@ -1,8 +1,9 @@
 <?php
     if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
     
-    if ( current_user_can('editor') ) {
         include( plugin_dir_path( __FILE__ ) . 'menu.php');
+    
+        if ( current_user_can('editor') ) {
     }
 
 ?>
