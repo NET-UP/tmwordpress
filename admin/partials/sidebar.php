ba64a1fe6466c678b6703ec73b6073e72ddc1f5b
Billie Bell <billie.bell@net-up.de>
2020-02-24 15:14:17 +0100

    test

--- admin/partials/sidebar.php
+++ admin/partials/sidebar.php
@@ -8,7 +8,6 @@
             <ul class="fa-ul mt-0 ml-4">
                 <li><span class="fa-li"><i class="fas fa-shopping-cart"></i></span> <?php esc_html_e('Sell event tickets', 'ticketmachine') ?></li>
                 <li><span class="fa-li"><i class="fas fa-sync-alt"></i></span> <?php esc_html_e('Recurring events', 'ticketmachine') ?></li>
-                <li><span class="fa-li"><i class="fas fa-chair"></i></span> <?php esc_html_e('Interactive seated events', 'ticketmachine') ?></li>
                 <li><span class="fa-li"><i class="fas fa-percent"></i></span> <?php esc_html_e('Create redeemable coupons', 'ticketmachine') ?></li>
                 <li><span class="fa-li"><i class="fas fa-search-dollar"></i></span> <?php esc_html_e('Price categories for events', 'ticketmachine') ?></li>
             </ul>
