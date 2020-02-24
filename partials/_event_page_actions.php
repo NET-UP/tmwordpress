c2ab27f0fe41c7a17c50aca2cb037a55c8c5b7fe
Billie Bell <billie.bell@net-up.de>
2020-02-24 11:52:44 +0100

    test

--- partials/_event_page_actions.php
+++ partials/_event_page_actions.php
@@ -12,7 +12,7 @@
         // Append the requested resource location to the URL   
         $url.= $_SERVER['REQUEST_URI'];    
 
-        include(WP_PLUGIN_DIR . "/ticketmachine-event-manager/includes/google_calendar.php";);
+        include(WP_PLUGIN_DIR . "/ticketmachine-event-manager/includes/google_calendar.php");
 
         $ticketmachine_output = '
                 <div class="title-height ticketmachine_actions text-right no-height-mobile mb-3 mb-lg-0">
