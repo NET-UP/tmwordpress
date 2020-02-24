ae134246eb3ffb2c6c5aab911264c66607562dee
Schneider Tobias <schneidert@net-up.net>
2020-02-24 11:23:05 +0100

    bugfix for google and ical

--- includes/google_calendar.php
+++ includes/google_calendar.php
@@ -1,11 +1,12 @@
 <?php
     if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-    $google_calendar_url ='https://calendar.google.com/calendar/r/eventedit?
-    &text='. urlencode($event->ev_name) .'
-    &dates='. date("Ymd", strtotime($event->ev_date)) .'T'. date("His", strtotime($event->ev_date)) . 'Z' . '/'. date("Ymd", strtotime($event->endtime)).'T'. date("His", strtotime($event->endtime)) . 'Z
-    &details='. str_replace(' ', '+', wp_strip_all_tags($event->ev_description)) .'
-    &location='. urlencode($event->ev_location_name) .'
-    &sf=true
-    &output=xml';
+//dont move this!
+$google_calendar_url ='https://calendar.google.com/calendar/r/eventedit?
+&text='. urlencode($event->ev_name) .'
+&dates='. date("Ymd", strtotime($event->ev_date)) .'T'. date("His", strtotime($event->ev_date)) . 'Z' . '/'. date("Ymd", strtotime($event->endtime)).'T'. date("His", strtotime($event->endtime)) . 'Z
+&details='. str_replace(' ', '+', wp_strip_all_tags($event->ev_description)) .'
+&location='. urlencode($event->ev_location_name) .'
+&sf=true
+&output=xml';
 
 ?>
