1674b60f2885744bd4acaa29055dc1f6b8a56492
Billie Bell <billie.bell@net-up.de>
2020-02-24 16:54:14 +0100

    test

--- admin/menu.php
+++ admin/menu.php
@@ -7,7 +7,7 @@
 		include( plugin_dir_path( __FILE__ ) . 'pages/events.php');
 		include( plugin_dir_path( __FILE__ ) . 'pages/categories.php');
 		
-		if( current_user_can('edit_posts') ) {
+		if( current_user_can('edit_pages') ) {
 			add_menu_page(
 				'TicketMachine',
 				"TicketMachine",
