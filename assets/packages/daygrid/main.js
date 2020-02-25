755be9b71cd947c6c3475f6d0cae6a1d0aee8a1a
Billie Bell <billie.bell@net-up.de>
2020-02-25 11:28:26 +0100

    test

--- assets/packages/daygrid/main.js
+++ assets/packages/daygrid/main.js
@@ -34,8 +34,8 @@ Docs & License: https://fullcalendar.io/
 
     function __extends(d, b) {
         extendStatics(d, b);
-        function esc_html__() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new esc_html__());
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     }
 
     var __assign = function() {
