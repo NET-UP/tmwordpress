7deab16c7714b971d38749b4563b7260e3bf0ee4
Billie Bell <billie.bell@net-up.de>
2020-02-26 12:11:09 +0100

    test

--- includes/google_calendar.php
+++ includes/google_calendar.php
@@ -2,6 +2,6 @@
 
     if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
     
-    $google_calendar_url ='https://calendar.google.com/calendar/r/eventedit?&text='. urlencode($event->ev_name) .'&dates='. date("Ymd", strtotime($event->ev_date)) .'T'. date("His", strtotime($event->ev_date)) . 'Z' . '/'. date("Ymd", strtotime($event->endtime)).'T'. date("His", strtotime($event->endtime)) . 'Z&details='. str_replace(' ', '+', wp_strip_all_tags($event->ev_description)) .'&location='. urlencode($event->ev_location_name) .'&sf=true&output=xml';
+    $google_calendar_url ='https://calendar.google.com/calendar/r/eventedit?text='. urlencode($event->ev_name) .'&dates='. date("Ymd", strtotime($event->ev_date)) .'T'. date("His", strtotime($event->ev_date)) . 'Z' . '/'. date("Ymd", strtotime($event->endtime)).'T'. date("His", strtotime($event->endtime)) . 'Z&details='. str_replace(' ', '+', wp_strip_all_tags($event->ev_description)) .'&location='. urlencode($event->ev_location_name) .'&sf=true&output=xml';
 
 ?>
