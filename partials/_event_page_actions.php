5c3999b0f2a7665ba8369aa4e53219863defe2b9
Billie Bell <billie.bell@net-up.de>
2020-02-26 11:16:00 +0100

    test

--- partials/_event_page_actions.php
+++ partials/_event_page_actions.php
@@ -13,16 +13,21 @@
         $url.= $_SERVER['REQUEST_URI'];    
 
         include( str_replace("/partials", "", plugin_dir_path(__FILE__)) . 'includes/google_calendar.php');
+        
 
-        $event->ical = '"Demo Event", "This is an all day event", "Nome, AK", "8/7/2013", "8/7/2013"';
-    
-        $ticketmachine_output = '
+        $ticketmachine_output = "
+        <script>
+            var cal = ics();
+            cal.addEvent('Demo Event', 'This is an all day event', 'Nome, AK', '8/7/2013', '8/7/2013');
+        </script>";
+
+        $ticketmachine_output .= '
                 <div class="title-height ticketmachine_actions text-right no-height-mobile mb-3 mb-lg-0">
                     <a class="btn btn-secondary px-3 mb-1 mb-lg-0 ml-1" href="/' . $globals->events_slug . '">
                         <i class="fas fa-chevron-left"></i> &nbsp; ' . esc_html__('Go back', 'ticketmachine') . '
                     </a>';
                     if ($globals->show_social_media_ical && $globals->show_social_media) {
-                        $ticketmachine_output .= '<a class="btn btn-secondary mb-1 mb-lg-0 ml-1" href="javascript: ics().addEvent(' . $event->ical . ').download()" title="' . esc_attr('Save as iCal', 'ticketmachine') . '">
+                        $ticketmachine_output .= '<a class="btn btn-secondary mb-1 mb-lg-0 ml-1" href="javascript:cal.download()" title="' . esc_attr('Save as iCal', 'ticketmachine') . '">
                             <i class="fas fa-calendar-alt"></i>
                         </a>';
                     }
