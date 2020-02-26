407ded93f8f3b6695c9d51e724dd839342929308
Billie Bell <billie.bell@net-up.de>
2020-02-26 11:10:16 +0100

    test

--- partials/_event_page_actions.php
+++ partials/_event_page_actions.php
@@ -13,8 +13,15 @@
         $url.= $_SERVER['REQUEST_URI'];    
 
         include( str_replace("/partials", "", plugin_dir_path(__FILE__)) . 'includes/google_calendar.php');
+        
 
-        $ticketmachine_output = '
+        $ticketmachine_output = "
+        <script>
+            var cal = ics();
+            cal.addEvent('Demo Event', 'This is an all day event', 'Nome, AK', '8/7/2013', '8/7/2013');
+        </script>";
+
+        $ticketmachine_output .= '
                 <div class="title-height ticketmachine_actions text-right no-height-mobile mb-3 mb-lg-0">
                     <a class="btn btn-secondary px-3 mb-1 mb-lg-0 ml-1" href="/' . $globals->events_slug . '">
                         <i class="fas fa-chevron-left"></i> &nbsp; ' . esc_html__('Go back', 'ticketmachine') . '
@@ -56,12 +63,6 @@
                     }
         $ticketmachine_output .= '
                 </div>';
-
-                $ticketmachine_output .= "
-                    <script>
-                        var cal = ics();
-                        cal.addEvent('Demo Event', 'This is an all day event', 'Nome, AK', '8/7/2013', '8/7/2013');
-                    </script>";
 
         return $ticketmachine_output;
 	}
