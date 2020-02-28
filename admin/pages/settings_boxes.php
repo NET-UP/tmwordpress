c07335d1049f4c9723a320197a189c0012741e18
Billie Bell <mrbilliebell@outlook.com>
2020-02-28 16:52:31 +0100

    test

--- admin/pages/settings_boxes.php
+++ admin/pages/settings_boxes.php
@@ -21,7 +21,7 @@
 				$post->show_boxes = 0;
 			}
 
-			if(in_array($post->event_grouping, array("Month", "Year","None")){
+			if(in_array($post->event_grouping, array("Month", "Year","None"))){
 				$post->event_grouping = sanitize_text_field($post->event_grouping)
 			}else{
 				$errors[] =  'Sorry, your groups did not verify.';
