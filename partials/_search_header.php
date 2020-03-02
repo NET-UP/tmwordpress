6ce374716b1f4d707ee6cc02756f2848a6a133be
Billie Bell <billie.bell@net-up.de>
2020-03-02 09:56:00 +0100

    test

--- partials/_search_header.php
+++ partials/_search_header.php
@@ -13,8 +13,6 @@
             $params .= "&tag=" . $globals->tag;
         }
 
-        print_r($params);
-
         $ticketmachine_output .= "<form class='mb-3'>";
 
             if(isset($_GET['display'])){
