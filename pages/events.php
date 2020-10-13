3a2b33d337055f4415841f266f9b40154458f4e4
Billie Bell <billie.bell@net-up.de>
2020-10-13 11:46:06 +0200

    new modern list NU

--- pages/events.php
+++ pages/events.php
@@ -94,20 +94,21 @@
 					foreach($events as $event){
 						$event = (object)$event;
 
-						$ticketmachine_output .= '<li class="media mx-0 mt-2">';
+						$ticketmachine_output .= '<li class="media mx-0 mt-2 p-3">';
 
 						if(isset($atts['show_image']) && $atts['show_image'] > 0){
 							$ticketmachine_output .= '<a class="mr-3 media-img" href="/' . esc_html($tm_globals->event_slug) . '?id=' . $event->id . '" style="background-image:url('. $event->event_img_url .')"></a>';
 						}
 											
 							$ticketmachine_output .= '<div class="media-body">';
-							$ticketmachine_output .= '<h5 class="mt-0 mb-1"><a class="tm-list-title" href="/' . esc_html($tm_globals->event_slug) . '?id=' . $event->id . '">' . $event->ev_name . '</a></h5>';
 
 							if(isset($atts['show_date']) && $atts['show_date'] > 0){
 								$ticketmachine_output .= '
 								<div class="card-meta-tag"><i class="far fa-calendar-alt tm-icon" aria-hidden="true"></i> &nbsp;'. ticketmachine_i18n_date("d.m.Y", $event->ev_date) .'</div> 
 								<div class="card-meta-tag"><i class="far fa-clock tm-icon" aria-hidden="true"></i> &nbsp;'. ticketmachine_i18n_date("H:i", $event->ev_date) .'</div>';
 							}
+
+							$ticketmachine_output .= '<h5 class="mt-0 mb-1"><a class="tm-list-title" href="/' . esc_html($tm_globals->event_slug) . '?id=' . $event->id . '">' . $event->ev_name . '</a></h5>';
 							
 							if(isset($atts['show_description']) && $atts['show_description'] > 0){
 								if(empty($atts['description_length'])){
