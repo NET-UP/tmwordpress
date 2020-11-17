3965d8cc9aaac1a6b817fe3c5132e8b99953c64e
Billie Bell <billie.bell@net-up.de>
2020-11-17 18:11:55 +0100

    community events stuff

--- admin/pages/event_edit.php
+++ admin/pages/event_edit.php
@@ -38,7 +38,18 @@
 
     if(!empty($_GET['id'])){
         $params = [ "id" => absint($_GET['id']) ];
-        $event = (object)ticketmachine_tmapi_event($params);
+        if(!empty($_GET['mode'] && $_GET['mode'] == "community")) {
+            
+            if (!function_exists('is_plugin_active')) {
+                include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+            }
+            if (is_plugin_active('ticketmachine-community-events/ticketmachine-community-events.php')){
+                $table = $wpdb->prefix . "ticketmachine_events";
+                $event = (object)$wpdb->get_row( "SELECT * FROM $table WHERE id = " . $params['id'] );
+            }
+        }else{
+            $event = (object)ticketmachine_tmapi_event($params);
+        }
     }
 ?>
 
