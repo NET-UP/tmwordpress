27577f167f3a7ef8d09b04d4773af8daeaa720b6
Billie Bell <billie.bell@net-up.de>
2020-11-19 17:42:50 +0100

    community events stuff

--- partials/_event_organizer_details.php
+++ partials/_event_organizer_details.php
@@ -1,7 +1,8 @@
 <?php
 	if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
     function ticketmachine_event_organizer_details ( $organizer, $tm_globals ) {
-        $ticketmachine_output = '<div class="card mb-3">'; 
+        $ticketmachine_output = '<div class="row"><div class="col-12"><div class="card mb-3"> 
+                                    <div class="row card-body position-relative">';
                                     
                 $ticketmachine_output .=       '<div class="col-12">
                                         <h4 class="d-inline-block">'. esc_html__("Organizer details", "ticketmachine-event-manager") .'</h4>
@@ -11,7 +12,7 @@
                                         <div>'. esc_html($event->event_location['street']) .' '. esc_html($event->event_location['house_number']) .'</div>
                                     </div>';
 
-            $ticketmachine_output .=   '</div>
+            $ticketmachine_output .=   '</div></div></div>
                        </div>';
         return $ticketmachine_output;
     }
