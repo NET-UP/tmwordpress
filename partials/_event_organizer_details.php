b926673989cb6f16f87b000faeea45d1140bdf9f
Billie Bell <billie.bell@net-up.de>
2020-11-19 17:45:32 +0100

    community events stuff

--- partials/_event_organizer_details.php
+++ partials/_event_organizer_details.php
@@ -4,13 +4,22 @@
         $ticketmachine_output = '<div class="row"><div class="col-12"><div class="card mb-3"> 
                                     <div class="row card-body position-relative">';
                                     
-                $ticketmachine_output .=       '<div class="col-12">
-                                        <h4 class="d-inline-block">'. esc_html__("Organizer details", "ticketmachine-event-manager") .'</h4>
-                                        <br>
-                                        <div>'. esc_html($organizer->og_name) .'</div>
-                                        <div>'. esc_html($organizer->zip) .' '. esc_html($organizer->city) .'</div>
-                                        <div>'. esc_html($event->event_location['street']) .' '. esc_html($event->event_location['house_number']) .'</div>
-                                    </div>';
+                $ticketmachine_output .= '<div class="col-sm-6">
+                                            <h4 class="d-inline-block">'. esc_html__("Organizer", "ticketmachine-event-manager") .'</h4>
+                                            <br>
+                                            <div><strong>'. esc_html($organizer->og_name) .'</strong></div>
+                                            <div>'. esc_html($organizer->street) .' '. esc_html($organizer->og_house_number) .'</div>
+                                            <div>'. esc_html($organizer->zip) .' '. esc_html($organizer->city) .'</div>
+                                            <div>'. esc_html($event->event_location['street']) .' '. esc_html($event->event_location['house_number']) .'</div>
+                                        </div>';
+                                    
+                $ticketmachine_output .= '<div class="col-sm-6">
+                                            <h4 class="d-inline-block">'. esc_html__("Contact", "ticketmachine-event-manager") .'</h4>
+                                            <br>
+                                            <div><strong>'. esc_html($organizer->og_name) .'</strong></div>
+                                            <div>'. esc_html($organizer->zip) .' '. esc_html($organizer->city) .'</div>
+                                            <div>'. esc_html($event->event_location['street']) .' '. esc_html($event->event_location['house_number']) .'</div>
+                                        </div>';
 
             $ticketmachine_output .=   '</div></div></div>
                        </div>';
--- partials/_event_page_details.php
+++ partials/_event_page_details.php
@@ -26,8 +26,9 @@
                                         <h4 class="d-inline-block">'. esc_html__("Location", "ticketmachine-event-manager") .'</h4>
                                         <br>
                                         <div>'. esc_html($event->ev_location_name) .'</div>
-                                        <div>'. esc_html($event->event_location['zip']) .' '. esc_html($event->event_location['city']) .'</div>
                                         <div>'. esc_html($event->event_location['street']) .' '. esc_html($event->event_location['house_number']) .'</div>
+                                        <div>'. esc_html($event->event_location['zip']) .' '. esc_html($event->event_location['city']) .'</div>
+                                        <div>'. esc_html($event->event_location['country']) .'</div>
                                     </div>';
             }      
 
