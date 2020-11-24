df1817a8e71a2bd704cfc0fb8c9dc8be95ba2d67
Billie Bell <billie.bell@net-up.de>
2020-11-24 15:59:07 +0100

    community events stuff

--- admin/pages/settings_general.php
+++ admin/pages/settings_general.php
@@ -101,7 +101,7 @@
 		</tr>
 
 		<label class="hidden" style="display:none;">
-			<?php echo esc_html__('I have read and agree to the', 'ticketmachine-event-manager') . ' ' . esc_html__('Privacy Policy', 'ticketmachine-event-manager'); ?>
+			<?php echo esc_html__('I have read and understood the', 'ticketmachine-event-manager') . esc_html__('Privacy Policy', 'ticketmachine-event-manager') . esc_html__('and agree with the contents.', 'ticketmachine-event-manager'); ?>
 		</label>
 
 	</tbody>
