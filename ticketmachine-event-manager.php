183edb3440a10f662a853618c8fb5b14cf99fe98
Billie Bell <mrbilliebell@outlook.com>
2021-04-02 17:22:33 +0200

    Card image ratios

--- ticketmachine-event-manager.php
+++ ticketmachine-event-manager.php
@@ -274,6 +274,7 @@
                     box_text_color varchar(64) DEFAULT '#222' NOT NULL,
                     box_meta_color varchar(64) DEFAULT '#222' NOT NULL,
                     box_header_color varchar(64) DEFAULT '#222' NOT NULL,
+                    box_border_color varchar(64) DEFAULT '#CCC' NOT NULL,
                     date_text_color varchar(64) DEFAULT '#ee7d26' NOT NULL,
                     price_text_color varchar(64) DEFAULT '#ee7d26' NOT NULL,
                     button_primary_background_color varchar(64) DEFAULT '#ee7d26' NOT NULL,
