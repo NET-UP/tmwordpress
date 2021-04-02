186a3c5b38fa9a3e054f77c9718016b4e8a84873
Billie Bell <mrbilliebell@outlook.com>
2021-04-02 16:52:03 +0200

    Card image ratios

--- assets/css/custom.php
+++ assets/css/custom.php
@@ -14,66 +14,66 @@
             color: ". $ticketmachine_design->link_text_color_hover .";
             text-decoration:underline !important;
         }
-        .card-img-top:focus {
+        body .ticketmachine_page .card-img-top:focus {
             border:2px solid ". $ticketmachine_design->button_primary_background_color ." !important;
         }
 
         
-        .ticketmachine_page #calendar .fc-event:focus {
+        body .ticketmachine_page #calendar .fc-event:focus {
             box-shadow: 0px 0px 1px 2px ". $ticketmachine_design->button_primary_background_color ." !important;
         }
 
-        .ticketmachine_page .card {
+        body .ticketmachine_page .card {
             background: ". $ticketmachine_design->container_background_color ." !important;
         }
 
-        .ticketmachine_page .card-date {
+        body .ticketmachine_page .card-date {
             color: ". $ticketmachine_design->date_text_color ." !important;
         }
 
-        .ticketmachine_page .card-price {
+        body .ticketmachine_page .card-price {
             color: ". $ticketmachine_design->price_text_color ." !important;
         }
 
-        .ticketmachine_page .btn-primary,
-        .ticketmachine_page .btn-primary:visited,
-        .ticketmachine_page .btn-primary:not(:disabled):not(.disabled).active,
-        .ticketmachine_page .btn-primary:not(:disabled):not(.disabled):active,
+        body .ticketmachine_page .btn-primary,
+        body .ticketmachine_page .btn-primary:visited,
+        body .ticketmachine_page .btn-primary:not(:disabled):not(.disabled).active,
+        body .ticketmachine_page .btn-primary:not(:disabled):not(.disabled):active,
         .show>.ticketmachine_page .btn-primary.dropdown-toggle,
-        .ticketmachine_page .btn-primary.disabled,
-        .ticketmachine_page .btn-primary:disabled {
+        body .ticketmachine_page .btn-primary.disabled,
+        body .ticketmachine_page .btn-primary:disabled {
             background-color: ". $ticketmachine_design->button_primary_background_color ." !important;
             color: ". $ticketmachine_design->button_primary_text_color ." !important;
             border-color: ". $ticketmachine_design->button_primary_border_color ." !important;
         }
 
-        .ticketmachine_page .btn-primary:hover,
-        .ticketmachine_page .btn-primary:active,
-        .ticketmachine_page .btn-primary:focus {
+        body .ticketmachine_page .btn-primary:hover,
+        body .ticketmachine_page .btn-primary:active,
+        body .ticketmachine_page .btn-primary:focus {
             background-color: ". $ticketmachine_design->button_primary_background_color_hover ." !important;
             color: ". $ticketmachine_design->button_primary_text_color_hover ." !important;
             border-color: ". $ticketmachine_design->button_primary_border_color_hover ." !important;
         }
 
-        .btn-secondary,
-        .btn-secondary:visited,
-        .btn-secondary:not(:disabled):not(.disabled).active,
-        .btn-secondary:not(:disabled):not(.disabled):active {
+        body .btn-secondary,
+        body .btn-secondary:visited,
+        body .btn-secondary:not(:disabled):not(.disabled).active,
+        body .btn-secondary:not(:disabled):not(.disabled):active {
             background-color: ". $ticketmachine_design->button_secondary_background_color ." !important;
             color: ". $ticketmachine_design->button_secondary_text_color ." !important;
             border-color: ". $ticketmachine_design->button_secondary_border_color ." !important;
         }
 
-        .btn-secondary:hover,
-        .btn-secondary:active,
-        .btn-secondary:focus,
+        body .btn-secondary:hover,
+        body .btn-secondary:active,
+        body .btn-secondary:focus,
         .show>.btn-secondary.dropdown-toggle {
             background-color: ". $ticketmachine_design->button_secondary_background_color_hover ." !important;
             color: ". $ticketmachine_design->button_secondary_text_color_hover ." !important;
             border-color: ". $ticketmachine_design->button_secondary_border_color_hover ." !important;
         }
 
-        .spinner-border {
+        body .spinner-border {
             border-color: ". $ticketmachine_design->button_primary_background_color ." !important;
             color: ". $ticketmachine_design->button_primary_background_color ." !important;
             border-right-color: transparent !important;
