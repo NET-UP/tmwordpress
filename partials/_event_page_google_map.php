15668c02a8c7950cd5e2633223f2c007b631b002
Billie Bell <mrbilliebell@outlook.com>
2021-05-26 11:01:24 +0200

    test

--- partials/_event_page_google_map.php
+++ partials/_event_page_google_map.php
@@ -6,7 +6,7 @@
 
         if(isset($event->has_location) && $event->has_location == 1){
             if (isset($_COOKIE["allow_google_maps"])){
-                $ticketmachine_output .= "<iframe width='100%' height='300' id='mapcanvas' src='" . esc_url("https://maps.google.com/maps?q=". urlencode($event->ev_location_name . ' ' . $event->event_location['street'] . ' ' . $event->event_location['house_number'] . ' ' . $event->event_location['zip'] . ' ' . $event->event_location['city'] . ' ' . $event->event_location['country'] ) ."&z=10&ie=UTF8&iwloc=&output=embed") . "' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'>
+                $ticketmachine_output .= "<iframe width='100%' height='300' id='mapcanvas' src='" . esc_url("https://google.com/maps?q=". urlencode($event->ev_location_name . ' ' . $event->event_location['street'] . ' ' . $event->event_location['house_number'] . ' ' . $event->event_location['zip'] . ' ' . $event->event_location['city'] . ' ' . $event->event_location['country'] ) ."&z=10&ie=UTF8&iwloc=&output=embed") . "' frameborder='0' scrolling='no' marginheight='0' marginwidth='0'>
                                 </iframe>";            
                 $ticketmachine_output .= "<a class='disallow-google-maps float-right' href='#'>" . esc_html__('Disallow Google Maps', 'ticketmachine-event-manager') . "</a>";
             }else{
