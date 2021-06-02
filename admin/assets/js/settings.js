16014e6d06dc87132338d8b8050ad57217502bd8
Billie Bell <mrbilliebell@outlook.com>
2021-06-03 00:31:19 +0200

    test

--- admin/assets/js/settings.js
+++ admin/assets/js/settings.js
@@ -14,16 +14,16 @@ jQuery(document).ready(function($){
 
 function isFullDayEvent() {
     if(jQuery("#full_day").is(':checked')) {
-        jQuery('input[name="entrytime[date]"').val(jQuery('input[name="ev_date[date]"').val()).attr("disabled", true);
-        jQuery('input[name="entrytime[time]"').val("00:00").attr("disabled", true);
-        jQuery('input[name="ev_date[time]"').val("00:00").attr("disabled", true);
-        jQuery('input[name="endtime[date]"').val(jQuery('input[name="ev_date[date]"').val()).attr("disabled", true);
-        jQuery('input[name="endtime[time]"').val("23:59").attr("disabled", true);
+        jQuery('input[name="entrytime[date]"').val(jQuery('input[name="ev_date[date]"').val()).attr("readonly", true);
+        jQuery('input[name="entrytime[time]"').val("00:00").attr("readonly", true);
+        jQuery('input[name="ev_date[time]"').val("00:00").attr("readonly", true);
+        jQuery('input[name="endtime[date]"').val(jQuery('input[name="ev_date[date]"').val()).attr("readonly", true);
+        jQuery('input[name="endtime[time]"').val("23:59").attr("readonly", true);
     }else{
-        jQuery('input[name="entrytime[date]"').attr("disabled", false);
-        jQuery('input[name="entrytime[time]"').attr("disabled", false);
-        jQuery('input[name="ev_date[time]"').attr("disabled", false);
-        jQuery('input[name="endtime[date]"').attr("disabled", false);
-        jQuery('input[name="endtime[time]"').attr("disabled", false);
+        jQuery('input[name="entrytime[date]"').attr("readonly", false);
+        jQuery('input[name="entrytime[time]"').attr("readonly", false);
+        jQuery('input[name="ev_date[time]"').attr("readonly", false);
+        jQuery('input[name="endtime[date]"').attr("readonly", false);
+        jQuery('input[name="endtime[time]"').attr("readonly", false);
     }
 }
