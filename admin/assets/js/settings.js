7ed3daa5f1bb25f032645124ed3fd4eaee9e4329
Billie Bell <mrbilliebell@outlook.com>
2021-06-03 00:15:15 +0200

    test

--- admin/assets/js/settings.js
+++ admin/assets/js/settings.js
@@ -4,4 +4,24 @@ jQuery(document).ready(function($){
     jQuery('.tm-admin-page').on("click", ".close", function(){
         jQuery(this).parents(".box").remove();
     });
+    
+    jQuery('.tm-admin-page').on("change", "#full_day", function(){
+        isFullDayEvent();
+    });
+    
+    isFullDayEvent();
 });
+
+function isFullDayEvent() {
+    if(jQuery("#full_day").attr("checked") == true) {
+        jQuery('input[name="entrytime[date]"').val(jQuery('input[name="ev_date[date]"').val()).attr("disabled", true);
+        jQuery('input[name="entrytime[time]"').val("00:00").attr("disabled", true);
+        jQuery('input[name="endtime[date]"').val(jQuery('input[name="ev_date[date]"').val()).attr("disabled", true);
+        jQuery('input[name="endtime[time]"').val("00:00").attr("disabled", true);
+    }else{
+        jQuery('input[name="entrytime[date]"').attr("disabled", false);
+        jQuery('input[name="entrytime[time]"').attr("disabled", false);
+        jQuery('input[name="endtime[date]"').attr("disabled", false);
+        jQuery('input[name="endtime[time]"').attr("disabled", false);
+    }
+}
