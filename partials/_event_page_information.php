8ed035e9b4b57e2f4e9c889ae4ba94f6920048ea
Billie Bell <billie.bell@net-up.de>
2021-06-23 10:18:06 +0200

    map fax

--- partials/_event_page_information.php
+++ partials/_event_page_information.php
@@ -30,9 +30,9 @@
                         if(isset($event->has_location) && $event->has_location == 1){                       
                             $ticketmachine_output .= '<div class="card-meta-tag"><i class="fas fa-map-marker-alt tm-icon"></i> &nbsp; ';
                                 if(isset($event->has_location_link) && $event->has_location_link == 1){        
-                                    $ticketmachine_output = '<a aria-label="' . esc_attr__("Event Location", 'ticketmachine-event-manager') . ': ' . esc_html($event->ev_location_name) . '" href="' . esc_url($tm_globals->map_query_url . urlencode($event->ev_location_name . " " .$event->event_location['street'] . " " . $event->event_location["house_number"] . " " . $event->event_location["zip"] . " " . $event->event_location["city"] . " " . $event->event_location["country"] )) . '" target="_blank" title="' . esc_attr__("Event Location", 'ticketmachine-event-manager') . ': ' . esc_html($event->ev_location_name) . '">' . esc_html($event->ev_location_name) . '</a>';
+                                    $ticketmachine_output .= '<a aria-label="' . esc_attr__("Event Location", 'ticketmachine-event-manager') . ': ' . esc_html($event->ev_location_name) . '" href="' . esc_url($tm_globals->map_query_url . urlencode($event->ev_location_name . " " .$event->event_location['street'] . " " . $event->event_location["house_number"] . " " . $event->event_location["zip"] . " " . $event->event_location["city"] . " " . $event->event_location["country"] )) . '" target="_blank" title="' . esc_attr__("Event Location", 'ticketmachine-event-manager') . ': ' . esc_html($event->ev_location_name) . '">' . esc_html($event->ev_location_name) . '</a>';
                                 }
-                            $ticketmachine_output = '</div>';
+                            $ticketmachine_output .= '</div>';
                         }
                        
 
