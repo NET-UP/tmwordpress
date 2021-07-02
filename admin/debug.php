18dfc9a253db658538f0fc55ebb99dc4c4620cff
Billie Bell <mrbilliebell@outlook.com>
2021-07-02 16:17:37 +0200

    debug

--- admin/debug.php
+++ admin/debug.php
@@ -10,7 +10,7 @@
         foreach ($rows as $row) {
             echo "[" . date("c", $row->log_time) . "] - " . $row->log_type . "\n";
             $message = json_decode($row->log_message );
-            echo $message . "]\n\n";
+            echo $message . "\n\n";
         }
     }
 ?>
