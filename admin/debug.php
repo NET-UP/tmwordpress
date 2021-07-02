2434c2ef36b43de994dc98ed2d6abcea340948c6
Billie Bell <mrbilliebell@outlook.com>
2021-07-02 16:08:05 +0200

    debug

--- admin/debug.php
+++ admin/debug.php
@@ -7,7 +7,7 @@
     $rows = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}ticketmachine_log");
 
     foreach ($rows as $row) {
-        echo "[" . $row['error_time'] . "]";
+        echo "[" . $row->error_time . "]";
     }
 ?>
 
