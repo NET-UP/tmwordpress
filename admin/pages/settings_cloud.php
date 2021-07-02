fcb896878681b59eeda47369fc94b0d1bef34748
Billie Bell <mrbilliebell@outlook.com>
2021-07-02 14:56:42 +0200

    logging

--- admin/pages/settings_cloud.php
+++ admin/pages/settings_cloud.php
@@ -6,7 +6,19 @@
     $ticketmachine_config = $ticketmachine_config[0];
 
     if(!empty($_GET['debug_log'])) {
-        echo "hi";
+        $file = "test.txt";
+        $txt = fopen($file, "w") or die("Unable to open file!");
+        fwrite($txt, "lorem ipsum");
+        fclose($txt);
+
+        header('Content-Description: File Transfer');
+        header('Content-Disposition: attachment; filename='.basename($file));
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        header("Content-Type: text/plain");
+        readfile($file);
     }
 
     if(!empty($_GET['code'])) {
