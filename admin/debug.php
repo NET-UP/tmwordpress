b89039014f8e8c874e716ee01dab54ce5bfd295f
Billie Bell <mrbilliebell@outlook.com>
2021-07-02 16:05:33 +0200

    debug

--- admin/debug.php
+++ admin/debug.php
@@ -1,5 +1,13 @@
 <?php 
     header("Content-Type: text/plain"); 
     require_once('../../../../wp-load.php');
+
+    global $wpdb;
+
+    $rows = $wpdb->get_results( "SELECT * FROM ticketmachine_log");
+
+    foreach ($rows as $row) {
+        echo "[]";
+    }
 ?>
 
