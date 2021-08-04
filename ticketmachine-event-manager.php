2e69d17d0ecbb97af6750c99c239c711b7c0cc5c
Billie Bell <mrbilliebell@outlook.com>
2021-08-04 13:42:17 +0200

    testing save

--- ticketmachine-event-manager.php
+++ ticketmachine-event-manager.php
@@ -542,11 +542,8 @@
 				'method'  => 'POST',
 				'timeout' => 45,
 				'headers' => $headers,
-				'body' 	  => str_replace("\r\n", "<br>", str_replace("&nbsp;", "", str_replace('\"', "'", json_encode($tm_post, JSON_UNESCAPED_SLASHES))))
+				'body' 	  => str_replace("\'", "'", str_replace("\r\n", "<br>", str_replace("&nbsp;", "", str_replace('\"', "'", json_encode($tm_post, JSON_UNESCAPED_SLASHES)))))
 			));
-			print_r("<pre>");
-			print_r(str_replace("\r\n", "<br>", str_replace("&nbsp;", "", str_replace('\"', "'", json_encode($tm_post, JSON_UNESCAPED_SLASHES)))));
-			print_r("<pre>");
 			$log_resource = $resource;
 			$log_resource['headers'] = (array)$log_resource["headers"];
 			$log = array(
