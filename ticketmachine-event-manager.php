b9be28f100bdb38fe98f141734dbd0bfdd23cca6
Billie Bell <mrbilliebell@outlook.com>
2021-09-30 18:48:17 +0200

    Version check 5.8.1

--- ticketmachine-event-manager.php
+++ ticketmachine-event-manager.php
@@ -777,9 +777,9 @@
 			);
 		}
 
-		print_r($tm_api->auth->code);
-		
 		$token = ticketmachine_apiRequest($tm_api->token, $tm_api->auth->code, "POST");
+
+		print_r($token);
 
 		$tm_globals->api_access_token = $token['access_token'];
 
