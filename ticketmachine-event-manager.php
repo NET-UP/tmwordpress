2071179efba9c3f76a47f58d7ab903dca50b8085
Billie Bell <billie.bell@net-up.de>
2022-02-23 10:06:20 +0100

    test

--- ticketmachine-event-manager.php
+++ ticketmachine-event-manager.php
@@ -636,6 +636,12 @@
 		}
 
 		print_r($tm_url);
+		echo "<br>";
+		echo "<br>";
+		print_r($tm_post);
+		echo "<br>";
+		echo "<br>";
+		print_r($tm_headers);
 
 		if(isset($tm_url_only) && $tm_url_only == 1) {
 			return $tm_url;
