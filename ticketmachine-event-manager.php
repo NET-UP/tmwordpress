c1f9369a12e2097f74ee79500ad0656094c3c413
Billie Bell <billie.bell@net-up.de>
2022-03-21 16:20:26 +0100

    test

--- ticketmachine-event-manager.php
+++ ticketmachine-event-manager.php
@@ -933,7 +933,7 @@
                 $event->status_color = "#d4edda";
                 $event->status_text_color = "#155724";
                 
-                $start = strtotime(($event->ev_date) * 1000;
+                $start = strtotime($event->ev_date) * 1000;
                 $end = strtotime($event->endtime) * 1000;	
 
                 if ($end < (strtotime("midnight", time())*1000)){
