5fd15a937c786ce19b446fedeb2b512918311feb
Billie Bell <billie.bell@net-up.de>
2022-05-04 17:45:26 +0200

    test

--- widgets/event_list.php
+++ widgets/event_list.php
@@ -107,27 +107,26 @@
                     }
 
                     $ticketmachine_output .= '</ul>';
+                    
+                    $ticketmachine_output .= '<div>';
+
+                    $query = $_GET;
+                    if($meta['has_previous_page']) {
+                        $query['pg'] = $params['pg']-1;
+                        $query_result = http_build_query($query);
+                        $ticketmachine_output .= "<a class='btn btn-secondary mr-3' href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "'>" . __("Previous page", "ticketmachine-event-manager") . "</a>";
+                    }
+
+                    if($meta['next'] < $meta['count_filtered']) {
+                        $query['pg'] = $params['pg']+1;
+                        $query_result = http_build_query($query);
+                        $ticketmachine_output .= "<a class='btn btn-secondary'  href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "'>" . __("Next page", "ticketmachine-event-manager") . "</a>";
+                    }
+                    $ticketmachine_output .= '</div>';
 
                 $ticketmachine_output .= '</div>';
 
             }
-
-            $query = $_GET;
-
-            $ticketmachine_output .= '<div>';
-
-            if($meta['has_previous_page']) {
-                $query['pg'] = $params['pg']-1;
-                $query_result = http_build_query($query);
-                $ticketmachine_output .= "<a class='btn btn-secondary mr-3' href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "'>" . __("Previous page", "ticketmachine-event-manager") . "</a>";
-            }
-
-            if($meta['next'] < $meta['count_filtered']) {
-                $query['pg'] = $params['pg']+1;
-                $query_result = http_build_query($query);
-                $ticketmachine_output .= "<a class='btn btn-secondary'  href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "'>" . __("Next page", "ticketmachine-event-manager") . "</a>";
-            }
-            $ticketmachine_output .= '<div class="pagination">';
 
             if($isWidget == 1){
                 $ticketmachine_output .= "</div></div>";
