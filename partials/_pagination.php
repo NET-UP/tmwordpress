b165d743647120f6e3c74206286d1c976b159d27
Billie Bell <billie.bell@net-up.de>
2022-05-20 11:41:58 +0200

    test

--- partials/_pagination.php
+++ partials/_pagination.php
@@ -3,22 +3,25 @@
     function ticketmachine_pagination ( $meta, $params ) {
         ticketmachine_debug($meta);
         $query = $_GET;
-        $ticketmachine_output = '<div class="float-right btn-group">';
-        if($meta['has_previous_page']) {
-            $query['pg'] = $params['pg']-1;
-            $query_result = http_build_query($query);
-            $ticketmachine_output .= "<a class='btn btn-secondary' href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "'><i class='fas fa-angle-left'></i></a>";
+        if($params['per_page'] < $meta['count_all']) {
+            $ticketmachine_output = '<div class="float-right btn-group">';
+
+            if($meta['has_previous_page']) {
+                $query['pg'] = $params['pg']-1;
+                $query_result = http_build_query($query);
+                $ticketmachine_output .= "<a class='btn btn-secondary' href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "'><i class='fas fa-angle-left'></i></a>";
+            }
+
+            $ticketmachine_output .= "<";
+
+            if($meta['has_next_page'] && $meta['next'] <  $meta['count_filtered']) {
+                $query['pg'] = $params['pg']+1;
+                $query_result = http_build_query($query);
+                $ticketmachine_output .= "<a class='btn btn-secondary' href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "'><i class='fas fa-angle-right'></i></a>";
+            }
+
+            $ticketmachine_output .= '</div>';
         }
-
-        $ticketmachine_output .= "<";
-
-        if($meta['has_next_page'] && $meta['next'] <  $meta['count_filtered']) {
-            $query['pg'] = $params['pg']+1;
-            $query_result = http_build_query($query);
-            $ticketmachine_output .= "<a class='btn btn-secondary' href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "'><i class='fas fa-angle-right'></i></a>";
-        }
-
-        $ticketmachine_output .= '</div>';
 
         return $ticketmachine_output;
 	}
