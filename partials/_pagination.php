cb0a78ad53eb8d6137a9a633b9f88205a0224325
Billie Bell <billie.bell@net-up.de>
2022-05-20 11:45:28 +0200

    test

--- partials/_pagination.php
+++ partials/_pagination.php
@@ -8,19 +8,23 @@
         if($params['per_page'] < $meta['count_filtered']) {
             $ticketmachine_output .= '<div class="float-right btn-group">';
 
+            $disabled = "disabled";
             if($meta['has_previous_page']) {
                 $query['pg'] = $params['pg']-1;
                 $query_result = http_build_query($query);
-                $ticketmachine_output .= "<a class='btn btn-secondary' href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "'><i class='fas fa-angle-left'></i></a>";
+                $disabled = "";
             }
+            $ticketmachine_output .= "<a class='btn btn-secondary' href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "' " . $disabled . "><i class='fas fa-angle-left'></i></a>";
 
             $ticketmachine_output .= "<";
 
+            $disabled = "disabled";
             if($meta['has_next_page'] && $meta['next'] <  $meta['count_filtered']) {
                 $query['pg'] = $params['pg']+1;
                 $query_result = http_build_query($query);
-                $ticketmachine_output .= "<a class='btn btn-secondary' href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "'><i class='fas fa-angle-right'></i></a>";
+                $disabled = "";
             }
+            $ticketmachine_output .= "<a class='btn btn-secondary' href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "' " . $disabled . "><i class='fas fa-angle-right'></i></a>";
 
             $ticketmachine_output .= '</div>';
         }
