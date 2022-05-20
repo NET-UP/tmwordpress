297adddd0f9cda11f0311b149052aaadc06aaa5f
Billie Bell <billie.bell@net-up.de>
2022-05-20 11:26:14 +0200

    test

--- partials/_pagination.php
+++ partials/_pagination.php
@@ -1,6 +1,7 @@
 <?php
 	if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
     function ticketmachine_pagination ( $meta, $params ) {
+        $ticketmachine_output = "";
         $query = $_GET;
         if($meta['has_previous_page']) {
             $query['pg'] = $params['pg']-1;
@@ -12,5 +13,7 @@
             $query_result = http_build_query($query);
             $ticketmachine_output .= "<a class='btn btn-secondary' href='" . strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result . "'><i class='fas fa-angle-right'></i></a>";
         }
+
+        return $ticketmachine_output;
 	}
 ?>
