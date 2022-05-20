c00fc6de7a2d62ff0c2076ccedda33c85bfe49b4
Billie Bell <billie.bell@net-up.de>
2022-05-20 13:10:37 +0200

    test

--- partials/_pagination.php
+++ partials/_pagination.php
@@ -1,6 +1,7 @@
 <?php
 	if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
     function ticketmachine_pagination ( $meta, $params ) {
+        ticketmachine_debug($meta);
         ticketmachine_debug($params);
         $query = $_GET;
         $ticketmachine_output = "";
