2742a9975fbaa9c2eb3d01ae8c2e881abd4d1d38
Billie Bell <billie.bell@net-up.de>
2024-07-01 18:02:19 +0200

    fix: WP and PHP version in mail

--- ticketmachine-event-manager.php
+++ ticketmachine-event-manager.php
@@ -21,7 +21,7 @@
 	add_action( 'init', 'ticketmachine_wpdocs_load_textdomain' );
 
 	global $ticketmachine_db_version;
-	$ticketmachine_db_version = "1.9.1";
+	$ticketmachine_db_version = "1.9.2";
 	
 	// Load translations if they don't already exist
     function ticketmachine_wpdocs_load_textdomain() {
