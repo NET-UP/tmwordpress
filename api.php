97da04df607a93797b2062f41150b1b3836db241
Billie Bell <billie.bell@net-up.de>
2024-11-08 12:34:23 +0100

    fix: endless loop of mails

--- api.php
+++ api.php
@@ -290,7 +290,7 @@
     }
 
     function ticketmachine_tmapi_refresh_token_failed () {
-        global $wpdb, $ticketmachine_globals;
+        global $wpdb, $ticketmachine_globals, $wp_version, $ticketmachine_db_version;
         
         $actual_config = (object)$wpdb->get_results("SELECT * FROM {$wpdb->prefix}ticketmachine_config LIMIT 0,1")[0];
 
