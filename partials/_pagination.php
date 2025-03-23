bda0dc3d31197c77a7773977aea1ec0b02421892
Billie Bell <billie.bell@net-up.de>
2025-03-23 17:45:50 +0100

    feat: full pagination

--- partials/_pagination.php
+++ partials/_pagination.php
@@ -16,11 +16,17 @@
                 $href = "";
                 $disabled = "disabled";
             }
-            $ticketmachine_output .= "<a class='btn btn-secondary " . $disabled . "'" . $href . "><i class='fas fa-angle-left'></i></a>";
+            
+            $query_first = $query;
+            $query_first['pg'] = 1;
+            $query_result_first = http_build_query($query_first);
+            $link_first = strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result_first;
+            $href_first = " href='" . $link_first . "'";
 
-            if($meta['has_previous_page']) {
-                $ticketmachine_output .= "<a class='btn btn-secondary'" . $href . ">" .$params['pg']-1 . "</a>";
-            }
+            $ticketmachine_output .= "<a class='btn btn-secondary " . $disabled . "'" . $href . "><i class='fas fa-angle-left'></i></a>";
+            if(!$disabled && $params['pg'] > 2) $ticketmachine_output .= "<a class='btn btn-secondary'" . $href_first . ">1</a>";
+            if(!$disabled && $params['pg'] > 3) $ticketmachine_output .= "<a class='btn btn-secondary'>...</a>";
+            if(!$disabled) $ticketmachine_output .= "<a class='btn btn-secondary'" . $href . ">" .$params['pg']-1 . "</a>";
 
             $ticketmachine_output .= "<button class='btn btn-primary' readonly>" .$params['pg'] . "</button>";
 
@@ -34,11 +40,16 @@
                 $href = "";
                 $disabled = "disabled";
             }
+
+            $query_last = $query;
+            $query_last['pg'] =  ceil( $meta['count_filtered'] / $params['per_page'] );
+            $query_result_last = http_build_query($query_last);
+            $link_last = strtok($_SERVER["REQUEST_URI"], '?') . "?" . $query_result_last;
+            $href_last = " href='" . $link_last . "'";
             
-            if($meta['has_next_page'] && $meta['next'] <  $meta['count_filtered']) {
-                $ticketmachine_output .= "<a class='btn btn-secondary'" . $href . ">" .$params['pg']+1 . "</a>";
-            }
-            
+            if(!$disabled) $ticketmachine_output .= "<a class='btn btn-secondary " . $disabled . "'" . $href . ">" .$params['pg']+1 . "</a>";
+            if(!$disabled && $params['pg'] < $query_last['pg']-2) $ticketmachine_output .= "<a class='btn btn-secondary'>...</a>";
+            if(!$disabled && $params['pg'] < $query_last['pg']-1) $ticketmachine_output .= "<a class='btn btn-secondary'" . $href_last . ">".$query_last['pg']."</a>";
             $ticketmachine_output .= "<a class='btn btn-secondary " . $disabled . "'" . $href . "><i class='fas fa-angle-right'></i></a>";
 
             $ticketmachine_output .= '</div>';
