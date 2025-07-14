0ecf6f25749194a4533081df16acc48d417048a6
= <=>
2025-07-14 18:49:17 +0200

    fix: typo

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,4 +1,4 @@
-jQuwery(document).ready(function () {
+jQuery(document).ready(function () {
   var today = new Date()
   var dd = String(today.getDate()).padStart(2, "0")
   var mm = String(today.getMonth() + 1).padStart(2, "0") //January is 0!
